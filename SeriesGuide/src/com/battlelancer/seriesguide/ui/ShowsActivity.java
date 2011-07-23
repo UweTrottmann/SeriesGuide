a328a82265b91eabccda71ce344dd735c78f7ddd
Uwe Trottmann <uwe.trottmann@gmail.com>
2011-07-23 15:20:14 +0200

    Add tracking to favoriting shows.

--- SeriesGuide/src/com/battlelancer/seriesguide/ui/ShowsActivity.java
+++ SeriesGuide/src/com/battlelancer/seriesguide/ui/ShowsActivity.java
@@ -439,6 +439,8 @@ public class ShowsActivity extends BaseActivity implements AbsListView.OnScrollL
 
         switch (item.getItemId()) {
             case CONTEXT_FAVORITE: {
+                fireTrackerEvent("Favorite show");
+                
                 ContentValues values = new ContentValues();
                 values.put(Shows.FAVORITE, true);
                 getContentResolver().update(Shows.buildShowUri(String.valueOf(info.id)), values,
@@ -447,6 +449,8 @@ public class ShowsActivity extends BaseActivity implements AbsListView.OnScrollL
                 return true;
             }
             case CONTEXT_UNFAVORITE: {
+                fireTrackerEvent("Unfavorite show");
+                
                 ContentValues values = new ContentValues();
                 values.put(Shows.FAVORITE, false);
                 getContentResolver().update(Shows.buildShowUri(String.valueOf(info.id)), values,
