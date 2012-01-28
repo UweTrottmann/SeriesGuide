822475391a285defd89daa31eb09440bcfa7b648
Uwe Trottmann <uwe.trottmann@gmail.com>
2012-01-28 18:37:28 +0100

    Use https for GetGlue request and access urls. Use shorter callbacks.

--- SeriesGuide/src/com/battlelancer/seriesguide/getglueapi/GetGlue.java
+++ SeriesGuide/src/com/battlelancer/seriesguide/getglueapi/GetGlue.java
@@ -27,15 +27,15 @@ public class GetGlue {
 
     private static final String GETGLUE_SOURCE = "&source=http://seriesguide.uwetrottmann.com/&app=SeriesGuide";
 
-    public static final String REQUEST_URL = "http://api.getglue.com/oauth/request_token";
+    public static final String REQUEST_URL = "https://api.getglue.com/oauth/request_token";
 
-    public static final String ACCESS_URL = "http://api.getglue.com/oauth/access_token";
+    public static final String ACCESS_URL = "https://api.getglue.com/oauth/access_token";
 
     public static final String AUTHORIZE_URL = "http://getglue.com/oauth/authorize?style=mobile";
 
-    public static final String OAUTH_CALLBACK_SCHEME = "seriesguide-oauth-getglue";
+    public static final String OAUTH_CALLBACK_SCHEME = "sgoauth";
 
-    public static final String OAUTH_CALLBACK_HOST = "callback";
+    public static final String OAUTH_CALLBACK_HOST = "getgluecallback";
 
     public static final String OAUTH_CALLBACK_URL = OAUTH_CALLBACK_SCHEME + "://"
             + OAUTH_CALLBACK_HOST;
