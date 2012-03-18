d9b73de6a751ba55ebd17773e16803c6eb5ef0d0
Uwe Trottmann <uwe.trottmann@gmail.com>
2012-03-18 19:25:53 +0100

    Add spoiler support to trakt-java (courtesy of florianmski). Get this and various other changes upstream!

--- SeriesGuide/src/com/jakewharton/trakt/entities/Shout.java
+++ SeriesGuide/src/com/jakewharton/trakt/entities/Shout.java
@@ -1,3 +1,4 @@
+
 package com.jakewharton.trakt.entities;
 
 import com.jakewharton.trakt.TraktEntity;
@@ -8,19 +9,25 @@ public class Shout implements TraktEntity {
     private static final long serialVersionUID = 4324069488018464744L;
 
     public Calendar inserted;
+
     public String shout;
+
     public UserProfile user;
+
+    public boolean spoiler;
 
     /** @deprecated Use {@link #inserted} */
     @Deprecated
     public Calendar getInserted() {
         return this.inserted;
     }
+
     /** @deprecated Use {@link #shout} */
     @Deprecated
     public String getShout() {
         return this.shout;
     }
+
     /** @deprecated Use {@link #user} */
     @Deprecated
     public UserProfile getUser() {
