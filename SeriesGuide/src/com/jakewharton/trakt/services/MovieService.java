43c3ea63af6eee0e8cef524d9d3173d276111710
Uwe Trottmann <uwe.trottmann@gmail.com>
2012-03-25 14:10:40 +0200

    Add support for message parameter of trakt show/checkin and movie/checkin.

--- SeriesGuide/src/com/jakewharton/trakt/services/MovieService.java
+++ SeriesGuide/src/com/jakewharton/trakt/services/MovieService.java
@@ -1002,6 +1002,7 @@ public class MovieService extends TraktApiService {
         private static final String POST_DURATION = "duration";
         private static final String POST_VENUE_ID = "venue_id";
         private static final String POST_VENUE_NAME = "venue_name";
+        private static final String POST_MESSAGE = "message";
 
         private static final String URI = "/movie/checkin/" + FIELD_API_KEY;
 
@@ -1049,6 +1050,17 @@ public class MovieService extends TraktApiService {
         /** Custom venue name for display purposes. */
         public CheckinBuilder venueName(String venueName) {
             this.postParameter(POST_VENUE_NAME, venueName);
+            return this;
+        }
+        
+        /**
+         * The message to use for sharing. If not sent, it will use the
+         * localized watching string set on the connections page. The message
+         * will be truncated to 100 characters to make sure it fits in the tweet
+         * with the url and hashtag.
+         */
+        public CheckinBuilder message(String message) {
+            this.postParameter(POST_MESSAGE, message);
             return this;
         }
     }
