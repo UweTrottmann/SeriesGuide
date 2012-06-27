4d78c811ec4296606e1c51a8a7a2a57b45f3538f
Uwe Trottmann <uwe.trottmann@gmail.com>
2012-06-27 15:30:59 +0200

    Update shows if their overview screen is used
    With a 12 hour threshold (to be implemented).

--- SeriesGuide/src/com/battlelancer/seriesguide/ui/OverviewActivity.java
+++ SeriesGuide/src/com/battlelancer/seriesguide/ui/OverviewActivity.java
@@ -5,8 +5,13 @@ import com.actionbarsherlock.app.ActionBar;
 import com.battlelancer.seriesguide.R;
 import com.battlelancer.seriesguide.items.Series;
 import com.battlelancer.seriesguide.util.DBUtils;
+import com.battlelancer.seriesguide.util.TaskManager;
+import com.battlelancer.seriesguide.util.UpdateTask;
+import com.battlelancer.seriesguide.util.Utils;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
 
@@ -31,6 +36,39 @@ public class OverviewActivity extends BaseActivity {
             FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
             ft.setCustomAnimations(android.R.anim.fade_in, android.R.anim.fade_out);
             ft.replace(R.id.fragment_overview, mFragment).commit();
+        }
+
+        // try to update this show
+        onUpdate();
+    }
+
+    private void onUpdate() {
+        final int showId = getIntent().getIntExtra(OverviewFragment.InitBundle.SHOW_TVDBID, -1);
+
+        // only update this show if no global update is running
+        if (showId != -1 && !TaskManager.getInstance(this).isUpdateTaskRunning(false)) {
+
+            // look if we are online
+            if (Utils.isNetworkConnected(this)) {
+                final SharedPreferences prefs = PreferenceManager
+                        .getDefaultSharedPreferences(getApplicationContext());
+
+                // check if auto-update is enabled
+                final boolean isAutoUpdateEnabled = prefs.getBoolean(
+                        SeriesGuidePreferences.KEY_AUTOUPDATE, true);
+                if (isAutoUpdateEnabled) {
+
+                    // check if wifi is required and available
+                    final boolean isWifiOnly = prefs.getBoolean(
+                            SeriesGuidePreferences.KEY_AUTOUPDATEWLANONLY, true);
+                    if (!isWifiOnly || Utils.isWifiConnected(this)) {
+
+                        UpdateTask updateTask = new UpdateTask(String.valueOf(showId), this);
+                        TaskManager.getInstance(this).tryUpdateTask(updateTask, false, -1);
+                    }
+                }
+            }
+
         }
     }
 
