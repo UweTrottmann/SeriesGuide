c9aaffbf505b07908cbf1eeffe345a28490d7f6c
Uwe Trottmann <uwe.trottmann@gmail.com>
2012-07-13 09:53:02 +0200

    Added license to pretty much everywhere

--- SeriesGuide/src/com/battlelancer/seriesguide/provider/SeriesGuideDatabase.java
+++ SeriesGuide/src/com/battlelancer/seriesguide/provider/SeriesGuideDatabase.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2011 Uwe Trottmann
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ */
 
 package com.battlelancer.seriesguide.provider;
 
@@ -8,7 +24,6 @@ import com.battlelancer.seriesguide.provider.SeriesContract.EpisodesColumns;
 import com.battlelancer.seriesguide.provider.SeriesContract.SeasonsColumns;
 import com.battlelancer.seriesguide.provider.SeriesContract.Shows;
 import com.battlelancer.seriesguide.provider.SeriesContract.ShowsColumns;
-import com.battlelancer.seriesguide.provider.SeriesGuideDatabase.Tables;
 import com.battlelancer.seriesguide.util.Utils;
 
 import android.app.SearchManager;
--- SeriesGuide/src/com/battlelancer/seriesguide/ui/SeasonsActivity.java
+++ SeriesGuide/src/com/battlelancer/seriesguide/ui/SeasonsActivity.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012 Uwe Trottmann
+ * Copyright 2011 Uwe Trottmann
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
