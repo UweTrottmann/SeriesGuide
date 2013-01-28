5535465b503c7101a55cb01b8bb3568d4c9a124b
Uwe Trottmann <uwe.trottmann@gmail.com>
2013-01-28 09:33:57 +0100

    Properly send view of add list dialog, add description.

--- SeriesGuide/src/com/battlelancer/seriesguide/ui/dialogs/AddListDialogFragment.java
+++ SeriesGuide/src/com/battlelancer/seriesguide/ui/dialogs/AddListDialogFragment.java
@@ -24,6 +24,9 @@ import com.battlelancer.seriesguide.ui.dialogs.ListManageDialogFragment.CharAndD
 import com.google.analytics.tracking.android.EasyTracker;
 import com.uwetrottmann.seriesguide.R;
 
+/**
+ * Displays a dialog to add a new list to lists.
+ */
 public class AddListDialogFragment extends DialogFragment {
 
     public static AddListDialogFragment newInstance() {
@@ -109,7 +112,7 @@ public class AddListDialogFragment extends DialogFragment {
     @Override
     public void onStart() {
         super.onStart();
-        EasyTracker.getTracker().trackView("Add List Dialog");
+        EasyTracker.getTracker().sendView("Add List Dialog");
     }
 
     /**
