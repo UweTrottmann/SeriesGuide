0edcce8ec3cf5cdd528a819a27b64a709dfa7705
Uwe Trottmann <uwe.trottmann@gmail.com>
2013-02-12 12:21:35 +0100

    Show next upcoming episode (only first one for now).

--- SeriesGuide/src/com/battlelancer/seriesguide/dashclock/UpcomingEpisodeExtension.java
+++ SeriesGuide/src/com/battlelancer/seriesguide/dashclock/UpcomingEpisodeExtension.java
@@ -2,8 +2,12 @@
 package com.battlelancer.seriesguide.dashclock;
 
 import android.content.Intent;
-import android.net.Uri;
+import android.database.Cursor;
 
+import com.battlelancer.seriesguide.ui.UpcomingFragment.UpcomingQuery;
+import com.battlelancer.seriesguide.ui.UpcomingRecentActivity;
+import com.battlelancer.seriesguide.util.DBUtils;
+import com.battlelancer.seriesguide.util.Utils;
 import com.google.android.apps.dashclock.api.DashClockExtension;
 import com.google.android.apps.dashclock.api.ExtensionData;
 import com.uwetrottmann.seriesguide.R;
@@ -11,15 +15,41 @@ import com.uwetrottmann.seriesguide.R;
 public class UpcomingEpisodeExtension extends DashClockExtension {
 
     @Override
-    protected void onUpdateData(int arg0) {
-        publishUpdate(new ExtensionData()
-                .visible(true)
-                .icon(R.drawable.ic_notification)
-                .status("Hello")
-                .expandedTitle("Hello, world!")
-                .expandedBody("This is an example.")
-                .clickIntent(new Intent(Intent.ACTION_VIEW,
-                        Uri.parse("http://www.google.com"))));
+    protected void onInitialize(boolean isReconnect) {
+        super.onInitialize(isReconnect);
+        setUpdateWhenScreenOn(true);
     }
+    
+    @Override
+    protected void onUpdateData(int arg0) {
+        final Cursor upcomingEpisodes = DBUtils.getUpcomingEpisodes(true, getApplicationContext());
 
+        if (upcomingEpisodes != null && upcomingEpisodes.getCount() > 0
+                && upcomingEpisodes.moveToFirst()) {
+            final String[] timeAndDay = Utils.formatToTimeAndDay(
+                    upcomingEpisodes.getLong(UpcomingQuery.FIRSTAIREDMS), this);
+
+            publishUpdate(new ExtensionData()
+                    .visible(true)
+                    .icon(R.drawable.ic_notification)
+                    .status(timeAndDay[2])
+                    .expandedTitle(
+                            timeAndDay[2] + " - "
+                                    + upcomingEpisodes.getString(UpcomingQuery.SHOW_TITLE))
+                    .expandedBody(
+                            getString(R.string.upcoming_show_detailed, timeAndDay[0],
+                                    upcomingEpisodes.getString(UpcomingQuery.SHOW_NETWORK)))
+                    .clickIntent(
+                            new Intent(getApplicationContext(), UpcomingRecentActivity.class)
+                                    .putExtra(
+                                            UpcomingRecentActivity.InitBundle.SELECTED_TAB, 1)));
+        } else {
+            // nothing to show
+            publishUpdate(null);
+        }
+
+        if (upcomingEpisodes != null) {
+            upcomingEpisodes.close();
+        }
+    }
 }
