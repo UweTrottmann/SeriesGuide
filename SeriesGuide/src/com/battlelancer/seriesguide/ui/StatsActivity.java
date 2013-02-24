de3dfae8adf250f773cabd4a090db24090b866cf
Uwe Trottmann <uwe.trottmann@gmail.com>
2013-02-24 10:16:51 +0100

    Add stats fragment hull, icon for stats activity.

--- SeriesGuide/src/com/battlelancer/seriesguide/ui/StatsActivity.java
+++ SeriesGuide/src/com/battlelancer/seriesguide/ui/StatsActivity.java
@@ -1,11 +1,68 @@
+/*
+ * Copyright 2012 Uwe Trottmann
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ */
+
 package com.battlelancer.seriesguide.ui;
 
+import android.os.Bundle;
+import android.support.v4.app.FragmentTransaction;
+
+import com.actionbarsherlock.app.ActionBar;
+import com.google.analytics.tracking.android.EasyTracker;
+import com.uwetrottmann.seriesguide.R;
+
+/**
+ * Hosts fragments displaying statistics.
+ */
 public class StatsActivity extends BaseTopActivity {
+
+    private static final String TAG = "Statistics";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_singlepane_empty);
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setTitle(R.string.statistics);
+        actionBar.setIcon(R.drawable.ic_action_bargraph);
+
+        if (savedInstanceState == null) {
+            StatsFragment f = new StatsFragment();
+            FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+            ft.add(R.id.root_container, f);
+            ft.commit();
+        }
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        EasyTracker.getInstance().activityStart(this);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        EasyTracker.getInstance().activityStop(this);
+    }
 
     @Override
     protected void fireTrackerEvent(String label) {
-        // TODO Auto-generated method stub
-
+        EasyTracker.getTracker().sendEvent(TAG, "Action Item", label, (long) 0);
     }
 
 }
---
+++ SeriesGuide/src/com/battlelancer/seriesguide/ui/StatsFragment.java
@@ -0,0 +1,7 @@
+package com.battlelancer.seriesguide.ui;
+
+import com.actionbarsherlock.app.SherlockFragment;
+
+public class StatsFragment extends SherlockFragment {
+
+}
