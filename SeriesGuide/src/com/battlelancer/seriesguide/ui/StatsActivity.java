16a619bf783a71c2b3d444c80dc0276c04380d61
Uwe Trottmann <uwe.trottmann@gmail.com>
2013-02-24 09:56:34 +0100

    Add stats activity to manifests.

---
+++ SeriesGuide/src/com/battlelancer/seriesguide/ui/StatsActivity.java
@@ -0,0 +1,11 @@
+package com.battlelancer.seriesguide.ui;
+
+public class StatsActivity extends BaseTopActivity {
+
+    @Override
+    protected void fireTrackerEvent(String label) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
