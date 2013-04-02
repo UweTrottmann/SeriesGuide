1a59c0aef40b4fab480ebb349508b8e06c6a0acb
Uwe Trottmann <uwe.trottmann@gmail.com>
2013-04-02 16:48:18 +0200

    Add remaining show properties.

--- SeriesGuide/src/com/battlelancer/seriesguide/dataliberation/model/Show.java
+++ SeriesGuide/src/com/battlelancer/seriesguide/dataliberation/model/Show.java
@@ -16,9 +16,11 @@ public class Show {
     public boolean favorite;
 
     public boolean hidden;
-    
+
+    public boolean sync;
+
     public long airtime;
-    
+
     public String airday;
 
     @SerializedName("check_in_getglue_id")
@@ -26,4 +28,39 @@ public class Show {
 
     @SerializedName("last_watched_episode")
     public int lastWatchedEpisode;
+
+    public String poster;
+
+    @SerializedName("content_rating")
+    public String contentRating;
+
+    public String status;
+
+    public int runtime;
+
+    public String network;
+
+    @SerializedName("imdb_id")
+    public String imdbId;
+
+    @SerializedName("first_aired")
+    public String firstAired;
+
+    /*
+     * Full dump only following.
+     */
+
+    public String overview;
+
+    public double rating;
+
+    public String genres;
+
+    public String actors;
+
+    @SerializedName("last_updated")
+    public long lastUpdated;
+
+    @SerializedName("last_edited")
+    public long lastEdited;
 }
