3b998a0829e0254f3980033ea95799c3f3a30316
Uwe Trottmann <uwe.trottmann@gmail.com>
2013-04-10 23:12:47 +0200

    Clean up episode row layout (too many numbers all over).

--- SeriesGuide/src/com/battlelancer/seriesguide/adapters/EpisodesAdapter.java
+++ SeriesGuide/src/com/battlelancer/seriesguide/adapters/EpisodesAdapter.java
@@ -63,8 +63,8 @@ public class EpisodesAdapter extends CursorAdapter {
                     .findViewById(R.id.textViewEpisodeNumber);
             viewHolder.episodeAirdate = (TextView) convertView
                     .findViewById(R.id.textViewEpisodeAirdate);
-            viewHolder.episodeAbsoluteNumber = (TextView) convertView
-                    .findViewById(R.id.textViewEpisodeAbsoluteNumber);
+            viewHolder.episodeAlternativeNumbers = (TextView) convertView
+                    .findViewById(R.id.textViewEpisodeAlternativeNumbers);
             viewHolder.collected = (ImageView) convertView
                     .findViewById(R.id.imageViewCollected);
             viewHolder.contextMenu = (ImageView) convertView
@@ -99,20 +99,24 @@ public class EpisodesAdapter extends CursorAdapter {
                         : View.INVISIBLE);
 
         // number
-        StringBuilder episodenumberValue = new StringBuilder(String.valueOf(episodeNumber));
-        float dvdNumber = mCursor.getFloat(EpisodesQuery.DVDNUMBER);
-        if (dvdNumber > 0 && dvdNumber != episodeNumber) {
-            episodenumberValue.append(" (").append(dvdNumber).append(")");
-        }
-        viewHolder.episodeNumber.setText(episodenumberValue);
+        viewHolder.episodeNumber.setText(String.valueOf(episodeNumber));
 
-        // absolute number
+        // alternative numbers
+        StringBuilder altNumbers = new StringBuilder();
         int absoluteNumber = mCursor.getInt(EpisodesQuery.ABSOLUTE_NUMBER);
-        if (absoluteNumber > 0 && absoluteNumber != episodeNumber) {
-            viewHolder.episodeAbsoluteNumber.setText(String.valueOf(absoluteNumber));
-        } else {
-            viewHolder.episodeAbsoluteNumber.setText("");
+        if (absoluteNumber > 0) {
+            altNumbers.append(mContext.getString(R.string.episode_number_absolute)).append(" ")
+                    .append(absoluteNumber);
         }
+        double dvdNumber = mCursor.getDouble(EpisodesQuery.DVDNUMBER);
+        if (dvdNumber > 0) {
+            if (altNumbers.length() != 0) {
+                altNumbers.append(" | ");
+            }
+            altNumbers.append(mContext.getString(R.string.episode_number_disk)).append(" ")
+                    .append(dvdNumber);
+        }
+        viewHolder.episodeAlternativeNumbers.setText(altNumbers);
 
         // air date
         long airtime = mCursor.getLong(EpisodesQuery.FIRSTAIREDMS);
@@ -139,7 +143,7 @@ public class EpisodesAdapter extends CursorAdapter {
     }
 
     static class ViewHolder {
-        TextView episodeAbsoluteNumber;
+        TextView episodeAlternativeNumbers;
         TextView episodeAirdate;
         TextView episodeNumber;
         TextView episodeTitle;
