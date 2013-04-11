c09a5260cc010567f9332537bcc59c7bf124e84a
Uwe Trottmann <uwe.trottmann@gmail.com>
2013-04-11 09:47:24 +0200

    Remove old share action provider, use basic share tool.

--- SeriesGuide/src/com/battlelancer/seriesguide/ui/ShowInfoFragment.java
+++ SeriesGuide/src/com/battlelancer/seriesguide/ui/ShowInfoFragment.java
@@ -22,7 +22,6 @@ import com.actionbarsherlock.app.SherlockFragment;
 import com.actionbarsherlock.view.Menu;
 import com.actionbarsherlock.view.MenuInflater;
 import com.actionbarsherlock.view.MenuItem;
-import com.actionbarsherlock.widget.ShareActionProvider;
 import com.battlelancer.seriesguide.Constants;
 import com.battlelancer.seriesguide.items.Series;
 import com.battlelancer.seriesguide.provider.SeriesContract.ListItemTypes;
@@ -45,8 +44,6 @@ public class ShowInfoFragment extends SherlockFragment {
 
     private static final String TAG = "Show Info";
 
-    private IntentBuilder mShareIntentBuilder;
-
     public static ShowInfoFragment newInstance(int showTvdbId) {
         ShowInfoFragment f = new ShowInfoFragment();
 
@@ -56,6 +53,8 @@ public class ShowInfoFragment extends SherlockFragment {
 
         return f;
     }
+
+    private Series mShow;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -67,7 +66,7 @@ public class ShowInfoFragment extends SherlockFragment {
         super.onActivityCreated(savedInstanceState);
 
         onPopulateShowData();
-        
+
         setHasOptionsMenu(true);
     }
 
@@ -75,11 +74,6 @@ public class ShowInfoFragment extends SherlockFragment {
     public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
         super.onCreateOptionsMenu(menu, inflater);
         inflater.inflate(R.menu.showinfo_menu, menu);
-
-        MenuItem shareItem = menu.findItem(R.id.menu_share);
-        ShareActionProvider shareActionProvider = (ShareActionProvider) shareItem
-                .getActionProvider();
-        shareActionProvider.setShareIntent(mShareIntentBuilder.getIntent());
     }
 
     @Override
@@ -92,13 +86,16 @@ public class ShowInfoFragment extends SherlockFragment {
             ListsDialogFragment.showListsDialog(String.valueOf(getShowTvdbId()),
                     ListItemTypes.SHOW, getFragmentManager());
             return true;
+        } else if (itemId == R.id.menu_share) {
+            onShareShow();
+            return true;
         }
         return super.onOptionsItemSelected(item);
     }
 
     private void onPopulateShowData() {
-        final Series show = DBUtils.getShow(getActivity(), String.valueOf(getShowTvdbId()));
-        if (show == null) {
+        mShow = DBUtils.getShow(getActivity(), String.valueOf(getShowTvdbId()));
+        if (mShow == null) {
             return;
         }
 
@@ -108,61 +105,61 @@ public class ShowInfoFragment extends SherlockFragment {
         TextView status = (TextView) getView().findViewById(R.id.showStatus);
 
         // Name
-        seriesname.setText(show.getTitle());
+        seriesname.setText(mShow.getTitle());
 
         // Overview
-        if (TextUtils.isEmpty(show.getOverview())) {
+        if (TextUtils.isEmpty(mShow.getOverview())) {
             overview.setText("");
         } else {
-            overview.setText(show.getOverview());
+            overview.setText(mShow.getOverview());
         }
 
         // air time
         StringBuilder infoText = new StringBuilder();
-        if (show.getAirsDayOfWeek().length() == 0 || show.getAirsTime() == -1) {
+        if (mShow.getAirsDayOfWeek().length() == 0 || mShow.getAirsTime() == -1) {
             infoText.append(getString(R.string.show_noairtime));
         } else {
-            String[] values = Utils.parseMillisecondsToTime(show.getAirsTime(),
-                    show.getAirsDayOfWeek(), getActivity());
+            String[] values = Utils.parseMillisecondsToTime(mShow.getAirsTime(),
+                    mShow.getAirsDayOfWeek(), getActivity());
             infoText.append(values[1]).append(" ").append(values[0]);
         }
         // network
-        if (show.getNetwork().length() != 0) {
+        if (mShow.getNetwork().length() != 0) {
             infoText.append(" ").append(getString(R.string.show_network)).append(" ")
-                    .append(show.getNetwork());
+                    .append(mShow.getNetwork());
         }
         info.setText(infoText);
 
         // Running state
-        if (show.getStatus() == 1) {
+        if (mShow.getStatus() == 1) {
             TypedValue outValue = new TypedValue();
             getActivity().getTheme().resolveAttribute(R.attr.textColorSgGreen,
                     outValue, true);
             status.setTextColor(getResources().getColor(outValue.resourceId));
             status.setText(getString(R.string.show_isalive));
-        } else if (show.getStatus() == 0) {
+        } else if (mShow.getStatus() == 0) {
             status.setTextColor(Color.GRAY);
             status.setText(getString(R.string.show_isnotalive));
         }
 
         // first airdate
-        long airtime = Utils.buildEpisodeAirtime(show.getFirstAired(), show.getAirsTime());
+        long airtime = Utils.buildEpisodeAirtime(mShow.getFirstAired(), mShow.getAirsTime());
         Utils.setValueOrPlaceholder(getView().findViewById(R.id.TextViewShowInfoFirstAirdate),
                 Utils.formatToDate(airtime, getActivity()));
 
         // Others
         Utils.setValueOrPlaceholder(getView().findViewById(R.id.TextViewShowInfoActors),
-                Utils.splitAndKitTVDBStrings(show.getActors()));
+                Utils.splitAndKitTVDBStrings(mShow.getActors()));
         Utils.setValueOrPlaceholder(getView().findViewById(R.id.TextViewShowInfoContentRating),
-                show.getContentRating());
+                mShow.getContentRating());
         Utils.setValueOrPlaceholder(getView().findViewById(R.id.TextViewShowInfoGenres),
-                Utils.splitAndKitTVDBStrings(show.getGenres()));
+                Utils.splitAndKitTVDBStrings(mShow.getGenres()));
         Utils.setValueOrPlaceholder(getView().findViewById(R.id.TextViewShowInfoRuntime),
-                show.getRuntime()
+                mShow.getRuntime()
                         + " " + getString(R.string.show_airtimeunit));
 
         // TVDb rating
-        String ratingText = show.getRating();
+        String ratingText = mShow.getRating();
         if (ratingText != null && ratingText.length() != 0) {
             RatingBar ratingBar = (RatingBar) getView().findViewById(R.id.bar);
             ratingBar.setProgress((int) (Double.valueOf(ratingText) / 0.1));
@@ -181,7 +178,7 @@ public class ShowInfoFragment extends SherlockFragment {
 
         // Last edit date
         TextView lastEdit = (TextView) getView().findViewById(R.id.lastEdit);
-        long lastEditRaw = show.getLastEdit();
+        long lastEditRaw = mShow.getLastEdit();
         if (lastEditRaw > 0) {
             lastEdit.setText(DateUtils.formatDateTime(getActivity(), lastEditRaw * 1000,
                     DateUtils.FORMAT_SHOW_DATE | DateUtils.FORMAT_SHOW_TIME));
@@ -191,20 +188,20 @@ public class ShowInfoFragment extends SherlockFragment {
 
         // Google Play button
         View playButton = getView().findViewById(R.id.buttonGooglePlay);
-        Utils.setUpGooglePlayButton(show.getTitle(), playButton, TAG);
+        Utils.setUpGooglePlayButton(mShow.getTitle(), playButton, TAG);
 
         // Amazon button
         View amazonButton = getView().findViewById(R.id.buttonAmazon);
-        Utils.setUpAmazonButton(show.getTitle(), amazonButton, TAG);
+        Utils.setUpAmazonButton(mShow.getTitle(), amazonButton, TAG);
 
         // IMDb button
         View imdbButton = (View) getView().findViewById(R.id.buttonShowInfoIMDB);
-        final String imdbId = show.getImdbId();
+        final String imdbId = mShow.getImdbId();
         Utils.setUpImdbButton(imdbId, imdbButton, TAG, getActivity());
 
         // TVDb button
         View tvdbButton = (View) getView().findViewById(R.id.buttonTVDB);
-        final String tvdbId = show.getId();
+        final String tvdbId = mShow.getId();
         if (tvdbButton != null) {
             tvdbButton.setOnClickListener(new OnClickListener() {
                 public void onClick(View v) {
@@ -222,26 +219,17 @@ public class ShowInfoFragment extends SherlockFragment {
             public void onClick(View v) {
                 fireTrackerEvent("Shouts");
                 Intent i = new Intent(getActivity(), TraktShoutsActivity.class);
-                i.putExtras(TraktShoutsActivity.createInitBundleShow(show.getTitle(),
+                i.putExtras(TraktShoutsActivity.createInitBundleShow(mShow.getTitle(),
                         getShowTvdbId()));
                 startActivity(i);
             }
         });
 
-        // Share intent
-        mShareIntentBuilder = ShareCompat.IntentBuilder
-                .from(getActivity())
-                .setChooserTitle(R.string.share)
-                .setText(
-                        getString(R.string.share_checkout) + " \"" + show.getTitle()
-                                + "\" " + Utils.IMDB_TITLE_URL + imdbId)
-                .setType("text/plain");
-
         // Poster
         final ImageView poster = (ImageView) getView().findViewById(R.id.ImageViewShowInfoPoster);
-        ImageProvider.getInstance(getActivity()).loadImage(poster, show.getPoster(), false);
+        ImageProvider.getInstance(getActivity()).loadImage(poster, mShow.getPoster(), false);
         Utils.setPosterBackground((ImageView) getView().findViewById(R.id.background),
-                show.getPoster(), getActivity());
+                mShow.getPoster(), getActivity());
 
         // trakt ratings
         TraktSummaryTask task = new TraktSummaryTask(getActivity(), getView().findViewById(
@@ -263,5 +251,19 @@ public class ShowInfoFragment extends SherlockFragment {
         TraktRateDialogFragment newFragment = TraktRateDialogFragment
                 .newInstance(getShowTvdbId());
         newFragment.show(getFragmentManager(), "traktratedialog");
+    }
+
+    private void onShareShow() {
+        if (mShow != null) {
+            // Share intent
+            IntentBuilder ib = ShareCompat.IntentBuilder
+                    .from(getActivity())
+                    .setChooserTitle(R.string.share)
+                    .setText(
+                            getString(R.string.share_checkout) + " \"" + mShow.getTitle()
+                                    + "\" " + Utils.IMDB_TITLE_URL + mShow.getImdbId())
+                    .setType("text/plain");
+            ib.startChooser();
+        }
     }
 }
