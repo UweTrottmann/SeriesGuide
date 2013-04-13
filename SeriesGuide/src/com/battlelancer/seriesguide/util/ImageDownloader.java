1ef5b4fc49513da766ba4c9e314c6d81ba679c62
Uwe Trottmann <uwe.trottmann@gmail.com>
2013-04-13 16:42:49 +0200

    Increase image downloader cache clear delay to 10 seconds.

--- SeriesGuide/src/com/battlelancer/seriesguide/util/ImageDownloader.java
+++ SeriesGuide/src/com/battlelancer/seriesguide/util/ImageDownloader.java
@@ -357,7 +357,7 @@ public class ImageDownloader {
 
     private static final int HARD_CACHE_CAPACITY = 10;
 
-    private static final int DELAY_BEFORE_PURGE = 5 * 1000; // in milliseconds
+    private static final int DELAY_BEFORE_PURGE = 10 * 1000; // in milliseconds
 
     // Hard cache, with a fixed maximum capacity and a life duration
     @SuppressWarnings("serial")
