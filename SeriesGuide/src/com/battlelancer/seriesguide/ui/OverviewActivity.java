c3eaece1bddbf94853e151c6c6fb6009287cc074
Uwe Trottmann <uwe.trottmann@gmail.com>
2013-05-18 16:12:54 +0200

    Move CreateNdefMessageCallback implementation down.
    This way we can support all Android versions! Simple?!

--- SeriesGuide/src/com/battlelancer/seriesguide/ui/OverviewActivity.java
+++ SeriesGuide/src/com/battlelancer/seriesguide/ui/OverviewActivity.java
@@ -17,9 +17,16 @@
 
 package com.battlelancer.seriesguide.ui;
 
+import android.annotation.TargetApi;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.nfc.NdefMessage;
+import android.nfc.NdefRecord;
+import android.nfc.NfcAdapter;
+import android.nfc.NfcAdapter.CreateNdefMessageCallback;
+import android.nfc.NfcEvent;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.preference.PreferenceManager;
@@ -38,9 +45,11 @@ import com.battlelancer.seriesguide.util.UpdateTask;
 import com.battlelancer.seriesguide.util.Utils;
 import com.battlelancer.thetvdbapi.TheTVDB;
 import com.google.analytics.tracking.android.EasyTracker;
+import com.uwetrottmann.androidutils.AndroidUtils;
 import com.uwetrottmann.seriesguide.R;
 
 import java.lang.ref.WeakReference;
+import java.nio.charset.Charset;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -50,7 +59,9 @@ import java.util.List;
 public class OverviewActivity extends BaseActivity {
 
     private int mShowId;
+    private NfcAdapter mNfcAdapter;
 
+    @TargetApi(Build.VERSION_CODES.ICE_CREAM_SANDWICH)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -92,13 +103,40 @@ public class OverviewActivity extends BaseActivity {
             }
         }
 
-        // if (AndroidUtils.isICSOrHigher()) {
-        // // register for Android Beam
-        // mNfcAdapter = NfcAdapter.getDefaultAdapter(this);
-        // if (mNfcAdapter != null) {
-        // mNfcAdapter.setNdefPushMessageCallback(this, this);
-        // }
-        // }
+        // Support beaming shows via Android Beam
+        if (AndroidUtils.isICSOrHigher()) {
+            mNfcAdapter = NfcAdapter.getDefaultAdapter(this);
+            if (mNfcAdapter != null) {
+                mNfcAdapter.setNdefPushMessageCallback(new CreateNdefMessageCallback() {
+                    @Override
+                    public NdefMessage createNdefMessage(NfcEvent event) {
+                        final Series show = DBUtils.getShow(OverviewActivity.this, mShowId);
+                        // send id, also title and overview (both can be empty)
+                        NdefMessage msg = new NdefMessage(new NdefRecord[] {
+                                createMimeRecord(
+                                        "application/com.battlelancer.seriesguide.beam",
+                                        String.valueOf(mShowId).getBytes()),
+                                createMimeRecord("application/com.battlelancer.seriesguide.beam",
+                                        show.getTitle().getBytes()),
+                                createMimeRecord("application/com.battlelancer.seriesguide.beam",
+                                        show
+                                                .getOverview().getBytes())
+                        });
+                        return msg;
+                    }
+
+                    /**
+                     * Creates a custom MIME type encapsulated in an NDEF record
+                     */
+                    public NdefRecord createMimeRecord(String mimeType, byte[] payload) {
+                        byte[] mimeBytes = mimeType.getBytes(Charset.forName("US-ASCII"));
+                        NdefRecord mimeRecord = new NdefRecord(
+                                NdefRecord.TNF_MIME_MEDIA, mimeBytes, new byte[0], payload);
+                        return mimeRecord;
+                    }
+                }, this);
+            }
+        }
 
         // try to update this show
         onUpdate();
@@ -258,37 +296,5 @@ public class OverviewActivity extends BaseActivity {
         startSearch(null, false, args, false);
         return true;
     }
-
-    // @TargetApi(Build.VERSION_CODES.ICE_CREAM_SANDWICH)
-    // @Override
-    // public NdefMessage createNdefMessage(NfcEvent event) {
-    // final Series show = DBUtils.getShow(this, String.valueOf(mShowId));
-    // // send id, also title and overview (both can be empty)
-    // NdefMessage msg = new NdefMessage(new NdefRecord[] {
-    // createMimeRecord(
-    // "application/com.battlelancer.seriesguide.beam", String.valueOf(mShowId)
-    // .getBytes()),
-    // createMimeRecord("application/com.battlelancer.seriesguide.beam",
-    // show.getTitle()
-    // .getBytes()),
-    // createMimeRecord("application/com.battlelancer.seriesguide.beam", show
-    // .getOverview()
-    // .getBytes())
-    // });
-    // return msg;
-    // }
-    //
-    // /**
-    // * Creates a custom MIME type encapsulated in an NDEF record
-    // *
-    // * @param mimeType
-    // */
-    // @TargetApi(Build.VERSION_CODES.ICE_CREAM_SANDWICH)
-    // public NdefRecord createMimeRecord(String mimeType, byte[] payload) {
-    // byte[] mimeBytes = mimeType.getBytes(Charset.forName("US-ASCII"));
-    // NdefRecord mimeRecord = new NdefRecord(
-    // NdefRecord.TNF_MIME_MEDIA, mimeBytes, new byte[0], payload);
-    // return mimeRecord;
-    // }
 
 }
