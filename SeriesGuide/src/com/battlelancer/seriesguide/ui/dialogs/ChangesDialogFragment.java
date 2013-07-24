2cb947e2bdeac3a93feb147242d8b2151cc5288f
Uwe Trottmann <uwe.trottmann@gmail.com>
2013-07-24 16:27:42 +0200

    Change changes dialog into X deprecation dialog.

--- SeriesGuide/src/com/battlelancer/seriesguide/ui/dialogs/ChangesDialogFragment.java
+++ SeriesGuide/src/com/battlelancer/seriesguide/ui/dialogs/ChangesDialogFragment.java
@@ -32,20 +32,21 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.battlelancer.seriesguide.dataliberation.DataLiberationActivity;
 import com.battlelancer.seriesguide.ui.SeriesGuidePreferences;
 import com.google.analytics.tracking.android.EasyTracker;
 import com.uwetrottmann.seriesguide.R;
 
 /**
- * Shows recent changes set by the betamessage string. Warns about being on the
- * beta channel, offers to download stable version or continue.
+ * Shows a warning that SeriesGuide X will be deprecated soon, migration
+ * tutorial.
  */
 public class ChangesDialogFragment extends DialogFragment {
     private static final String MARKETLINK_HTTP = "http://play.google.com/store/apps/details?id=com.battlelancer.seriesguide";
 
     private static final String MARKETLINK_APP = "market://details?id=com.battlelancer.seriesguide";
 
-    public static final String TAG = "Changes Dialog";
+    public static final String TAG = "Deprecation Dialog";
 
     public static ChangesDialogFragment show(FragmentManager fm) {
         ChangesDialogFragment f = new ChangesDialogFragment();
@@ -70,19 +71,26 @@ public class ChangesDialogFragment extends DialogFragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         final View layout = inflater.inflate(R.layout.changes_dialog, null);
 
-        // title
-        ((TextView) layout.findViewById(R.id.title)).setText(getString(R.string.app_name));
-
         // message
         TextView message = (TextView) layout.findViewById(R.id.message);
         message.setMovementMethod(LinkMovementMethod.getInstance());
 
         // buttons
         Button buttonNegative = (Button) layout.findViewById(R.id.buttonNegative);
-        buttonNegative.setText(R.string.download_stable);
+        buttonNegative.setText(R.string.backup_tool);
         buttonNegative.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View v) {
+                // launch backup activity
+                startActivity(new Intent(getActivity(), DataLiberationActivity.class));
+            }
+        });
+        Button buttonPositive = (Button) layout.findViewById(R.id.buttonPositive);
+        buttonPositive.setText(R.string.download_stable);
+        buttonPositive.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // launch SeriesGuide Play Store page
                 try {
                     Intent myIntent = new Intent(Intent.ACTION_VIEW, Uri
                             .parse(MARKETLINK_APP));
@@ -94,14 +102,6 @@ public class ChangesDialogFragment extends DialogFragment {
                 }
             }
         });
-        Button buttonPositive = (Button) layout.findViewById(R.id.buttonPositive);
-        buttonPositive.setText(R.string.gobreak);
-        buttonPositive.setOnClickListener(new OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                dismiss();
-            }
-        });
 
         return layout;
     }
@@ -109,6 +109,6 @@ public class ChangesDialogFragment extends DialogFragment {
     @Override
     public void onStart() {
         super.onStart();
-        EasyTracker.getTracker().sendView("Changes Dialog");
+        EasyTracker.getTracker().sendView(TAG);
     }
 }
