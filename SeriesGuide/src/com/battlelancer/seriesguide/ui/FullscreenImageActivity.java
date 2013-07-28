ddef575584ddce598f6e19fd55a3ac651cf6a7d2
Uwe Trottmann <uwe.trottmann@gmail.com>
2013-07-28 11:00:30 +0200

    Use SystemUiHider in fullscreen activity.

--- SeriesGuide/src/com/battlelancer/seriesguide/ui/FullscreenImageActivity.java
+++ SeriesGuide/src/com/battlelancer/seriesguide/ui/FullscreenImageActivity.java
@@ -13,6 +13,8 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  * 
+ * Modified by Uwe Trottmann to better work with newer devices.
+ * 
  */
 
 package com.battlelancer.seriesguide.ui;
@@ -20,24 +22,42 @@ package com.battlelancer.seriesguide.ui;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
 import android.widget.ImageView;
 
 import com.battlelancer.seriesguide.util.ImageProvider;
+import com.battlelancer.seriesguide.util.SystemUiHider;
+import com.battlelancer.seriesguide.util.SystemUiHider.OnVisibilityChangeListener;
 import com.uwetrottmann.seriesguide.R;
 
 /**
  * This {@link Activity} is used to display a full screen image of a TV show's
  * poster, or the image provided for a specific episode.
- * 
- * @author Andrew Neal (andrew@seeingpixels.org)
  */
 public class FullscreenImageActivity extends Activity {
 
     /** The {@link Intent} extra used to deliver the path to the requested image */
     public static final String PATH = "fullscreenimageactivity.intent.extra.image";
 
+    /**
+     * The number of milliseconds to wait after user interaction before hiding
+     * the system UI.
+     */
+    private static final int AUTO_HIDE_DELAY_MILLIS = 3000;
+
+    /**
+     * The flags to pass to {@link SystemUiHider#getInstance}.
+     */
+    private static final int HIDER_FLAGS = SystemUiHider.FLAG_HIDE_NAVIGATION;
+
+    /**
+     * The instance of the {@link SystemUiHider} for this activity.
+     */
+    private SystemUiHider mSystemUiHider;
+
     /** Displays the poster or episode preview */
-    private ImageView mImage;
+    private ImageView mContentView;
 
     /**
      * {@inheritDoc}
@@ -51,11 +71,40 @@ public class FullscreenImageActivity extends Activity {
     }
 
     private void setupViews() {
-        mImage = (ImageView) findViewById(R.id.fullscreenImageView);
+        mContentView = (ImageView) findViewById(R.id.fullscreen_content);
 
         // Load the requested image
         String imagePath = getIntent().getExtras().getString(PATH);
-        mImage.setImageBitmap(ImageProvider.getInstance(this).getImage(imagePath, false));
+        mContentView.setImageBitmap(ImageProvider.getInstance(this).getImage(imagePath, false));
+
+        // Set up an instance of SystemUiHider to control the system UI for
+        // this activity.
+        mSystemUiHider = SystemUiHider.getInstance(this, mContentView, HIDER_FLAGS);
+        mSystemUiHider.setup();
+        mSystemUiHider.setOnVisibilityChangeListener(new OnVisibilityChangeListener() {
+            @Override
+            public void onVisibilityChange(boolean visible) {
+                if (visible) {
+                    // Schedule a hide().
+                    delayedHide(AUTO_HIDE_DELAY_MILLIS);
+                }
+            }
+        });
+
+        // Set up the user interaction to manually show or hide the system UI.
+        mContentView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mSystemUiHider.toggle();
+            }
+        });
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+
+        delayedHide(100);
     }
 
     /**
@@ -64,9 +113,27 @@ public class FullscreenImageActivity extends Activity {
     @Override
     public void onDetachedFromWindow() {
         // Release any references to the ImageView
-        mImage.setImageDrawable(null);
-        mImage = null;
+        mContentView.setImageDrawable(null);
+        mContentView = null;
         super.onDetachedFromWindow();
+    }
+
+    Handler mHideHandler = new Handler();
+
+    Runnable mHideRunnable = new Runnable() {
+        @Override
+        public void run() {
+            mSystemUiHider.hide();
+        }
+    };
+
+    /**
+     * Schedules a call to hide() in [delay] milliseconds, canceling any
+     * previously scheduled calls.
+     */
+    private void delayedHide(int delayMillis) {
+        mHideHandler.removeCallbacks(mHideRunnable);
+        mHideHandler.postDelayed(mHideRunnable, delayMillis);
     }
 
 }
