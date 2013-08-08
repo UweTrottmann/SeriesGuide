5b77a87d89d9a01b4ad625a2262391a9e328f29b
Uwe Trottmann <uwe.trottmann@gmail.com>
2013-08-08 22:03:32 +0200

    Setup account only after launching main activity.

--- SeriesGuide/src/com/battlelancer/seriesguide/SeriesGuideApplication.java
+++ SeriesGuide/src/com/battlelancer/seriesguide/SeriesGuideApplication.java
@@ -17,20 +17,15 @@
 
 package com.battlelancer.seriesguide;
 
-import android.accounts.Account;
-import android.accounts.AccountManager;
 import android.annotation.SuppressLint;
 import android.app.Application;
 import android.content.ContentProvider;
-import android.content.ContentResolver;
 import android.content.SharedPreferences;
-import android.os.Bundle;
 import android.os.StrictMode;
 import android.os.StrictMode.ThreadPolicy;
 import android.os.StrictMode.VmPolicy;
 import android.preference.PreferenceManager;
 
-import com.battlelancer.seriesguide.sync.SgAccountAuthenticator;
 import com.battlelancer.seriesguide.ui.SeriesGuidePreferences;
 import com.battlelancer.seriesguide.util.ImageProvider;
 import com.battlelancer.seriesguide.util.Utils;
@@ -71,16 +66,6 @@ public class SeriesGuideApplication extends Application {
 
         // Set a context for Google Analytics
         EasyTracker.getInstance().setContext(this);
-
-        // Set up a dummy account for syncing
-        AccountManager manager = AccountManager.get(this);
-        final Account account = SgAccountAuthenticator.getSyncAccount(this);
-        if (manager.addAccountExplicitly(account, null, null)) {
-            ContentResolver.setIsSyncable(account, CONTENT_AUTHORITY, 1);
-            ContentResolver.setSyncAutomatically(account, CONTENT_AUTHORITY, true);
-            // Sync daily by default
-            ContentResolver.addPeriodicSync(account, CONTENT_AUTHORITY, new Bundle(), 24 * 60 * 60);
-        }
 
         // Enable StrictMode
         enableStrictMode();
