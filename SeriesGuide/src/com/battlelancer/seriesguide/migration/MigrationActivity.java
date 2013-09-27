75531ce15208b9b6bd69039aefd5c561e60452e2
Uwe Trottmann <uwe.trottmann@gmail.com>
2013-09-27 18:10:21 +0200

    Enable backup functionality in migration assistant.

--- SeriesGuide/src/com/battlelancer/seriesguide/migration/MigrationActivity.java
+++ SeriesGuide/src/com/battlelancer/seriesguide/migration/MigrationActivity.java
@@ -1,19 +1,32 @@
 package com.battlelancer.seriesguide.migration;
 
 import android.content.Context;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.view.View;
+import android.widget.ProgressBar;
 
+import com.battlelancer.seriesguide.dataliberation.JsonExportTask;
+import com.battlelancer.seriesguide.dataliberation.OnTaskFinishedListener;
 import com.battlelancer.seriesguide.ui.BaseActivity;
 import com.uwetrottmann.seriesguide.R;
 
 /**
  * Helps users migrate their show database to the free version of SeriesGuide.
  */
-public class MigrationActivity extends BaseActivity {
+public class MigrationActivity extends BaseActivity implements JsonExportTask.OnTaskProgressListener, OnTaskFinishedListener {
 
     private static final String KEY_MIGRATION_OPT_OUT = "com.battlelancer.seriesguide.migration.optout";
+
+    public static boolean hasOptedOutOfMigration(Context context) {
+        return PreferenceManager.getDefaultSharedPreferences(context).getBoolean(KEY_MIGRATION_OPT_OUT,
+                false);
+    }
+
+    private JsonExportTask mTask;
+    private View mButtonBackup;
+    private ProgressBar mProgressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,10 +38,21 @@ public class MigrationActivity extends BaseActivity {
     }
 
     private void setupViews() {
-        findViewById(R.id.buttonMigrationExport).setOnClickListener(new View.OnClickListener() {
+        mProgressBar = (ProgressBar) findViewById(R.id.progressBarMigration);
+
+        mButtonBackup = findViewById(R.id.buttonMigrationExport);
+        mButtonBackup.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                // TODO backup shows
+                // backup shows
+                mTask = new JsonExportTask(MigrationActivity.this, MigrationActivity.this,
+                        MigrationActivity.this,
+                        true, false);
+                mTask.execute();
+
+                mProgressBar.setVisibility(View.VISIBLE);
+
+                preventUserInput(true);
             }
         });
     }
@@ -42,8 +66,35 @@ public class MigrationActivity extends BaseActivity {
         // TODO check if SeriesGuide is already installed
     }
 
-    public static boolean hasOptedOutOfMigration(Context context) {
-        return PreferenceManager.getDefaultSharedPreferences(context).getBoolean(KEY_MIGRATION_OPT_OUT,
-                false);
+    @Override
+    protected void onDestroy() {
+        // clean up backup task
+        if (mTask != null && mTask.getStatus() != AsyncTask.Status.FINISHED) {
+            mTask.cancel(true);
+        }
+        mTask = null;
+
+        super.onDestroy();
+    }
+
+    private void preventUserInput(boolean isLockdown) {
+        // TODO toggle buttons enable state
+        mButtonBackup.setEnabled(!isLockdown);
+    }
+
+    @Override
+    public void onProgressUpdate(Integer... values) {
+        if (mProgressBar == null) {
+            return;
+        }
+        mProgressBar.setIndeterminate(values[0] == values[1]);
+        mProgressBar.setMax(values[0]);
+        mProgressBar.setProgress(values[1]);
+    }
+
+    @Override
+    public void onTaskFinished() {
+        mProgressBar.setVisibility(View.GONE);
+        preventUserInput(false);
     }
 }
