d6ed3f201861f9bdc586eeb4d27af628bcd08915
Uwe Trottmann <uwe.trottmann@gmail.com>
2013-11-11 15:02:35 +0100

    Abort early when fetching shows from trakt failed in add fragment.

--- SeriesGuide/src/main/java/com/battlelancer/seriesguide/ui/TraktAddFragment.java
+++ SeriesGuide/src/main/java/com/battlelancer/seriesguide/ui/TraktAddFragment.java
@@ -38,6 +38,7 @@ import android.database.Cursor;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -158,6 +159,8 @@ public class TraktAddFragment extends AddFragment {
 
     public class GetTraktShowsTask extends AsyncTask<Integer, Void, List<SearchResult>> {
 
+        private static final String TAG = "GetTraktShowsTask";
+
         private Context mContext;
 
         public GetTraktShowsTask(Context context) {
@@ -166,10 +169,11 @@ public class TraktAddFragment extends AddFragment {
 
         @Override
         protected List<SearchResult> doInBackground(Integer... params) {
+            Log.d(TAG, "Getting shows from trakt...");
             int type = params[0];
-            List<SearchResult> showList = new ArrayList<SearchResult>();
+            List<SearchResult> showList = new ArrayList<>();
 
-            List<TvShow> shows = new ArrayList<TvShow>();
+            List<TvShow> shows = new ArrayList<>();
 
             if (type == AddPagerAdapter.TRENDING_TAB_POSITION) {
                 try {
@@ -198,6 +202,11 @@ public class TraktAddFragment extends AddFragment {
                 } catch (RetrofitError e) {
                     // we don't care
                 }
+            }
+
+            // return empty list right away if there are no results
+            if (shows == null || shows.size() == 0) {
+                return showList;
             }
 
             // get a list of existing shows to filter against
