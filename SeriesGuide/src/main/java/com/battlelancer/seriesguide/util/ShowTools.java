1b62b92c598bbc2a6ee4615ea3c64ec0c32a9148
Uwe Trottmann <uwe.trottmann@gmail.com>
2013-12-07 12:29:55 +0100

    Initial version of ShowTools, helps upload show prop changes.

---
+++ SeriesGuide/src/main/java/com/battlelancer/seriesguide/util/ShowTools.java
@@ -0,0 +1,119 @@
+package com.battlelancer.seriesguide.util;
+
+import com.google.api.client.extensions.android.http.AndroidHttp;
+import com.google.api.client.googleapis.extensions.android.gms.auth.GoogleAccountCredential;
+import com.google.api.client.json.jackson.JacksonFactory;
+
+import com.battlelancer.seriesguide.backend.CloudEndpointUtils;
+import com.battlelancer.seriesguide.backend.settings.HexagonSettings;
+import com.battlelancer.seriesguide.provider.SeriesContract;
+import com.uwetrottmann.seriesguide.shows.Shows;
+import com.uwetrottmann.seriesguide.shows.model.Show;
+import com.uwetrottmann.seriesguide.shows.model.ShowList;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.os.AsyncTask;
+import android.util.Log;
+
+import java.io.IOException;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * Common activities and tools useful when interacting with shows.
+ */
+public class ShowTools {
+
+    private static ShowTools _instance;
+
+    private final Context mContext;
+
+    private final GoogleAccountCredential mCredential;
+
+    private final Shows mShowsService;
+
+    public static synchronized ShowTools get(Context context) {
+        if (_instance == null) {
+            _instance = new ShowTools(context);
+        }
+        return _instance;
+    }
+
+    private ShowTools(Context context) {
+        mContext = context;
+
+        // get registered Google account
+        mCredential = GoogleAccountCredential.usingAudience(context, HexagonSettings.AUDIENCE);
+        setShowsServiceAccountName(HexagonSettings.getAccountName(context));
+
+        // build show service endpoint
+        Shows.Builder builder = new Shows.Builder(
+                AndroidHttp.newCompatibleTransport(), new JacksonFactory(), mCredential
+        );
+        mShowsService = CloudEndpointUtils.updateBuilder(builder).build();
+    }
+
+    public void setShowsServiceAccountName(String accountName) {
+        mCredential.setSelectedAccountName(accountName);
+    }
+
+    public void storeIsFavorite(int showTvdbId, boolean isFavorite) {
+        // save to local database
+        ContentValues values = new ContentValues();
+        values.put(SeriesContract.Shows.FAVORITE, isFavorite);
+        mContext.getContentResolver().update(
+                SeriesContract.Shows.buildShowUri(showTvdbId), values, null, null);
+
+        if (isSignedIn()) {
+            // send to cloud
+            storeIsFavoriteRemote(showTvdbId, isFavorite);
+        }
+    }
+
+    private void storeIsFavoriteRemote(int showTvdbId, boolean isFavorite) {
+        Show show = new Show();
+        show.setTvdbId(showTvdbId);
+        show.setIsFavorite(isFavorite);
+
+        List<Show> shows = new LinkedList<>();
+        shows.add(show);
+
+        new ShowsUploadTask(mShowsService, shows).execute();
+    }
+
+    private boolean isSignedIn() {
+        return mCredential.getSelectedAccountName() != null;
+    }
+
+    private static class ShowsUploadTask extends AsyncTask<Void, Void, Void> {
+
+        private static final String TAG = "Show Upload";
+
+        private final Shows mShowsService;
+
+        private final List<Show> mShows;
+
+        public ShowsUploadTask(Shows showsService, List<Show> shows) {
+            mShowsService = showsService;
+            mShows = shows;
+        }
+
+        @Override
+        protected Void doInBackground(Void... params) {
+            ShowList showList = new ShowList();
+            showList.setShows(mShows);
+
+            // upload shows
+            try {
+                mShowsService.save(showList).execute();
+            } catch (IOException e) {
+                Log.w(TAG, e.getMessage(), e);
+            }
+
+            return null;
+        }
+
+    }
+
+}
