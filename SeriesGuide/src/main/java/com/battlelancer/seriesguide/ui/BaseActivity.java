f0c61346d2ccea58cedad1779713a298cad76a17
Uwe Trottmann <uwe.trottmann@gmail.com>
2013-12-20 13:47:33 +0100

    Drop unused methods.

--- SeriesGuide/src/main/java/com/battlelancer/seriesguide/ui/BaseActivity.java
+++ SeriesGuide/src/main/java/com/battlelancer/seriesguide/ui/BaseActivity.java
@@ -29,7 +29,6 @@ import com.battlelancer.seriesguide.util.TraktTask.OnTraktActionCompleteListener
 import com.uwetrottmann.seriesguide.R;
 
 import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
@@ -144,45 +143,4 @@ public abstract class BaseActivity extends SherlockFragmentActivity implements
         }
     }
 
-    /**
-     * Converts an intent into a {@link Bundle} suitable for use as fragment
-     * arguments.
-     */
-    public static Bundle intentToFragmentArguments(Intent intent) {
-        Bundle arguments = new Bundle();
-        if (intent == null) {
-            return arguments;
-        }
-
-        final Uri data = intent.getData();
-        if (data != null) {
-            arguments.putParcelable("_uri", data);
-        }
-
-        final Bundle extras = intent.getExtras();
-        if (extras != null) {
-            arguments.putAll(intent.getExtras());
-        }
-
-        return arguments;
-    }
-
-    /**
-     * Converts a fragment arguments bundle into an intent.
-     */
-    public static Intent fragmentArgumentsToIntent(Bundle arguments) {
-        Intent intent = new Intent();
-        if (arguments == null) {
-            return intent;
-        }
-
-        final Uri data = arguments.getParcelable("_uri");
-        if (data != null) {
-            intent.setData(data);
-        }
-
-        intent.putExtras(arguments);
-        intent.removeExtra("_uri");
-        return intent;
-    }
 }
