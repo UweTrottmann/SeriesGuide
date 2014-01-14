585c39c0a380ba00922b21a8142f13d5d9809f9a
Uwe Trottmann <uwe.trottmann@gmail.com>
2014-01-14 09:43:56 +0100

    Format and organize imports.

--- SeriesGuide/src/main/java/com/battlelancer/seriesguide/provider/SeriesGuideContract.java
+++ SeriesGuide/src/main/java/com/battlelancer/seriesguide/provider/SeriesGuideContract.java
@@ -16,17 +16,20 @@
 
 package com.battlelancer.seriesguide.provider;
 
-import android.net.Uri;
-import android.provider.BaseColumns;
-
 import com.battlelancer.seriesguide.SeriesGuideApplication;
 import com.battlelancer.seriesguide.util.ParserUtils;
 import com.battlelancer.seriesguide.util.Utils;
 
+import android.net.Uri;
+import android.provider.BaseColumns;
+
 public class SeriesGuideContract {
 
     interface ShowsColumns {
-        /** This column is NOT in this table, it is for reference purposes only. */
+
+        /**
+         * This column is NOT in this table, it is for reference purposes only.
+         */
         String REF_SHOW_ID = "series_id";
 
         String TITLE = "seriestitle";
@@ -41,23 +44,28 @@ public class SeriesGuideContract {
 
         String RUNTIME = "runtime";
 
-        /** Rating value of TVDb as double ranging from 0.0 to 10.0. */
+        /**
+         * Rating value of TVDb as double ranging from 0.0 to 10.0.
+         */
         String RATING = "rating";
 
         String NETWORK = "network";
 
         String GENRES = "genres";
 
-        /** Air date of first episode, e.g. 2009-01-25. */
+        /**
+         * Air date of first episode, e.g. 2009-01-25.
+         */
         String FIRSTAIRED = "firstaired";
 
         /**
-         * Air time (e.g. 20:00 PM) in ms as parsed by {@link Utils}
-         * .parseTimeToMilliseconds().
+         * Air time (e.g. 20:00 PM) in ms as parsed by {@link Utils} .parseTimeToMilliseconds().
          */
         String AIRSTIME = "airstime";
 
-        /** Added in db version 21 to store the air time in pure text. */
+        /**
+         * Added in db version 21 to store the air time in pure text.
+         */
         String AIRTIME = "series_airtime";
 
         String AIRSDAYOFWEEK = "airsdayofweek";
@@ -66,92 +74,128 @@ public class SeriesGuideContract {
 
         String IMDBID = "imdbid";
 
-        /** Whether this show has been favorited. */
+        /**
+         * Whether this show has been favorited.
+         */
         String FAVORITE = "series_favorite";
 
-        /** Whether this show has been hidden. Added in db version 23. */
+        /**
+         * Whether this show has been hidden. Added in db version 23.
+         */
         String HIDDEN = "series_hidden";
 
-        /** Whether this show is included in manual trakt upload. */
+        /**
+         * Whether this show is included in manual trakt upload.
+         */
         String SYNCENABLED = "series_syncenabled";
 
-        /** Next episode ID. */
+        /**
+         * Next episode ID.
+         */
         String NEXTEPISODE = "next";
 
-        /** Next episode text, e.g. '0x12 Episode Name'. */
+        /**
+         * Next episode text, e.g. '0x12 Episode Name'.
+         */
         String NEXTTEXT = "nexttext";
 
-        /** DEPRECATED. Use {@link NEXTAIRDATEMS} instead. */
+        /**
+         * DEPRECATED. Use {@link #NEXTAIRDATEMS} instead.
+         */
         String NEXTAIRDATE = "nextairdate";
 
-        /** Added in db version 25 to allow correct sorting by next air date. */
+        /**
+         * Added in db version 25 to allow correct sorting by next air date.
+         */
         String NEXTAIRDATEMS = "series_nextairdate";
 
-        /** Next air date text, e.g. 'Apr 2 (Mon)'. */
+        /**
+         * Next air date text, e.g. 'Apr 2 (Mon)'.
+         */
         String NEXTAIRDATETEXT = "series_nextairdatetext";
 
-        /** Added in db version 22 to store the last time a show was updated. */
+        /**
+         * Added in db version 22 to store the last time a show was updated.
+         */
         String LASTUPDATED = "series_lastupdate";
 
         /**
-         * Last time show was edited on theTVDb.com (lastupdated field). Added
-         * in db version 27.
+         * Last time show was edited on theTVDb.com (lastupdated field). Added in db version 27.
          */
         String LASTEDIT = "series_lastedit";
 
         /**
-         * GetGlue object id, added in version 29 to support checking into shows
-         * without IMDb id.
+         * GetGlue object id, added in version 29 to support checking into shows without IMDb id.
          */
         String GETGLUEID = "series_getglueid";
 
         /**
-         * Id of the last watched episode, used to calculate the next episode to
-         * watch. Added in db version 31.
+         * Id of the last watched episode, used to calculate the next episode to watch. Added in db
+         * version 31.
          */
         String LASTWATCHEDID = "series_lastwatchedid";
 
     }
 
     interface SeasonsColumns {
-        /** This column is NOT in this table, it is for reference purposes only. */
+
+        /**
+         * This column is NOT in this table, it is for reference purposes only.
+         */
         String REF_SEASON_ID = "season_id";
 
-        /** The number of a season. Starting from 0 for Special Episodes. */
+        /**
+         * The number of a season. Starting from 0 for Special Episodes.
+         */
         String COMBINED = "combinednr";
 
-        /** Number of all episodes in a season. */
+        /**
+         * Number of all episodes in a season.
+         */
         String TOTALCOUNT = "season_totalcount";
 
-        /** Number of unwatched, aired episodes. */
+        /**
+         * Number of unwatched, aired episodes.
+         */
         String WATCHCOUNT = "watchcount";
 
-        /** Number of unwatched, future episodes (not aired yet). */
+        /**
+         * Number of unwatched, future episodes (not aired yet).
+         */
         String UNAIREDCOUNT = "willaircount";
 
-        /** Number of unwatched episodes with no air date. */
+        /**
+         * Number of unwatched episodes with no air date.
+         */
         String NOAIRDATECOUNT = "noairdatecount";
 
-        /** Text tags for this season. <em>Repurposed.</em>. */
+        /**
+         * Text tags for this season. <em>Repurposed.</em>.
+         */
         String TAGS = "seasonposter";
     }
 
     interface EpisodesColumns {
-        /** Season number. A reference to the season id is stored separately. */
+
+        /**
+         * Season number. A reference to the season id is stored separately.
+         */
         String SEASON = "season";
 
-        /** Number of an episode within its season. */
+        /**
+         * Number of an episode within its season.
+         */
         String NUMBER = "episodenumber";
 
         /**
-         * Sometimes episodes are ordered differently when released on DVD. Uses
-         * decimal point notation, e.g. 1.0, 1.5.
+         * Sometimes episodes are ordered differently when released on DVD. Uses decimal point
+         * notation, e.g. 1.0, 1.5.
          */
         String DVDNUMBER = "dvdnumber";
 
         /**
-         * Some shows, mainly anime, use absolute episode numbers instead of the
-         * season/episode grouping. Added in db version 30.
+         * Some shows, mainly anime, use absolute episode numbers instead of the season/episode
+         * grouping. Added in db version 30.
          */
         String ABSOLUTE_NUMBER = "absolute_number";
 
@@ -167,33 +211,45 @@ public class SeriesGuideContract {
 
         String DIRECTORS = "directors";
 
-        /** Rating value of TVDb as double ranging from 0.0 to 10.0. */
+        /**
+         * Rating value of TVDb as double ranging from 0.0 to 10.0.
+         */
         String RATING = "rating";
 
-        /** First aired date in text as given by TVDb.com */
+        /**
+         * First aired date in text as given by TVDb.com
+         */
         String FIRSTAIRED = "epfirstaired";
 
-        /** First aired date in ms. */
+        /**
+         * First aired date in ms.
+         */
         String FIRSTAIREDMS = "episode_firstairedms";
 
-        /** Whether an episode has been watched. */
+        /**
+         * Whether an episode has been watched.
+         */
         String WATCHED = "watched";
 
-        /** Whether an episode has been collected in digital, physical form. */
+        /**
+         * Whether an episode has been collected in digital, physical form.
+         */
         String COLLECTED = "episode_collected";
 
-        /** IMDb id for a single episode. Added in db version 27. */
+        /**
+         * IMDb id for a single episode. Added in db version 27.
+         */
         String IMDBID = "episode_imdbid";
 
         /**
-         * Last time episode was edited on theTVDb.com (lastupdated field).
-         * Added in db version 27.
+         * Last time episode was edited on theTVDb.com (lastupdated field). Added in db version 27.
          */
         String LAST_EDITED = "episode_lastedit";
 
     }
 
     interface EpisodeSearchColumns {
+
         String _DOCID = "docid";
 
         String TITLE = Episodes.TITLE;
@@ -202,19 +258,24 @@ public class SeriesGuideContract {
     }
 
     interface ListsColumns {
-        /** Unique string identifier. */
+
+        /**
+         * Unique string identifier.
+         */
         String LIST_ID = "list_id";
 
         String NAME = "list_name";
     }
 
     interface ListItemsColumns {
-        /** Unique string identifier. */
+
+        /**
+         * Unique string identifier.
+         */
         String LIST_ITEM_ID = "list_item_id";
 
         /**
-         * NON-unique string identifier referencing internal ids of other
-         * tables.
+         * NON-unique string identifier referencing internal ids of other tables.
          */
         String ITEM_REF_ID = "item_ref_id";
 
@@ -225,6 +286,7 @@ public class SeriesGuideContract {
     }
 
     public interface ListItemTypes {
+
         int SHOW = 1;
         int SEASON = 2;
         int EPISODE = 3;
@@ -298,6 +360,7 @@ public class SeriesGuideContract {
     public static final String PATH_WITH_EPISODE = "with_episode";
 
     public static class Shows implements ShowsColumns, BaseColumns {
+
         public static final Uri CONTENT_URI = BASE_CONTENT_URI.buildUpon().appendPath(PATH_SHOWS)
                 .build();
 
@@ -308,13 +371,20 @@ public class SeriesGuideContract {
 
         public static final Uri CONTENT_FILTER_URI = Uri.withAppendedPath(CONTENT_URI, "filter");
 
-        /** Use if multiple items get returned */
+        /**
+         * Use if multiple items get returned
+         */
         public static final String CONTENT_TYPE = "vnd.android.cursor.dir/vnd.seriesguide.show";
 
-        /** Use if a single item is returned */
-        public static final String CONTENT_ITEM_TYPE = "vnd.android.cursor.item/vnd.seriesguide.show";
+        /**
+         * Use if a single item is returned
+         */
+        public static final String CONTENT_ITEM_TYPE
+                = "vnd.android.cursor.item/vnd.seriesguide.show";
 
-        /** Default "ORDER BY" clause. */
+        /**
+         * Default "ORDER BY" clause.
+         */
         public static final String DEFAULT_SORT = ShowsColumns.TITLE + " ASC";
 
         public static final String SELECTION_FAVORITES = " AND " + Shows.FAVORITE + "=1";
@@ -336,17 +406,23 @@ public class SeriesGuideContract {
     }
 
     public static class Episodes implements EpisodesColumns, BaseColumns {
+
         public static final Uri CONTENT_URI = BASE_CONTENT_URI.buildUpon()
                 .appendPath(PATH_EPISODES).build();
 
         public static final Uri CONTENT_URI_WITHSHOW = CONTENT_URI.buildUpon()
                 .appendPath(PATH_WITHSHOW).build();
 
-        /** Use if multiple items get returned */
+        /**
+         * Use if multiple items get returned
+         */
         public static final String CONTENT_TYPE = "vnd.android.cursor.dir/vnd.seriesguide.episode";
 
-        /** Use if a single item is returned */
-        public static final String CONTENT_ITEM_TYPE = "vnd.android.cursor.item/vnd.seriesguide.episode";
+        /**
+         * Use if a single item is returned
+         */
+        public static final String CONTENT_ITEM_TYPE
+                = "vnd.android.cursor.item/vnd.seriesguide.episode";
 
         public static final String SELECTION_NOWATCHED = " AND " + Episodes.WATCHED + "=0";
 
@@ -391,14 +467,20 @@ public class SeriesGuideContract {
     }
 
     public static class Seasons implements SeasonsColumns, BaseColumns {
+
         public static final Uri CONTENT_URI = BASE_CONTENT_URI.buildUpon().appendPath(PATH_SEASONS)
                 .build();
 
-        /** Use if multiple items get returned */
+        /**
+         * Use if multiple items get returned
+         */
         public static final String CONTENT_TYPE = "vnd.android.cursor.dir/vnd.seriesguide.season";
 
-        /** Use if a single item is returned */
-        public static final String CONTENT_ITEM_TYPE = "vnd.android.cursor.item/vnd.seriesguide.season";
+        /**
+         * Use if a single item is returned
+         */
+        public static final String CONTENT_ITEM_TYPE
+                = "vnd.android.cursor.item/vnd.seriesguide.season";
 
         public static Uri buildSeasonUri(String seasonId) {
             return CONTENT_URI.buildUpon().appendPath(seasonId).build();
@@ -418,6 +500,7 @@ public class SeriesGuideContract {
     }
 
     public static class EpisodeSearch implements EpisodeSearchColumns {
+
         public static final Uri CONTENT_URI = BASE_CONTENT_URI.buildUpon()
                 .appendPath(PATH_EPISODESEARCH).build();
 
@@ -437,6 +520,7 @@ public class SeriesGuideContract {
     }
 
     public static class Lists implements ListsColumns, BaseColumns {
+
         public static final Uri CONTENT_URI = BASE_CONTENT_URI.buildUpon().appendPath(PATH_LISTS)
                 .build();
 
@@ -444,11 +528,16 @@ public class SeriesGuideContract {
                 .appendPath(PATH_WITH_LIST_ITEM_ID)
                 .build();
 
-        /** Use if multiple items get returned */
+        /**
+         * Use if multiple items get returned
+         */
         public static final String CONTENT_TYPE = "vnd.android.cursor.dir/vnd.seriesguide.list";
 
-        /** Use if a single item is returned */
-        public static final String CONTENT_ITEM_TYPE = "vnd.android.cursor.item/vnd.seriesguide.list";
+        /**
+         * Use if a single item is returned
+         */
+        public static final String CONTENT_ITEM_TYPE
+                = "vnd.android.cursor.item/vnd.seriesguide.list";
 
         public static Uri buildListUri(String id) {
             return CONTENT_URI.buildUpon().appendPath(id).build();
@@ -468,6 +557,7 @@ public class SeriesGuideContract {
     }
 
     public static class ListItems implements ListItemsColumns, BaseColumns {
+
         public static final Uri CONTENT_URI = BASE_CONTENT_URI.buildUpon()
                 .appendPath(PATH_LIST_ITEMS)
                 .build();
@@ -476,11 +566,16 @@ public class SeriesGuideContract {
                 .appendPath(PATH_WITH_DETAILS)
                 .build();
 
-        /** Use if multiple items get returned */
+        /**
+         * Use if multiple items get returned
+         */
         public static final String CONTENT_TYPE = "vnd.android.cursor.dir/vnd.seriesguide.listitem";
 
-        /** Use if a single item is returned */
-        public static final String CONTENT_ITEM_TYPE = "vnd.android.cursor.item/vnd.seriesguide.listitem";
+        /**
+         * Use if a single item is returned
+         */
+        public static final String CONTENT_ITEM_TYPE
+                = "vnd.android.cursor.item/vnd.seriesguide.listitem";
 
         public static Uri buildListItemUri(String id) {
             return CONTENT_URI.buildUpon().appendPath(id).build();
