d6fd6dd9c76505a5179776f18341589bd5eb42b8
Uwe Trottmann <uwe.trottmann@gmail.com>
2014-03-06 14:27:24 +0100

    Rename ExtensionData to just Action.

--- api/src/main/java/com/battlelancer/seriesguide/api/Action.java
+++ api/src/main/java/com/battlelancer/seriesguide/api/Action.java
@@ -18,18 +18,18 @@ package com.battlelancer.seriesguide.api;
 
 import android.content.Intent;
 
-public class ExtensionData {
+public class Action {
 
     private String mTitle;
     private Intent mViewIntent;
 
-    private ExtensionData(String title, Intent viewIntent) {
+    private Action(String title, Intent viewIntent) {
         mTitle = title;
         mViewIntent = viewIntent;
     }
 
     /**
-     * Returns the extension's user-visible title.
+     * Returns the user-visible title for this action.
      */
     public String getTitle() {
         return mTitle;
@@ -37,7 +37,7 @@ public class ExtensionData {
 
     /**
      * Returns the activity {@link Intent} that will be started when the user clicks on the button
-     * for this extension.
+     * for this action.
      */
     public Intent getViewIntent() {
         return mViewIntent;
@@ -49,8 +49,8 @@ public class ExtensionData {
         private Intent mViewIntent;
 
         /**
-         * Sets the user-visible title of the button for this extension, creates a builder
-         * instance.
+         * Sets the user-visible title of the button for this action, returns a builder
+         * instance to add further properties.
          */
         public Builder(String title) {
             if (title == null || title.length() == 0) {
@@ -62,7 +62,7 @@ public class ExtensionData {
         /**
          * Sets the (optional) activity {@link Intent} that will be {@linkplain
          * android.content.Context#startActivity(Intent)
-         * started} when the user clicks the button for your extension.<br/>
+         * started} when the user clicks the button for the action.<br/>
          * <br/>
          * The activity that this intent resolves to must have <code>android:exported</code> set
          * to <code>true</code>.
@@ -72,8 +72,8 @@ public class ExtensionData {
             return this;
         }
 
-        public ExtensionData build() {
-            return new ExtensionData(mTitle, mViewIntent);
+        public Action build() {
+            return new Action(mTitle, mViewIntent);
         }
     }
 }
