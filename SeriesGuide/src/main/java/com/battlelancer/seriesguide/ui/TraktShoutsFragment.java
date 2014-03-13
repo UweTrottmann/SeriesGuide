72c2113dff2d1ea8fe7834bc06b446e8953b8a3c
Uwe Trottmann <uwe.trottmann@gmail.com>
2014-03-13 08:03:36 +0100

    Do not auto refresh comments.
    - https://github.com/UweTrottmann/SeriesGuide/issues/255
    - Also remove auto refresh notice from layout, drop RTL layout.

--- SeriesGuide/src/main/java/com/battlelancer/seriesguide/ui/TraktShoutsFragment.java
+++ SeriesGuide/src/main/java/com/battlelancer/seriesguide/ui/TraktShoutsFragment.java
@@ -24,7 +24,6 @@ import android.support.v4.app.ListFragment;
 import android.support.v4.app.LoaderManager.LoaderCallbacks;
 import android.support.v4.content.Loader;
 import android.text.TextUtils;
-import android.text.format.DateUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -34,7 +33,6 @@ import android.widget.AdapterView;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.ImageButton;
-import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 import butterknife.ButterKnife;
@@ -227,17 +225,8 @@ public class TraktShoutsFragment extends SherlockFragment implements
         } else {
             setListShown(false);
             getLoaderManager().initLoader(0, getArguments(), this);
-            mHandler.postDelayed(mUpdateShoutsRunnable, DateUtils.MINUTE_IN_MILLIS);
         }
     }
-
-    private Runnable mUpdateShoutsRunnable = new Runnable() {
-        @Override
-        public void run() {
-            getLoaderManager().restartLoader(0, getArguments(), TraktShoutsFragment.this);
-            mHandler.postDelayed(mUpdateShoutsRunnable, DateUtils.MINUTE_IN_MILLIS);
-        }
-    };
 
     @Override
     public void onResume() {
@@ -259,7 +248,6 @@ public class TraktShoutsFragment extends SherlockFragment implements
     @Override
     public void onDestroyView() {
         mHandler.removeCallbacks(mRequestFocus);
-        mHandler.removeCallbacks(mUpdateShoutsRunnable);
         mList = null;
         mListShown = false;
         mEmptyView = mProgressContainer = mListContainer = null;
@@ -335,15 +323,8 @@ public class TraktShoutsFragment extends SherlockFragment implements
 
     /**
      * Control whether the list is being displayed. You can make it not displayed if you are
-     * waiting
-     * for the initial data to show in it. During this time an indeterminant progress indicator
-     * will
-     * be shown instead. <p> Applications do not normally need to use this themselves. The default
-     * behavior of ListFragment is to start with the list not being shown, only showing it once an
-     * adapter is given with {@link #setListAdapter(ListAdapter)}. If the list at that point had
-     * not
-     * been shown, when it does get shown it will be do without the user ever seeing the hidden
-     * state.
+     * waiting for the initial data to show in it. During this time an indeterminant progress
+     * indicator will be shown instead.
      *
      * @param shown If true, the list view is shown; if false, the progress indicator. The initial
      *              value is true.
@@ -432,11 +413,13 @@ public class TraktShoutsFragment extends SherlockFragment implements
                 if (rawListView == null) {
                     throw new RuntimeException(
                             "Your content must have a ListView whose id attribute is "
-                                    + "'android.R.id.list'");
+                                    + "'android.R.id.list'"
+                    );
                 }
                 throw new RuntimeException(
                         "Content has view with id attribute 'android.R.id.list' "
-                                + "that is not a ListView class");
+                                + "that is not a ListView class"
+                );
             }
             mList = (ListView) rawListView;
             if (mEmptyView != null) {
