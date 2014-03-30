c700868fa248a53bde840b93247258355fbd67b4
Uwe Trottmann <uwe.trottmann@gmail.com>
2014-03-30 13:32:47 +0200

    Support building show view intents from API as well.

--- api/src/main/java/com/battlelancer/seriesguide/api/Intents.java
+++ api/src/main/java/com/battlelancer/seriesguide/api/Intents.java
@@ -18,12 +18,18 @@ package com.battlelancer.seriesguide.api;
 
 import android.content.Intent;
 
+/**
+ * Helper methods to view shows or episodes within SeriesGuide.
+ */
 public class Intents {
 
     public static final String ACTION_VIEW_EPISODE
             = "com.battlelancer.seriesguide.api.action.VIEW_EPISODE";
 
-    public static final String EXTRA_EPISODE_TVDBID = "episode_tvdbid";
+    public static final String ACTION_VIEW_SHOW
+            = "com.battlelancer.seriesguide.api.action.VIEW_SHOW";
+
+    public static final String EXTRA_TVDBID = "tvdbid";
 
     /**
      * Builds an implicit {@link android.content.Intent} to view an episode in SeriesGuide. Make
@@ -32,6 +38,16 @@ public class Intents {
      */
     public static Intent buildViewEpisodeIntent(int episodeTvdbId) {
         return new Intent(ACTION_VIEW_EPISODE)
-                .putExtra(EXTRA_EPISODE_TVDBID, episodeTvdbId);
+                .putExtra(EXTRA_TVDBID, episodeTvdbId);
+    }
+
+    /**
+     * Builds an implicit {@link android.content.Intent} to view an episode in SeriesGuide. Make
+     * sure to check with {@link Intent#resolveActivity(android.content.pm.PackageManager)} if
+     * SeriesGuide (or another app capable of handling this intent) is available.
+     */
+    public static Intent buildViewShowIntent(int showTvdbId) {
+        return new Intent(ACTION_VIEW_SHOW)
+                .putExtra(EXTRA_TVDBID, showTvdbId);
     }
 }
