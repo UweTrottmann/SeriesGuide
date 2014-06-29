92f3b6f75a4d36f9a3df8678a639fc0047daa163
Uwe Trottmann <uwe.trottmann@gmail.com>
2014-06-29 10:43:13 +0200

    Rename list_fragment layout to fragment_episodes.
    - More appropriate name and consistency!

--- SeriesGuide/src/main/java/com/battlelancer/seriesguide/ui/EpisodesFragment.java
+++ SeriesGuide/src/main/java/com/battlelancer/seriesguide/ui/EpisodesFragment.java
@@ -98,7 +98,7 @@ public class EpisodesFragment extends ListFragment
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.list_fragment, container, false);
+        return inflater.inflate(R.layout.fragment_episodes, container, false);
     }
 
     @Override
