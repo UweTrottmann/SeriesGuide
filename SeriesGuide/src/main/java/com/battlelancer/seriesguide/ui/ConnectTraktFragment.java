ae38dd8f07e6c13717b89940c811ff5c804893dd
Uwe Trottmann <uwe.trottmann@gmail.com>
2014-06-29 10:49:05 +0200

    Renamed trakt connect fragment layouts for consistency.
    - Also dropped unused RTL layout.

--- SeriesGuide/src/main/java/com/battlelancer/seriesguide/ui/ConnectTraktFragment.java
+++ SeriesGuide/src/main/java/com/battlelancer/seriesguide/ui/ConnectTraktFragment.java
@@ -37,7 +37,7 @@ public class ConnectTraktFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.connect_trakt_fragment, container, false);
+        return inflater.inflate(R.layout.fragment_connect_trakt_info, container, false);
     }
 
     @Override
