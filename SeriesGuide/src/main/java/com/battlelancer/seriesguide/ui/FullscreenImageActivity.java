820423e4f2dc7b90c0b4933d5638425b2600a482
Uwe Trottmann <uwe.trottmann@gmail.com>
2014-07-06 15:01:39 +0200

    Fix up animation of FullscreenImageActivity.

--- SeriesGuide/src/main/java/com/battlelancer/seriesguide/ui/FullscreenImageActivity.java
+++ SeriesGuide/src/main/java/com/battlelancer/seriesguide/ui/FullscreenImageActivity.java
@@ -169,7 +169,7 @@ public class FullscreenImageActivity extends FragmentActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
             case android.R.id.home:
-                super.onBackPressed();
+                onBackPressed();
                 return true;
         }
         return super.onOptionsItemSelected(item);
