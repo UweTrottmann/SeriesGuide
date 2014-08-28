e37445fadfae0257b63237da8b8eb54cf79581cb
Uwe Trottmann <uwe.trottmann@gmail.com>
2014-08-28 12:03:39 +0200

    Enable show search, not pretty, yet. But works.

--- SeriesGuide/src/main/java/com/battlelancer/seriesguide/ui/ShowSearchFragment.java
+++ SeriesGuide/src/main/java/com/battlelancer/seriesguide/ui/ShowSearchFragment.java
@@ -16,17 +16,33 @@
 
 package com.battlelancer.seriesguide.ui;
 
+import android.app.SearchManager;
+import android.content.Context;
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.ActivityOptionsCompat;
 import android.support.v4.app.ListFragment;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.CursorLoader;
+import android.support.v4.content.Loader;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 import com.battlelancer.seriesguide.R;
+import com.battlelancer.seriesguide.adapters.BaseShowsAdapter;
+import com.battlelancer.seriesguide.provider.SeriesGuideContract;
+import de.greenrobot.event.EventBus;
 
 /**
  * Displays show search results.
  */
 public class ShowSearchFragment extends ListFragment {
+
+    private ShowResultsAdapter adapter;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -34,4 +50,101 @@ public class ShowSearchFragment extends ListFragment {
         return inflater.inflate(R.layout.fragment_search, container, false);
     }
 
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        adapter = new ShowResultsAdapter(getActivity(), null, 0);
+        setListAdapter(adapter);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        EventBus.getDefault().register(this);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        EventBus.getDefault().unregister(this);
+    }
+
+    @Override
+    public void onListItemClick(ListView l, View v, int position, long id) {
+        Intent i = new Intent(getActivity(), OverviewActivity.class);
+        i.putExtra(OverviewFragment.InitBundle.SHOW_TVDBID, (int) id);
+
+        ActivityCompat.startActivity(getActivity(), i,
+                ActivityOptionsCompat.makeScaleUpAnimation(v, 0, 0, v.getWidth(), v.getHeight())
+                        .toBundle());
+    }
+
+    public void onEventMainThread(SearchActivity.SearchQueryEvent event) {
+        search(event.args);
+    }
+
+    public void search(Bundle args) {
+        getLoaderManager().restartLoader(SearchActivity.SHOWS_LOADER_ID, args,
+                searchLoaderCallbacks);
+    }
+
+    private LoaderManager.LoaderCallbacks<Cursor>
+            searchLoaderCallbacks = new LoaderManager.LoaderCallbacks<Cursor>() {
+        @Override
+        public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+            String query = args.getString(SearchManager.QUERY);
+            Uri uri = Uri.withAppendedPath(SeriesGuideContract.Shows.CONTENT_FILTER_URI,
+                    Uri.encode(query));
+
+            return new CursorLoader(getActivity(), uri, SearchQuery.PROJECTION, null, null, null);
+        }
+
+        @Override
+        public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+            adapter.swapCursor(data);
+        }
+
+        @Override
+        public void onLoaderReset(Loader<Cursor> loader) {
+            adapter.swapCursor(null);
+        }
+    };
+
+    private static class ShowResultsAdapter extends BaseShowsAdapter {
+
+        public ShowResultsAdapter(Context context, Cursor c, int flags) {
+            super(context, c, flags);
+        }
+
+        @Override
+        public void bindView(View view, Context context, Cursor cursor) {
+            BaseShowsAdapter.ViewHolder viewHolder = (BaseShowsAdapter.ViewHolder) view.getTag();
+
+            viewHolder.name.setText(cursor.getString(SearchQuery.TITLE));
+        }
+
+        @Override
+        public View newView(Context context, Cursor cursor, ViewGroup parent) {
+            View v = super.newView(context, cursor, parent);
+
+            ViewHolder viewHolder = (ViewHolder) v.getTag();
+            viewHolder.favorited.setBackgroundResource(0); // remove selectable background
+            viewHolder.contextMenu.setVisibility(View.GONE);
+
+            return v;
+        }
+    }
+
+    private interface SearchQuery {
+        String[] PROJECTION = new String[] {
+                SeriesGuideContract.Shows._ID,
+                SeriesGuideContract.Shows.TITLE
+        };
+
+        int ID = 0;
+        int TITLE = 1;
+    }
 }
