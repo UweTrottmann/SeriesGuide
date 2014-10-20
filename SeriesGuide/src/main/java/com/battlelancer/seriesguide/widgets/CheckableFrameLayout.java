f03cec25f2989f69c9006fe5dc54767c38842af7
Uwe Trottmann <uwe.trottmann@gmail.com>
2014-10-20 19:06:03 +0200

    Use neutral activated state for episode dual pane view.

--- SeriesGuide/src/main/java/com/battlelancer/seriesguide/widgets/CheckableFrameLayout.java
+++ SeriesGuide/src/main/java/com/battlelancer/seriesguide/widgets/CheckableFrameLayout.java
@@ -36,11 +36,7 @@ public class CheckableFrameLayout extends FrameLayout implements Checkable {
 
     public void setChecked(boolean checked) {
         mChecked = checked;
-        if (SeriesGuidePreferences.THEME == R.style.Theme_SeriesGuide_DarkBlue) {
-            setBackgroundResource(checked ? R.drawable.list_pressed_holo_dark : 0);
-        } else {
-            setBackgroundResource(checked ? R.drawable.list_pressed_sg : 0);
-        }
+        setBackgroundResource(checked ? R.drawable.list_pressed_holo_dark : 0);
     }
 
     public boolean isChecked() {
@@ -50,5 +46,4 @@ public class CheckableFrameLayout extends FrameLayout implements Checkable {
     public void toggle() {
         setChecked(!mChecked);
     }
-
 }
