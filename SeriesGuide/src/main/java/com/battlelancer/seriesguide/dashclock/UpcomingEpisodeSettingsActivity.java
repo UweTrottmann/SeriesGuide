ac40d28396de10692cef451210be9db2413e9b9c
Uwe Trottmann <uwe.trottmann@gmail.com>
2014-10-28 09:58:43 +0100

    Update dashclock settings activity with toolbar.
    - https://fabric.io/seriesguide/android/apps/com.battlelancer.seriesguide/issues/544e7886e3de5099ba24fab9

--- SeriesGuide/src/main/java/com/battlelancer/seriesguide/dashclock/UpcomingEpisodeSettingsActivity.java
+++ SeriesGuide/src/main/java/com/battlelancer/seriesguide/dashclock/UpcomingEpisodeSettingsActivity.java
@@ -17,44 +17,55 @@
 
 package com.battlelancer.seriesguide.dashclock;
 
-import android.annotation.TargetApi;
+import android.app.Fragment;
+import android.app.FragmentTransaction;
 import android.media.Ringtone;
 import android.media.RingtoneManager;
 import android.net.Uri;
-import android.os.Build;
 import android.os.Bundle;
 import android.preference.ListPreference;
 import android.preference.Preference;
-import android.preference.PreferenceActivity;
+import android.preference.PreferenceFragment;
 import android.preference.RingtonePreference;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.widget.Toolbar;
 import android.text.TextUtils;
 import android.view.MenuItem;
 import com.battlelancer.seriesguide.R;
 import com.battlelancer.seriesguide.settings.DashClockSettings;
+import com.battlelancer.seriesguide.ui.SeriesGuidePreferences;
 
 /**
- * Allow setting the threshold for upcoming episodes to appear in DashClock.
+ * Allows setting the threshold for upcoming episodes to appear in DashClock.
  */
-@TargetApi(Build.VERSION_CODES.ICE_CREAM_SANDWICH)
-public class UpcomingEpisodeSettingsActivity extends PreferenceActivity {
+public class UpcomingEpisodeSettingsActivity extends ActionBarActivity {
 
     public void onCreate(Bundle savedInstanceState) {
+        // set a theme based on user preference
+        setTheme(SeriesGuidePreferences.THEME);
+
         super.onCreate(savedInstanceState);
-        getActionBar().setIcon(R.drawable.ic_notification);
-        getActionBar().setDisplayHomeAsUpEnabled(true);
+        setContentView(R.layout.activity_singlepane);
+        setupActionBar();
+
+        if (savedInstanceState == null) {
+            Fragment f = new SettingsFragment();
+            FragmentTransaction ft = getFragmentManager().beginTransaction();
+            ft.add(R.id.content_frame, f);
+            ft.commit();
+        }
     }
 
-    @Override
-    protected void onPostCreate(Bundle savedInstanceState) {
-        super.onPostCreate(savedInstanceState);
-        setupSimplePreferencesScreen();
+    private void setupActionBar() {
+        Toolbar toolbar = (Toolbar) findViewById(R.id.sgToolbar);
+        setSupportActionBar(toolbar);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         if (item.getItemId() == android.R.id.home) {
-            // TODO: if the previous activity on the stack isn't a
-            // ConfigurationActivity, launch it.
             finish();
             return true;
         }
@@ -62,91 +73,89 @@ public class UpcomingEpisodeSettingsActivity extends PreferenceActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    @SuppressWarnings("deprecation")
-    private void setupSimplePreferencesScreen() {
-        // In the simplified UI, fragments are not used at all and we instead
-        // use the older PreferenceActivity APIs.
-
-        // Add 'general' preferences.
-        getPreferenceManager().setSharedPreferencesName(DashClockSettings.SETTINGS_FILE);
-        getPreferenceManager().setSharedPreferencesMode(0);
-        addPreferencesFromResource(R.xml.settings_dashclock);
-
-        // Bind the summaries of EditText/List/Dialog/Ringtone preferences to
-        // their values. When their values change, their summaries are updated
-        // to reflect the new value, per the Android Design guidelines.
-        bindPreferenceSummaryToValue(findPreference(DashClockSettings.KEY_DASHCLOCK_THRESHOLD));
-    }
-
-    /**
-     * A preference value change listener that updates the preference's summary
-     * to reflect its new value.
-     */
-    private static Preference.OnPreferenceChangeListener sBindPreferenceSummaryToValueListener = new Preference.OnPreferenceChangeListener() {
+    public static class SettingsFragment extends PreferenceFragment {
         @Override
-        public boolean onPreferenceChange(Preference preference, Object value) {
-            String stringValue = value.toString();
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
 
-            if (preference instanceof ListPreference) {
-                // For list preferences, look up the correct display value in
-                // the preference's 'entries' list.
-                ListPreference listPreference = (ListPreference) preference;
-                int index = listPreference.findIndexOfValue(stringValue);
+            // Add 'general' preferences.
+            getPreferenceManager().setSharedPreferencesName(DashClockSettings.SETTINGS_FILE);
+            getPreferenceManager().setSharedPreferencesMode(0);
+            addPreferencesFromResource(R.xml.settings_dashclock);
 
-                // Set the summary to reflect the new value.
-                preference.setSummary(
-                        index >= 0
-                                ? listPreference.getEntries()[index]
-                                : null);
-
-            } else if (preference instanceof RingtonePreference) {
-                // For ringtone preferences, look up the correct display value
-                // using RingtoneManager.
-                if (TextUtils.isEmpty(stringValue)) {
-                    // Empty values correspond to 'silent' (no ringtone).
-                    // preference.setSummary(R.string.pref_ringtone_silent);
-
-                } else {
-                    Ringtone ringtone = RingtoneManager.getRingtone(
-                            preference.getContext(), Uri.parse(stringValue));
-
-                    if (ringtone == null) {
-                        // Clear the summary if there was a lookup error.
-                        preference.setSummary(null);
-                    } else {
-                        // Set the summary to reflect the new ringtone display
-                        // name.
-                        String name = ringtone.getTitle(preference.getContext());
-                        preference.setSummary(name);
-                    }
-                }
-
-            } else {
-                // For all other preferences, set the summary to the value's
-                // simple string representation.
-                preference.setSummary(stringValue);
-            }
-            return true;
+            // Bind the summaries of EditText/List/Dialog/Ringtone preferences to
+            // their values. When their values change, their summaries are updated
+            // to reflect the new value, per the Android Design guidelines.
+            bindPreferenceSummaryToValue(findPreference(DashClockSettings.KEY_DASHCLOCK_THRESHOLD));
         }
-    };
 
-    /**
-     * Binds a preference's summary to its value. More specifically, when the
-     * preference's value is changed, its summary (line of text below the
-     * preference title) is updated to reflect the value. The summary is also
-     * immediately updated upon calling this method. The exact display format is
-     * dependent on the type of preference.
-     * 
-     * @see #sBindPreferenceSummaryToValueListener
-     */
-    private static void bindPreferenceSummaryToValue(Preference preference) {
-        // Set the listener to watch for value changes.
-        preference.setOnPreferenceChangeListener(sBindPreferenceSummaryToValueListener);
+        /**
+         * Binds a preference's summary to its value. More specifically, when the preference's value
+         * is changed, its summary (line of text below the preference title) is updated to reflect
+         * the value. The summary is also immediately updated upon calling this method. The exact
+         * display format is dependent on the type of preference.
+         *
+         * @see #sBindPreferenceSummaryToValueListener
+         */
+        private static void bindPreferenceSummaryToValue(Preference preference) {
+            // Set the listener to watch for value changes.
+            preference.setOnPreferenceChangeListener(sBindPreferenceSummaryToValueListener);
 
-        // Trigger the listener immediately with the preference's
-        // current value.
-        sBindPreferenceSummaryToValueListener.onPreferenceChange(preference,
-                preference.getContext().getSharedPreferences(DashClockSettings.SETTINGS_FILE, 0)
-                        .getString(preference.getKey(), ""));
+            // Trigger the listener immediately with the preference's
+            // current value.
+            sBindPreferenceSummaryToValueListener.onPreferenceChange(preference,
+                    preference.getContext().getSharedPreferences(DashClockSettings.SETTINGS_FILE, 0)
+                            .getString(preference.getKey(), ""));
+        }
+
+        /**
+         * A preference value change listener that updates the preference's summary to reflect its
+         * new value.
+         */
+        private static Preference.OnPreferenceChangeListener sBindPreferenceSummaryToValueListener
+                = new Preference.OnPreferenceChangeListener() {
+            @Override
+            public boolean onPreferenceChange(Preference preference, Object value) {
+                String stringValue = value.toString();
+
+                if (preference instanceof ListPreference) {
+                    // For list preferences, look up the correct display value in
+                    // the preference's 'entries' list.
+                    ListPreference listPreference = (ListPreference) preference;
+                    int index = listPreference.findIndexOfValue(stringValue);
+
+                    // Set the summary to reflect the new value.
+                    preference.setSummary(
+                            index >= 0
+                                    ? listPreference.getEntries()[index]
+                                    : null);
+                } else if (preference instanceof RingtonePreference) {
+                    // For ringtone preferences, look up the correct display value
+                    // using RingtoneManager.
+                    if (TextUtils.isEmpty(stringValue)) {
+                        // Empty values correspond to 'silent' (no ringtone).
+                        // preference.setSummary(R.string.pref_ringtone_silent);
+                    } else {
+                        Ringtone ringtone = RingtoneManager.getRingtone(
+                                preference.getContext(), Uri.parse(stringValue));
+
+                        if (ringtone == null) {
+                            // Clear the summary if there was a lookup error.
+                            preference.setSummary(null);
+                        } else {
+                            // Set the summary to reflect the new ringtone display
+                            // name.
+                            String name = ringtone.getTitle(preference.getContext());
+                            preference.setSummary(name);
+                        }
+                    }
+                } else {
+                    // For all other preferences, set the summary to the value's
+                    // simple string representation.
+                    preference.setSummary(stringValue);
+                }
+                return true;
+            }
+        };
     }
 }
