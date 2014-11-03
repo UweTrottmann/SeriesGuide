5cfa0f6b944c99fa4e9190c3067e02d3485d5780
Uwe Trottmann <uwe.trottmann@gmail.com>
2014-11-03 11:53:45 +0100

    Use application context for upgrade toast.
    - https://fabric.io/seriesguide/android/apps/com.battlelancer.seriesguide/issues/545494dee3de5099ba2df69b
    - Potential fix for crash on L preview.

--- SeriesGuide/src/main/java/com/battlelancer/seriesguide/ui/ShowsActivity.java
+++ SeriesGuide/src/main/java/com/battlelancer/seriesguide/ui/ShowsActivity.java
@@ -541,7 +541,7 @@ public class ShowsActivity extends BaseTopActivity implements
             }
 
             // update notification
-            Toast.makeText(this, R.string.updated, Toast.LENGTH_LONG).show();
+            Toast.makeText(getApplicationContext(), R.string.updated, Toast.LENGTH_LONG).show();
 
             // set this as lastVersion
             editor.putInt(AppSettings.KEY_VERSION, currentVersion);
