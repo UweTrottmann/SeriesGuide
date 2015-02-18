ad2ee0582a31d610428083b1533d6b08fd137c4c
Uwe Trottmann <uwe.trottmann@gmail.com>
2015-02-18 15:03:55 +0100

    Infer item position from layout params to calculate decoration.
    - Fixes crash if adapter items are removed. Recyclerview keeps items around to animate them out. So previous span lookup failed, if item was gone from adapter.

--- SeriesGuide/src/main/java/com/battlelancer/seriesguide/util/GridInsetDecoration.java
+++ SeriesGuide/src/main/java/com/battlelancer/seriesguide/util/GridInsetDecoration.java
@@ -42,19 +42,21 @@ public class GridInsetDecoration extends RecyclerView.ItemDecoration {
     @Override
     public void getItemOffsets(Rect outRect, View view, RecyclerView parent,
             RecyclerView.State state) {
-        GridLayoutManager layoutManager = (GridLayoutManager) parent.getLayoutManager();
-        GridLayoutManager.SpanSizeLookup spanSizeLookup = layoutManager.getSpanSizeLookup();
+        GridLayoutManager.LayoutParams layoutParams
+                = (GridLayoutManager.LayoutParams) view.getLayoutParams();
 
-        int spanCount = layoutManager.getSpanCount();
-        int position = parent.getChildPosition(view);
-        int itemSpanIndex = spanSizeLookup.getSpanIndex(position, spanCount);
-        int itemSpanGroupIndex = spanSizeLookup.getSpanGroupIndex(position, spanCount);
+        int position = layoutParams.getViewPosition();
+        if (position == RecyclerView.NO_POSITION) {
+            outRect.set(0, 0, 0, 0);
+            return;
+        }
 
         // add edge margin only if item edge is not the grid edge
+        int itemSpanIndex = layoutParams.getSpanIndex();
         // is left grid edge?
         outRect.left = itemSpanIndex == 0 ? 0 : insetHorizontal;
-        // is top edge?
-        outRect.top = itemSpanGroupIndex == 0 ? 0 : insetVertical;
+        // is top grid edge?
+        outRect.top = itemSpanIndex == position ? 0 : insetVertical;
         outRect.right = 0;
         outRect.bottom = 0;
     }
