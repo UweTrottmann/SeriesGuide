3f1bb687e3d7a998af4bbd9052f29bcdada638d5
Uwe Trottmann <uwe.trottmann@gmail.com>
2015-07-13 16:19:20 +0200

    Update timber to 3.1.0.
    - https://github.com/JakeWharton/timber/blob/master/CHANGELOG.md#version-310-2015-05-11
    - Use new simpler API (aka drop a lot of code in AnalyticsTree).

--- SeriesGuide/src/main/java/com/battlelancer/seriesguide/AnalyticsTree.java
+++ SeriesGuide/src/main/java/com/battlelancer/seriesguide/AnalyticsTree.java
@@ -17,22 +17,18 @@
 package com.battlelancer.seriesguide;
 
 import android.content.Context;
+import android.util.Log;
 import com.battlelancer.seriesguide.thetvdbapi.TvdbException;
 import com.battlelancer.seriesguide.util.Utils;
 import com.crashlytics.android.Crashlytics;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 import retrofit.RetrofitError;
 import timber.log.Timber;
 
 /**
- * Mostly copy of {@link timber.log.Timber.DebugTree}, but logs to Crashlytics. Drops debug and
- * verbose logs.
+ * A customized {@link timber.log.Timber.DebugTree} that logs to Crashlytics and Google Analytics.
+ * Always drops debug and verbose logs.
  */
-public class AnalyticsTree extends Timber.HollowTree implements Timber.TaggedTree {
-    private static final Pattern ANONYMOUS_CLASS = Pattern.compile("\\$\\d+$");
-    private static final ThreadLocal<String> NEXT_TAG = new ThreadLocal<>();
-
+public class AnalyticsTree extends Timber.DebugTree {
     private final Context context;
 
     public AnalyticsTree(Context context) {
@@ -40,86 +36,59 @@ public class AnalyticsTree extends Timber.HollowTree implements Timber.TaggedTre
     }
 
     @Override
-    public void i(String message, Object... args) {
-        logToCrashlytics("INFO", createTag(), message, args);
-    }
+    protected void log(int priority, String tag, String message, Throwable t) {
+        if (priority == Log.ERROR) {
+            // special treatment for retrofit and TheTVDB errors
+            if (t instanceof RetrofitError) {
+                RetrofitError e = (RetrofitError) t;
 
-    @Override
-    public void i(Throwable t, String message, Object... args) {
-        logToCrashlytics("INFO", createTag(), message, args);
-    }
-
-    @Override
-    public void w(String message, Object... args) {
-        logToCrashlytics("WARN", createTag(), message, args);
-    }
-
-    @Override
-    public void w(Throwable t, String message, Object... args) {
-        logToCrashlytics("WARN", createTag(), message, args);
-    }
-
-    @Override
-    public void e(String message, Object... args) {
-        logToCrashlytics("ERROR", createTag(), message, args);
-    }
-
-    @Override
-    public void e(Throwable t, String message, Object... args) {
-        String tag = createTag();
-
-        // special treatment for retrofit errors
-        if (t instanceof RetrofitError) {
-            RetrofitError e = (RetrofitError) t;
-
-            // only logging retrofit HTTP and conversion errors for now
-            if (e.getKind() == RetrofitError.Kind.HTTP) {
+                // only logging retrofit HTTP and conversion errors for now
+                if (e.getKind() == RetrofitError.Kind.HTTP) {
+                    Utils.trackCustomEvent(context,
+                            "Network Request Error",
+                            tag + ": " + message,
+                            e.getResponse().getStatus() + " " + e.getUrl());
+                } else if (e.getKind() == RetrofitError.Kind.CONVERSION) {
+                    Utils.trackCustomEvent(context,
+                            "Request Conversion Error",
+                            tag + ": " + message,
+                            e.getResponse().getStatus() + " " + e.getUrl());
+                }
+                return;
+            } else if (t instanceof TvdbException) {
+                TvdbException e = (TvdbException) t;
                 Utils.trackCustomEvent(context,
-                        "Network Request Error",
+                        "TheTVDB Error",
                         tag + ": " + message,
-                        e.getResponse().getStatus() + " " + e.getUrl());
-            } else if (e.getKind() == RetrofitError.Kind.CONVERSION) {
-                Utils.trackCustomEvent(context,
-                        "Request Conversion Error",
-                        tag + ": " + message,
-                        e.getResponse().getStatus() + " " + e.getUrl());
+                        e.getMessage());
+                return;
             }
-        } else if (t instanceof TvdbException) {
-            TvdbException e = (TvdbException) t;
-            Utils.trackCustomEvent(context,
-                    "TheTVDB Error",
-                    tag + ": " + message,
-                    e.getMessage());
-        } else {
-            logToCrashlytics("ERROR", tag, message, args);
-            Crashlytics.logException(t);
-        }
-    }
-
-    @Override
-    public void tag(String tag) {
-        NEXT_TAG.set(tag);
-    }
-
-    private static String createTag() {
-        String tag = NEXT_TAG.get();
-        if (tag != null) {
-            NEXT_TAG.remove();
-            return tag;
         }
 
-        tag = new Throwable().getStackTrace()[4].getClassName();
-        Matcher m = ANONYMOUS_CLASS.matcher(tag);
-        if (m.find()) {
-            tag = m.replaceAll("");
-        }
-        return tag.substring(tag.lastIndexOf('.') + 1);
-    }
-
-    private void logToCrashlytics(String level, String tag, String message, Object... args) {
+        // drop empty messages
         if (message == null) {
             return;
         }
-        Crashlytics.log(level + "/" + tag + ": " + String.format(message, args));
+        // drop debug and verbose logs
+        if (priority == Log.DEBUG || priority == Log.VERBOSE) {
+            return;
+        }
+
+        // transform priority into string
+        String level = null;
+        switch (priority) {
+            case Log.INFO:
+                level = "INFO";
+                break;
+            case Log.WARN:
+                level = "WARN";
+                break;
+            case Log.ERROR:
+                level = "ERROR";
+                break;
+        }
+
+        // finally log to crashlytics
+        Crashlytics.log(level + "/" + tag + ": " + message);
     }
 }
