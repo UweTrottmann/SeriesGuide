03d0424bf04a187147f54431b6e01e4ccc1bc80c
Uwe Trottmann <uwe.trottmann@gmail.com>
2015-08-27 14:32:01 +0200

    Show deprecation warning when updating.

--- SeriesGuide/src/main/java/com/battlelancer/seriesguide/ui/ShowsActivity.java
+++ SeriesGuide/src/main/java/com/battlelancer/seriesguide/ui/ShowsActivity.java
@@ -572,6 +572,7 @@ public class ShowsActivity extends BaseTopActivity implements
 
             // update notification
             Toast.makeText(this, R.string.updated, Toast.LENGTH_LONG).show();
+            Toast.makeText(this, R.string.deprecation_warning, Toast.LENGTH_LONG).show();
 
             // set this as lastVersion
             editor.putInt(AppSettings.KEY_VERSION, currentVersion);
