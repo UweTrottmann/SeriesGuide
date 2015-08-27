f76d0ed37bdfc8bea5e0e3f5f6fce7ebf5fdf144
Uwe Trottmann <uwe.trottmann@gmail.com>
2015-08-27 14:11:41 +0200

    Disable any remaining trakt functionality.

--- SeriesGuide/src/main/java/com/battlelancer/seriesguide/settings/TraktCredentials.java
+++ SeriesGuide/src/main/java/com/battlelancer/seriesguide/settings/TraktCredentials.java
@@ -66,7 +66,8 @@ public class TraktCredentials {
      * If there is a username and password.
      */
     public boolean hasCredentials() {
-        return mHasCredentials;
+        // trakt is not supported for this version anymore
+        return false;
     }
 
     /**
