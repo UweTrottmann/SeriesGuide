54b27d5a27fadfd31d2414b56f9e9f83b93de1a9
Uwe Trottmann <uwe.trottmann@gmail.com>
2015-10-21 16:46:49 +0200

    Do not send watched/collected flags if show has no trakt id.
    - Warn user that the show can currently not be tracked with trakt.
    - Drop initial "Sending to trakt." toast.
    - Send watched/collected flags to trakt using trakt id instead of TVDb id.

--- SeriesGuide/src/main/java/com/battlelancer/seriesguide/util/EpisodeTools.java
+++ SeriesGuide/src/main/java/com/battlelancer/seriesguide/util/EpisodeTools.java
@@ -998,6 +998,8 @@ public class EpisodeTools {
         private boolean mIsSendingToTrakt;
         private boolean mIsSendingToHexagon;
 
+        private boolean mCanSendToTrakt;
+
         public EpisodeFlagTask(Context context, FlagType type) {
             mContext = context.getApplicationContext();
             mType = type;
@@ -1012,9 +1014,6 @@ public class EpisodeTools {
             }
             mIsSendingToTrakt = TraktCredentials.get(mContext).hasCredentials()
                     && !isSkipped(mType.mEpisodeFlag);
-            if (mIsSendingToTrakt) {
-                Toast.makeText(mContext, R.string.trakt_submitqueued, Toast.LENGTH_SHORT).show();
-            }
         }
 
         @Override
@@ -1039,17 +1038,22 @@ public class EpisodeTools {
              * to flagging as unwatched.
              */
             if (mIsSendingToTrakt) {
-                if (!AndroidUtils.isNetworkConnected(mContext)) {
-                    return ERROR_NETWORK;
-                }
+                // Do not send if show has no trakt id (was not on trakt last time we checked).
+                Integer traktId = getShowTraktId(mContext, mType.getShowTvdbId());
+                mCanSendToTrakt = traktId != null && traktId > 0;
+                if (mCanSendToTrakt) {
+                    if (!AndroidUtils.isNetworkConnected(mContext)) {
+                        return ERROR_NETWORK;
+                    }
 
-                int result = uploadToTrakt(mContext,
-                        mType.getShowTvdbId(),
-                        mType.mAction,
-                        mType.getEpisodesForTrakt(),
-                        !isUnwatched(mType.mEpisodeFlag));
-                if (result < 0) {
-                    return result;
+                    int result = uploadToTrakt(mContext,
+                            traktId,
+                            mType.mAction,
+                            mType.getEpisodesForTrakt(),
+                            !isUnwatched(mType.mEpisodeFlag));
+                    if (result < 0) {
+                        return result;
+                    }
                 }
             }
 
@@ -1059,6 +1063,25 @@ public class EpisodeTools {
             mType.onPostExecute();
 
             return SUCCESS;
+        }
+
+        @Nullable
+        private Integer getShowTraktId(@NonNull Context context, int showTvdbId) {
+            Cursor traktIdQuery = context.getContentResolver()
+                    .query(SeriesGuideContract.Shows.buildShowUri(showTvdbId),
+                            new String[] { SeriesGuideContract.Shows.TRAKT_ID }, null, null, null);
+            if (traktIdQuery == null) {
+                return null;
+            }
+
+            Integer traktId = null;
+            if (traktIdQuery.moveToFirst()) {
+                traktId = traktIdQuery.getInt(0);
+            }
+
+            traktIdQuery.close();
+
+            return traktId;
         }
 
         private static int uploadToHexagon(Context context, int showTvdbId,
@@ -1098,8 +1121,9 @@ public class EpisodeTools {
         /**
          * @param flags Send {@code null} to upload complete show.
          */
-        private static int uploadToTrakt(Context context, int showTvdbId, EpisodeAction flagAction,
-                @Nullable List<SyncSeason> flags, boolean isAddNotDelete) {
+        private static int uploadToTrakt(@NonNull Context context, int showTraktId,
+                @NonNull EpisodeAction flagAction, @Nullable List<SyncSeason> flags,
+                boolean isAddNotDelete) {
             if (flags != null && flags.isEmpty()) {
                 // nothing to upload
                 return SUCCESS;
@@ -1112,7 +1136,7 @@ public class EpisodeTools {
             Sync traktSync = trakt.sync();
 
             // outer wrapper and show are always required
-            SyncShow show = new SyncShow().id(ShowIds.tvdb(showTvdbId));
+            SyncShow show = new SyncShow().id(ShowIds.trakt(showTraktId));
             SyncItems items = new SyncItems().shows(show);
 
             // add season or episodes
@@ -1232,20 +1256,26 @@ public class EpisodeTools {
 
             // display success message
             if (mIsSendingToTrakt) {
-                int status = R.string.trakt_success;
-                if (mType.mAction == EpisodeAction.SHOW_WATCHED
-                        || mType.mAction == EpisodeAction.SHOW_COLLECTED
-                        || mType.mAction == EpisodeAction.EPISODE_WATCHED_PREVIOUS) {
-                    // simple ack
-                    Toast.makeText(mContext,
-                            mContext.getString(status),
-                            Toast.LENGTH_SHORT).show();
+                if (mCanSendToTrakt) {
+                    int status = R.string.trakt_success;
+                    if (mType.mAction == EpisodeAction.SHOW_WATCHED
+                            || mType.mAction == EpisodeAction.SHOW_COLLECTED
+                            || mType.mAction == EpisodeAction.EPISODE_WATCHED_PREVIOUS) {
+                        // simple ack
+                        Toast.makeText(mContext,
+                                mContext.getString(status),
+                                Toast.LENGTH_SHORT).show();
+                    } else {
+                        // detailed ack
+                        String message = mType.getNotificationText();
+                        Toast.makeText(mContext,
+                                message + " " + mContext.getString(status),
+                                Toast.LENGTH_SHORT).show();
+                    }
                 } else {
-                    // detailed ack
-                    String message = mType.getNotificationText();
-                    Toast.makeText(mContext,
-                            message + " " + mContext.getString(status),
-                            Toast.LENGTH_SHORT).show();
+                    // tell the user this change can not be sent to trakt for now
+                    Toast.makeText(mContext, R.string.trakt_notice_not_exists, Toast.LENGTH_LONG)
+                            .show();
                 }
             }
         }
