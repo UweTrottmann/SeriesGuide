ef5d48098d01eeefc393dcd20874b76c39d79369
Uwe Trottmann <uwe+git@uwetrottmann.com>
2016-03-10 16:06:08 +0100

    Fix some boxing warnings.

--- SeriesGuide/src/main/java/com/battlelancer/seriesguide/billing/IabHelper.java
+++ SeriesGuide/src/main/java/com/battlelancer/seriesguide/billing/IabHelper.java
@@ -728,7 +728,7 @@ public class IabHelper {
             logDebug("Bundle with null response code, assuming OK (known issue)");
             return BILLING_RESPONSE_RESULT_OK;
         } else if (o instanceof Integer) {
-            return ((Integer) o).intValue();
+            return (Integer) o;
         } else if (o instanceof Long) {
             return (int) ((Long) o).longValue();
         } else {
@@ -746,7 +746,7 @@ public class IabHelper {
             logError("Intent with no response code, assuming OK (known issue)");
             return BILLING_RESPONSE_RESULT_OK;
         } else if (o instanceof Integer) {
-            return ((Integer) o).intValue();
+            return (Integer) o;
         } else if (o instanceof Long) {
             return (int) ((Long) o).longValue();
         } else {
