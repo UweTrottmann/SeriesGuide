12cbe18606e57b1c82d1d65cb774cd7f5e917367
Uwe Trottmann <uwe+git@uwetrottmann.com>
2016-03-10 13:40:10 +0100

    Add docs about billing response codes.

--- SeriesGuide/src/main/java/com/battlelancer/seriesguide/billing/IabHelper.java
+++ SeriesGuide/src/main/java/com/battlelancer/seriesguide/billing/IabHelper.java
@@ -97,13 +97,27 @@ public class IabHelper {
             + BuildConfig.IAP_KEY_C + BuildConfig.IAP_KEY_D;
 
     // Billing response codes
+    /** Success. */
     public static final int BILLING_RESPONSE_RESULT_OK = 0;
+    /** User pressed back or canceled a dialog. */
     public static final int BILLING_RESPONSE_RESULT_USER_CANCELED = 1;
+    /** Network connection is down. */
+    public static final int BILLING_RESPONSE_RESULT_SERVICE_UNAVAILABLE = 2;
+    /** Billing API version is not supported for the type requested. */
     public static final int BILLING_RESPONSE_RESULT_BILLING_UNAVAILABLE = 3;
+    /** Requested product is not available for purchase. */
     public static final int BILLING_RESPONSE_RESULT_ITEM_UNAVAILABLE = 4;
+    /**
+     * Invalid arguments provided to the API. This error can also indicate that the application was
+     * not correctly signed or properly set up for In-app Billing in Google Play, or does not have
+     * the necessary permissions in its manifest.
+     */
     public static final int BILLING_RESPONSE_RESULT_DEVELOPER_ERROR = 5;
+    /** Fatal error during the API action. */
     public static final int BILLING_RESPONSE_RESULT_ERROR = 6;
+    /** Failure to purchase since item is already owned. */
     public static final int BILLING_RESPONSE_RESULT_ITEM_ALREADY_OWNED = 7;
+    /** Failure to consume since item is not owned. */
     public static final int BILLING_RESPONSE_RESULT_ITEM_NOT_OWNED = 8;
 
     // IAB Helper error codes
