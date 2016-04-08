11c118d9965b55a9f080184f6baf76a2e9516acf
Uwe Trottmann <uwe+git@uwetrottmann.com>
2016-04-08 07:29:05 +0200

    Store the current TVDB web token in a preferences file.
    - Prevents the need for a login call if the app is killed before the token expires. The token is currently valid up to 24 hours.

--- SeriesGuide/src/main/java/com/battlelancer/seriesguide/thetvdbapi/SgTheTvdb.java
+++ SeriesGuide/src/main/java/com/battlelancer/seriesguide/thetvdbapi/SgTheTvdb.java
@@ -17,6 +17,7 @@
 package com.battlelancer.seriesguide.thetvdbapi;
 
 import android.content.Context;
+import android.content.SharedPreferences;
 import com.battlelancer.seriesguide.BuildConfig;
 import com.battlelancer.seriesguide.util.ServiceUtils;
 import com.uwetrottmann.thetvdb.TheTvdb;
@@ -26,19 +27,37 @@ import okhttp3.Cache;
 import okhttp3.OkHttpClient;
 
 /**
- * Extends {@link TheTvdb} to use our own caching OkHttp client.
+ * Extends {@link TheTvdb} to use our own caching OkHttp client and a preferences file to store the
+ * current JSON web token long term. The token is currently valid 24 hours, so preserve it if the
+ * app gets killed to avoid an additional login endpoint call.
  */
 public class SgTheTvdb extends TheTvdb {
 
     private static final String CACHE_DIRECTORY = "thetvdb-cache";
+    private static final String PREFERENCE_FILE = "thetvdb-prefs";
+    private static final String KEY_JSON_WEB_TOKEN = "token";
 
     private static OkHttpClient cachingHttpClient;
 
     private final Context context;
+    private final SharedPreferences preferences;
 
     public SgTheTvdb(Context context) {
         super(BuildConfig.TVDB_API_KEY);
         this.context = context.getApplicationContext();
+        this.preferences = context.getSharedPreferences(PREFERENCE_FILE, Context.MODE_PRIVATE);
+    }
+
+    @Override
+    public String jsonWebToken() {
+        return preferences.getString(KEY_JSON_WEB_TOKEN, null);
+    }
+
+    @Override
+    public void jsonWebToken(String value) {
+        preferences.edit()
+                .putString(KEY_JSON_WEB_TOKEN, value)
+                .apply();
     }
 
     @Override
