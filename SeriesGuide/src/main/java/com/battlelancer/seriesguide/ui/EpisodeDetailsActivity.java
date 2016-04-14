b87e7a0b8570b39d14b96f554277e1ceca92f309
Uwe Trottmann <uwe+git@uwetrottmann.com>
2016-04-14 15:09:56 +0200

    Add season switcher to episode details for phones.
    - Closes https://github.com/UweTrottmann/SeriesGuide/issues/246

--- SeriesGuide/src/main/java/com/battlelancer/seriesguide/ui/EpisodeDetailsActivity.java
+++ SeriesGuide/src/main/java/com/battlelancer/seriesguide/ui/EpisodeDetailsActivity.java
@@ -18,8 +18,8 @@ package com.battlelancer.seriesguide.ui;
 
 import android.content.Context;
 import android.content.Intent;
-import android.database.Cursor;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
@@ -28,25 +28,32 @@ import android.support.v4.app.LoaderManager;
 import android.support.v4.app.NavUtils;
 import android.support.v4.app.TaskStackBuilder;
 import android.support.v4.content.ContextCompat;
-import android.support.v4.content.CursorLoader;
 import android.support.v4.content.Loader;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.ActionBar;
+import android.view.LayoutInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
 import android.widget.ImageView;
+import android.widget.Spinner;
+import android.widget.SpinnerAdapter;
+import android.widget.TextView;
 import butterknife.ButterKnife;
 import com.battlelancer.seriesguide.R;
+import com.battlelancer.seriesguide.dataliberation.model.Season;
 import com.battlelancer.seriesguide.items.Episode;
 import com.battlelancer.seriesguide.loaders.SeasonEpisodesLoader;
-import com.battlelancer.seriesguide.provider.SeriesGuideContract.Episodes;
-import com.battlelancer.seriesguide.provider.SeriesGuideContract.Seasons;
-import com.battlelancer.seriesguide.provider.SeriesGuideContract.Shows;
+import com.battlelancer.seriesguide.loaders.SeasonsLoader;
 import com.battlelancer.seriesguide.util.SeasonTools;
 import com.battlelancer.seriesguide.util.TextTools;
 import com.battlelancer.seriesguide.util.ThemeUtils;
 import com.battlelancer.seriesguide.util.Utils;
 import com.battlelancer.seriesguide.widgets.SlidingTabLayout;
 import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Hosts a {@link ViewPager} displaying an episode per fragment of a complete season. Used on
@@ -61,11 +68,14 @@ public class EpisodeDetailsActivity extends BaseNavDrawerActivity {
     private static final String ARGS_SEASON_TVDB_ID = "seasonTvdbId";
     private static final String ARGS_SEASON_NUMBER = "seasonNumber";
 
+    private Spinner toolbarSpinner;
+    private SeasonSpinnerAdapter spinnerAdapter;
     private ImageView imageViewBackground;
     private SlidingTabLayout tabs;
     private ViewPager viewPager;
 
     private boolean canSafelyCommit;
+    private boolean noSavedInstanceState;
     private int episodeTvdbId;
     private int seasonTvdbId;
     private int showTvdbId;
@@ -82,6 +92,7 @@ public class EpisodeDetailsActivity extends BaseNavDrawerActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         canSafelyCommit = true;
+        noSavedInstanceState = savedInstanceState == null;
 
         setContentView(R.layout.activity_episode);
         setupActionBar();
@@ -96,6 +107,16 @@ public class EpisodeDetailsActivity extends BaseNavDrawerActivity {
         ThemeUtils.setImmersiveTheme(this);
     }
 
+    @Override
+    protected void setupActionBar() {
+        super.setupActionBar();
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+            actionBar.setDisplayShowTitleEnabled(false);
+        }
+    }
+
     private void setupViews() {
         episodeTvdbId = getIntent().getIntExtra(InitBundle.EPISODE_TVDBID, -1);
         if (episodeTvdbId == -1) {
@@ -104,6 +125,7 @@ public class EpisodeDetailsActivity extends BaseNavDrawerActivity {
             return;
         }
 
+        toolbarSpinner = ButterKnife.findById(this, R.id.sgToolbarSpinner);
         imageViewBackground = ButterKnife.findById(this, R.id.imageViewEpisodeDetailsBackground);
         tabs = ButterKnife.findById(this, R.id.tabsEpisodeDetails);
         viewPager = ButterKnife.findById(this, R.id.pagerEpisodeDetails);
@@ -117,7 +139,7 @@ public class EpisodeDetailsActivity extends BaseNavDrawerActivity {
         // start loading data
         Bundle args = new Bundle();
         args.putInt(InitBundle.EPISODE_TVDBID, episodeTvdbId);
-        getSupportLoaderManager().initLoader(LOADER_EPISODE_ID, args, episodeLoaderCallbacks);
+        getSupportLoaderManager().initLoader(LOADER_EPISODE_ID, args, basicInfoLoaderCallbacks);
     }
 
     @Override
@@ -138,6 +160,9 @@ public class EpisodeDetailsActivity extends BaseNavDrawerActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
         int itemId = item.getItemId();
         if (itemId == android.R.id.home) {
+            if (seasonTvdbId == 0) {
+                return true; // season tvdb not determined yet, have no idea where to go up to.
+            }
             Intent upIntent = new Intent(this, EpisodesActivity.class);
             upIntent.putExtra(EpisodesActivity.InitBundle.SEASON_TVDBID, seasonTvdbId);
             if (NavUtils.shouldUpRecreateTask(this, upIntent)) {
@@ -168,78 +193,88 @@ public class EpisodeDetailsActivity extends BaseNavDrawerActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    interface EpisodeQuery {
-        String[] PROJECTION = new String[] {
-                Shows.REF_SHOW_ID,
-                Shows.TITLE,
-                Shows.POSTER,
-                Seasons.REF_SEASON_ID,
-                Episodes.SEASON
-        };
-        int SHOW_TVDB_ID = 0;
-        int SHOW_TITLE = 1;
-        int SHOW_POSTER = 2;
-        int SEASON_TVDB_ID = 3;
-        int SEASON_NUMBER = 4;
-    }
-
-    private LoaderManager.LoaderCallbacks<Cursor> episodeLoaderCallbacks
-            = new LoaderManager.LoaderCallbacks<Cursor>() {
+    private LoaderManager.LoaderCallbacks<SeasonsLoader.Result> basicInfoLoaderCallbacks
+            = new LoaderManager.LoaderCallbacks<SeasonsLoader.Result>() {
         @Override
-        public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+        public Loader<SeasonsLoader.Result> onCreateLoader(int id, Bundle args) {
             int episodeTvdbId = args.getInt(InitBundle.EPISODE_TVDBID);
-            return new CursorLoader(EpisodeDetailsActivity.this,
-                    Episodes.buildEpisodeWithShowUri(String.valueOf(episodeTvdbId)),
-                    EpisodeQuery.PROJECTION, null, null, null);
+            return new SeasonsLoader(EpisodeDetailsActivity.this, episodeTvdbId);
         }
 
         @Override
-        public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+        public void onLoadFinished(Loader<SeasonsLoader.Result> loader, SeasonsLoader.Result data) {
             populateBasicInfo(data);
         }
 
         @Override
-        public void onLoaderReset(Loader<Cursor> loader) {
+        public void onLoaderReset(Loader<SeasonsLoader.Result> loader) {
             // do nothing, keep existing data
         }
     };
 
-    private void populateBasicInfo(@Nullable Cursor episodeQuery) {
-        if (episodeQuery == null || !episodeQuery.moveToFirst()) {
+    private void populateBasicInfo(@Nullable SeasonsLoader.Result basicInfo) {
+        if (basicInfo == null) {
+            // do not have minimal data, give up.
             finish();
             return;
         }
 
-        int seasonNumber = episodeQuery.getInt(EpisodeQuery.SEASON_NUMBER);
-        setupActionBar(episodeQuery.getString(EpisodeQuery.SHOW_TITLE),
-                SeasonTools.getSeasonString(this, seasonNumber));
+        showTvdbId = basicInfo.showTvdbId;
+        final String showTitle = basicInfo.showTitle;
 
         // set show poster as background
-        Utils.loadPosterBackground(this, imageViewBackground,
-                episodeQuery.getString(EpisodeQuery.SHOW_POSTER));
+        Utils.loadPosterBackground(this, imageViewBackground, basicInfo.showPoster);
 
-        showTvdbId = episodeQuery.getInt(EpisodeQuery.SHOW_TVDB_ID);
-        seasonTvdbId = episodeQuery.getInt(EpisodeQuery.SEASON_TVDB_ID);
+        // set up season switcher
+        spinnerAdapter = new SeasonSpinnerAdapter(this, basicInfo.seasonsOfShow);
+        toolbarSpinner.setAdapter(spinnerAdapter);
+        // if there is saved state the spinner will restore the last selection (for example on
+        // config changes)
+        // this listener will be called and automatically re-load the last shown season
+        toolbarSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                Season season = spinnerAdapter.getItem(position);
+                if (season.tvdbId == seasonTvdbId) {
+                    // guard against firing after layout completes
+                    // still happening on custom ROMs despite workaround described at
+                    // http://stackoverflow.com/a/17336944/1000543
+                    return;
+                }
+                loadSeason(season, showTitle);
+            }
 
-        if (showTvdbId != 0) {
-            updateShowDelayed(showTvdbId);
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                // ignored
+            }
+        });
+
+        // when shown initially, display the season of the given episode
+        if (noSavedInstanceState) {
+            Season initialSeason = basicInfo.seasonsOfShow.get(basicInfo.seasonIndexOfEpisode);
+            loadSeason(initialSeason, showTitle);
+            toolbarSpinner.setSelection(basicInfo.seasonIndexOfEpisode, false);
+
+            // ...and schedule a show update
+            if (showTvdbId != 0) {
+                updateShowDelayed(showTvdbId);
+            }
         }
-
-        // load episodes of the whole season
-        Bundle args = new Bundle();
-        args.putInt(ARGS_SEASON_TVDB_ID, seasonTvdbId);
-        args.putInt(ARGS_SEASON_NUMBER, seasonNumber);
-        args.putInt(InitBundle.EPISODE_TVDBID, episodeTvdbId);
-        getSupportLoaderManager().initLoader(LOADER_SEASON_ID, args, seasonLoaderCallbacks);
     }
 
-    private void setupActionBar(String showTitle, String season) {
-        setTitle(getString(R.string.episodes) + " " + showTitle + " " + season);
-        ActionBar actionBar = getSupportActionBar();
-        if (actionBar != null) {
-            actionBar.setDisplayHomeAsUpEnabled(true);
-            actionBar.setTitle(showTitle);
-        }
+    private void loadSeason(Season season, String showTitle) {
+        seasonTvdbId = season.tvdbId;
+
+        // update the activity title for accessibility
+        setTitle(getString(R.string.episodes) + " " + showTitle + " "
+                + SeasonTools.getSeasonString(this, season.season));
+
+        Bundle args = new Bundle();
+        args.putInt(ARGS_SEASON_TVDB_ID, season.tvdbId);
+        args.putInt(ARGS_SEASON_NUMBER, season.season);
+        args.putInt(InitBundle.EPISODE_TVDBID, episodeTvdbId);
+        getSupportLoaderManager().restartLoader(LOADER_SEASON_ID, args, seasonLoaderCallbacks);
     }
 
     private LoaderManager.LoaderCallbacks<SeasonEpisodesLoader.Result> seasonLoaderCallbacks
@@ -275,8 +310,67 @@ public class EpisodeDetailsActivity extends BaseNavDrawerActivity {
                 data.episodes, false);
         viewPager.setAdapter(adapter);
         tabs.setViewPager(viewPager);
-        // set current item
-        viewPager.setCurrentItem(data.requestedEpisodeIndex, false);
+
+        // when shown initially, try to select the requested episode
+        // but if there is saved state the view pager will restore the last selection on its own
+        if (noSavedInstanceState) {
+            viewPager.setCurrentItem(data.requestedEpisodeIndex, false);
+        }
+    }
+
+    public static class SeasonSpinnerAdapter extends BaseAdapter implements SpinnerAdapter {
+
+        private final Context context;
+        private final LayoutInflater inflater;
+        private final List<Season> seasons;
+
+        public SeasonSpinnerAdapter(Context context, List<Season> seasons) {
+            this.context = context;
+            inflater = LayoutInflater.from(context);
+            this.seasons = seasons;
+        }
+
+        @Override
+        public int getCount() {
+            return seasons.size();
+        }
+
+        @Override
+        public Season getItem(int position) {
+            return seasons.get(position);
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            return createViewFromResource(position, convertView, parent,
+                    R.layout.item_spinner_title);
+        }
+
+        @Override
+        public View getDropDownView(int position, View convertView, ViewGroup parent) {
+            return createViewFromResource(position, convertView, parent, R.layout.item_dropdown);
+        }
+
+        @NonNull
+        private View createViewFromResource(int position, View convertView, ViewGroup parent,
+                int resource) {
+            TextView view;
+            if (convertView == null) {
+                view = (TextView) inflater.inflate(resource, parent, false);
+            } else {
+                view = (TextView) convertView;
+            }
+
+            Season item = getItem(position);
+            view.setText(SeasonTools.getSeasonString(context, item.season));
+
+            return view;
+        }
     }
 
     public static class EpisodePagerAdapter extends FragmentStatePagerAdapter {
