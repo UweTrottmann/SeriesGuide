50063eb9d14ba47d5a85bfbb99c5f34fae0bfe53
Uwe Trottmann <uwe+git@uwetrottmann.com>
2016-04-20 09:22:00 +0200

    Increase API disk cache size to 20 MB.
    - After positive experience with separate cache for TVDb Series calls looks like this makes sense.

--- SeriesGuide/src/main/java/com/battlelancer/seriesguide/util/ServiceUtils.java
+++ SeriesGuide/src/main/java/com/battlelancer/seriesguide/util/ServiceUtils.java
@@ -62,7 +62,7 @@ public final class ServiceUtils {
     public static final int READ_TIMEOUT_MILLIS = 20 * 1000; // 20s
     private static final String API_CACHE = "api-cache";
     private static final int MIN_DISK_API_CACHE_SIZE = 2 * 1024 * 1024; // 2MB
-    private static final int MAX_DISK_API_CACHE_SIZE = 10 * 1024 * 1024; // 10MB
+    private static final int MAX_DISK_API_CACHE_SIZE = 20 * 1024 * 1024; // 20MB
 
     private static final String IMDB_APP_TITLE_URI_POSTFIX = "/";
 
