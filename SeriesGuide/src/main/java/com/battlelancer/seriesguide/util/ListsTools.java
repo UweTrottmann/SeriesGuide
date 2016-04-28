650618b24f1132a4294bcd5cfbb25f7a30487223
Uwe Trottmann <uwe+git@uwetrottmann.com>
2016-04-28 17:21:08 +0200

    Remove list items that were removed on Cloud.
    - Do check for null list items of a list.

--- SeriesGuide/src/main/java/com/battlelancer/seriesguide/util/ListsTools.java
+++ SeriesGuide/src/main/java/com/battlelancer/seriesguide/util/ListsTools.java
@@ -46,6 +46,9 @@ public class ListsTools {
     private static final String[] SELECTION_ARG = new String[1];
 
     interface Query {
+        String[] PROJECTION_LIST_ID = new String[] {
+                SeriesGuideContract.Lists.LIST_ID
+        };
         String[] PROJECTION_LIST = new String[] {
                 SeriesGuideContract.Lists.LIST_ID,
                 SeriesGuideContract.Lists.NAME,
@@ -196,7 +199,7 @@ public class ListsTools {
                 break; // empty response, assume we are done
             }
 
-            if (!doListsDatabaseUpdate(context, lists, localListIds)) {
+            if (!doListsDatabaseUpdate(context, lists, localListIds, hasMergedLists)) {
                 return false; // database update failed, abort
             }
         } while (!TextUtils.isEmpty(cursor)); // fetch next batch
@@ -213,7 +216,7 @@ public class ListsTools {
     }
 
     private static boolean doListsDatabaseUpdate(Context context, List<SgList> lists,
-            HashSet<String> localListIds) {
+            HashSet<String> localListIds, boolean removeMissingListItems) {
         ArrayList<ContentProviderOperation> batch = new ArrayList<>();
         for (SgList list : lists) {
             // add or update the list
@@ -235,32 +238,57 @@ public class ListsTools {
             }
             batch.add(builder.build());
 
-            // add or remove items of the list
-            for (SgListItem listItem : list.getListItems()) {
-                String listItemId = listItem.getListItemId();
-                String[] brokenUpId = SeriesGuideContract.ListItems.splitListItemId(listItemId);
-                if (brokenUpId == null) {
-                    continue; // could not break up list item id
+            // keep track of items not in the list on hexagon
+            HashSet<String> listItemsToRemove = null;
+            if (removeMissingListItems) {
+                listItemsToRemove = getListItemIds(context, listId);
+                if (listItemsToRemove == null) {
+                    return false; // list item query failed
                 }
-                int itemTvdbId = -1;
-                int itemType = -1;
-                try {
-                    itemTvdbId = Integer.parseInt(brokenUpId[0]);
-                    itemType = Integer.parseInt(brokenUpId[1]);
-                } catch (NumberFormatException ignored) {
-                }
-                if (itemTvdbId == -1 || !SeriesGuideContract.ListItems.isValidItemType(itemType)) {
-                    continue; // failed to extract item TVDB id or item type not known
-                }
+            }
+            // add or update items of the list
+            List<SgListItem> listItems = list.getListItems();
+            if (listItems != null) {
+                for (SgListItem listItem : listItems) {
+                    String listItemId = listItem.getListItemId();
+                    String[] brokenUpId = SeriesGuideContract.ListItems.splitListItemId(listItemId);
+                    if (brokenUpId == null) {
+                        continue; // could not break up list item id
+                    }
+                    int itemTvdbId = -1;
+                    int itemType = -1;
+                    try {
+                        itemTvdbId = Integer.parseInt(brokenUpId[0]);
+                        itemType = Integer.parseInt(brokenUpId[1]);
+                    } catch (NumberFormatException ignored) {
+                    }
+                    if (itemTvdbId == -1 || !SeriesGuideContract.ListItems.isValidItemType(
+                            itemType)) {
+                        continue; // failed to extract item TVDB id or item type not known
+                    }
 
-                // just insert the list item, if the id already exists it will be replaced
-                builder = ContentProviderOperation
-                        .newInsert(SeriesGuideContract.ListItems.CONTENT_URI)
-                        .withValue(SeriesGuideContract.ListItems.LIST_ITEM_ID, listItemId)
-                        .withValue(SeriesGuideContract.ListItems.ITEM_REF_ID, itemTvdbId)
-                        .withValue(SeriesGuideContract.ListItems.TYPE, itemType)
-                        .withValue(SeriesGuideContract.Lists.LIST_ID, listId);
-                batch.add(builder.build());
+                    // just insert the list item, if the id already exists it will be replaced
+                    builder = ContentProviderOperation
+                            .newInsert(SeriesGuideContract.ListItems.CONTENT_URI)
+                            .withValue(SeriesGuideContract.ListItems.LIST_ITEM_ID, listItemId)
+                            .withValue(SeriesGuideContract.ListItems.ITEM_REF_ID, itemTvdbId)
+                            .withValue(SeriesGuideContract.ListItems.TYPE, itemType)
+                            .withValue(SeriesGuideContract.Lists.LIST_ID, listId);
+                    batch.add(builder.build());
+
+                    if (removeMissingListItems) {
+                        // do not remove this list item
+                        listItemsToRemove.remove(listItemId);
+                    }
+                }
+            }
+            if (removeMissingListItems) {
+                // remove items no longer in the list
+                for (String listItemId : listItemsToRemove) {
+                    builder = ContentProviderOperation
+                            .newDelete(SeriesGuideContract.ListItems.buildListItemUri(listItemId));
+                    batch.add(builder.build());
+                }
             }
         }
 
@@ -281,18 +309,43 @@ public class ListsTools {
      */
     private static HashSet<String> getListIds(Context context) {
         Cursor query = context.getContentResolver().query(SeriesGuideContract.Lists.CONTENT_URI,
-                new String[] { SeriesGuideContract.Lists.LIST_ID }, null, null, null);
+                Query.PROJECTION_LIST_ID, null, null, null);
         if (query == null) {
             return null;
         }
 
         HashSet<String> listIds = new HashSet<>();
         while (query.moveToNext()) {
-            listIds.add(query.getString(0));
+            listIds.add(query.getString(Query.LIST_ID));
         }
 
         query.close();
 
         return listIds;
+    }
+
+    /**
+     * Returns a all list item ids of the given list in the local database.
+     *
+     * @return null if there was an error, empty list if there are no list items in this list.
+     */
+    private static HashSet<String> getListItemIds(Context context, String listId) {
+        SELECTION_ARG[0] = listId;
+        Cursor query = context.getContentResolver().query(SeriesGuideContract.ListItems.CONTENT_URI,
+                Query.PROJECTION_LIST_ITEMS,
+                SeriesGuideContract.ListItems.SELECTION_LIST,
+                SELECTION_ARG, null);
+        if (query == null) {
+            return null;
+        }
+
+        HashSet<String> listItemIds = new HashSet<>();
+        while (query.moveToNext()) {
+            listItemIds.add(query.getString(Query.LIST_ITEM_ID));
+        }
+
+        query.close();
+
+        return listItemIds;
     }
 }
