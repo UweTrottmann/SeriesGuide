a3bc711e1c999e397d214080149068409a32e627
Uwe Trottmann <uwe+git@uwetrottmann.com>
2016-07-07 17:34:48 +0200

    Remove unneeded modifiers.

--- api/src/main/java/com/battlelancer/seriesguide/api/constants/IncomingConstants.java
+++ api/src/main/java/com/battlelancer/seriesguide/api/constants/IncomingConstants.java
@@ -2,12 +2,12 @@ package com.battlelancer.seriesguide.api.constants;
 
 public interface IncomingConstants {
 
-    public static final String ACTION_SUBSCRIBE = "com.battlelancer.seriesguide.api.action.SUBSCRIBE";
-    public static final String EXTRA_SUBSCRIBER_COMPONENT = "com.battlelancer.seriesguide.api.extra.SUBSCRIBER_COMPONENT";
-    public static final String EXTRA_TOKEN = "com.battlelancer.seriesguide.api.extra.TOKEN";
+    String ACTION_SUBSCRIBE = "com.battlelancer.seriesguide.api.action.SUBSCRIBE";
+    String EXTRA_SUBSCRIBER_COMPONENT = "com.battlelancer.seriesguide.api.extra.SUBSCRIBER_COMPONENT";
+    String EXTRA_TOKEN = "com.battlelancer.seriesguide.api.extra.TOKEN";
 
-    public static final String ACTION_UPDATE = "com.battlelancer.seriesguide.api.action.UPDATE";
-    public static final String EXTRA_EPISODE = "com.battlelancer.seriesguide.api.extra.EPISODE";
-    public static final String EXTRA_ENTITY_IDENTIFIER = "com.battlelancer.seriesguide.api.extra.ENTITY_IDENTIFIER";
+    String ACTION_UPDATE = "com.battlelancer.seriesguide.api.action.UPDATE";
+    String EXTRA_EPISODE = "com.battlelancer.seriesguide.api.extra.EPISODE";
+    String EXTRA_ENTITY_IDENTIFIER = "com.battlelancer.seriesguide.api.extra.ENTITY_IDENTIFIER";
 
 }
--- api/src/main/java/com/battlelancer/seriesguide/api/constants/OutgoingConstants.java
+++ api/src/main/java/com/battlelancer/seriesguide/api/constants/OutgoingConstants.java
@@ -2,7 +2,7 @@ package com.battlelancer.seriesguide.api.constants;
 
 public interface OutgoingConstants {
 
-    public static final String ACTION_PUBLISH_ACTION = "com.battlelancer.seriesguide.api.action.PUBLISH_ACTION";
-    public static final String EXTRA_ACTION = "com.battlelancer.seriesguide.api.extra.ACTION";
+    String ACTION_PUBLISH_ACTION = "com.battlelancer.seriesguide.api.action.PUBLISH_ACTION";
+    String EXTRA_ACTION = "com.battlelancer.seriesguide.api.extra.ACTION";
 
 }
