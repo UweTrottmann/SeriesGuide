b601e1bf933b4be01d20eab39abc73909ce29890
Uwe Trottmann <uwe+git@uwetrottmann.com>
2017-02-23 13:48:26 +0100

    Support setting staging version for hexagon.

--- SeriesGuide/src/main/java/com/battlelancer/seriesguide/backend/CloudEndpointUtils.java
+++ SeriesGuide/src/main/java/com/battlelancer/seriesguide/backend/CloudEndpointUtils.java
@@ -12,46 +12,52 @@ import java.io.IOException;
  * Common utilities for working with Cloud Endpoints.
  *
  * If you'd like to test using a locally-running version of your App Engine backend (i.e. running on
- * the Development App Server), you need to set LOCAL_ANDROID_RUN to 'true'.
+ * the Development App Server), you need to set USE_LOCAL_VERSION to 'true'.
  *
  * See https://cloud.google.com/appengine/docs/java/endpoints
  */
 public class CloudEndpointUtils {
 
-    /*
+    private static final String PATH_API = "/_ah/api/";
+
+    /**
      * Change this to 'true' if you're running your backend locally using the DevAppServer.
      */
     @SuppressWarnings("PointlessBooleanExpression")
-    protected static final boolean LOCAL_ANDROID_RUN = false && BuildConfig.DEBUG;
+    protected static final boolean USE_LOCAL_VERSION = false && BuildConfig.DEBUG;
 
-    /*
-     * The root URL of where your DevAppServer is running (if you're running the
-     * DevAppServer locally).
+    @SuppressWarnings("PointlessBooleanExpression")
+    private static final boolean USE_STAGING_VERSION = false && BuildConfig.DEBUG;
+
+    private static final String ROOT_URL_STAGING = "";
+
+    /**
+     * The root URL of where your DevAppServer is running (if you're running the DevAppServer
+     * locally).
      */
-    @SuppressWarnings("unused")
-    protected static final String LOCAL_APP_ENGINE_SERVER_URL = "http://localhost:8080/";
+    private static final String ROOT_URL_LOCALHOST = "http://localhost:8080/";
 
-    /*
-     * The root URL of where your DevAppServer is running when it's being
-     * accessed via the Android emulator (if you're running the DevAppServer
-     * locally). In this case, you're running behind Android's virtual router.
-     * See
-     * http://developer.android.com/tools/devices/emulator.html#networkaddresses
+    /**
+     * The root URL of where your DevAppServer is running when it's being accessed via the Android
+     * emulator (if you're running the DevAppServer locally). In this case, you're running behind
+     * Android's virtual router. See http://developer.android.com/tools/devices/emulator.html#networkaddresses
      * for more information.
      */
-    protected static final String LOCAL_APP_ENGINE_SERVER_URL_FOR_ANDROID = "http://10.0.2.2:8080";
+    private static final String ROOT_URL_LOCALHOST_ANDROID = "http://10.0.2.2:8080";
 
     /**
      * Updates the Google client builder to connect the appropriate server based on whether
-     * LOCAL_ANDROID_RUN is true or false and sets a custom user agent.
+     * USE_LOCAL_VERSION is true or false and sets a custom user agent.
      *
      * @param builder Google client builder
      * @return same Google client builder
      */
     public static <B extends AbstractGoogleClient.Builder> B updateBuilder(Context context,
             B builder) {
-        if (LOCAL_ANDROID_RUN) {
-            builder.setRootUrl(LOCAL_APP_ENGINE_SERVER_URL_FOR_ANDROID + "/_ah/api/");
+        if (USE_LOCAL_VERSION) {
+            builder.setRootUrl(ROOT_URL_LOCALHOST_ANDROID + PATH_API);
+        } else if (USE_STAGING_VERSION) {
+            builder.setRootUrl(ROOT_URL_STAGING + PATH_API);
         }
         // used for user agent
         builder.setApplicationName("SeriesGuide " + Utils.getVersion(context));
