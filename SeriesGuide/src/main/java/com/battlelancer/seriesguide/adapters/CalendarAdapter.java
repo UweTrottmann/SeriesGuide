906373f624d62fb273efb224c42de7cdae39cc43
Uwe Trottmann <uwe+git@uwetrottmann.com>
2017-06-29 13:46:04 +0200

    Declutter calendar card.
    - Use dot separator for network and time.

--- SeriesGuide/src/main/java/com/battlelancer/seriesguide/adapters/CalendarAdapter.java
+++ SeriesGuide/src/main/java/com/battlelancer/seriesguide/adapters/CalendarAdapter.java
@@ -103,8 +103,8 @@ public class CalendarAdapter extends CursorAdapter implements StickyGridHeadersB
         }
 
         // timestamp, absolute time and network
-        StringBuilder releaseInfo = new StringBuilder();
         long releaseTime = cursor.getLong(Query.RELEASE_TIME_MS);
+        String time = null;
         if (releaseTime != -1) {
             Date actualRelease = TimeTools.applyUserOffset(context, releaseTime);
             // timestamp
@@ -112,17 +112,12 @@ public class CalendarAdapter extends CursorAdapter implements StickyGridHeadersB
             viewHolder.timestamp.setText(displayExactDate ?
                     TimeTools.formatToLocalDateShort(context, actualRelease)
                     : TimeTools.formatToLocalRelativeTime(context, actualRelease));
-
-            // "10:00 PM / Network", as left aligned, exactly mirrored from show list
-            releaseInfo.append(TimeTools.formatToLocalTime(context, actualRelease));
+            // release time of this episode
+            time = TimeTools.formatToLocalTime(context, actualRelease);
         } else {
             viewHolder.timestamp.setText(null);
         }
-        final String network = cursor.getString(Query.SHOW_NETWORK);
-        if (!TextUtils.isEmpty(network)) {
-            releaseInfo.append(" / ").append(network);
-        }
-        viewHolder.info.setText(releaseInfo);
+        viewHolder.info.setText(TextTools.dotSeparate(cursor.getString(Query.SHOW_NETWORK), time));
 
         // collected indicator
         boolean isCollected = EpisodeTools.isCollected(
