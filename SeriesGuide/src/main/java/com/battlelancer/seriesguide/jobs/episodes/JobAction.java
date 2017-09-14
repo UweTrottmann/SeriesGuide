6ce638073eecc89608c3c413e6d10a9873e48c46
Uwe Trottmann <uwe+git@uwetrottmann.com>
2017-09-14 11:49:38 +0200

    Drop unused flag from JobAction enum.

--- SeriesGuide/src/main/java/com/battlelancer/seriesguide/jobs/episodes/JobAction.java
+++ SeriesGuide/src/main/java/com/battlelancer/seriesguide/jobs/episodes/JobAction.java
@@ -1,17 +1,11 @@
 package com.battlelancer.seriesguide.jobs.episodes;
 
 public enum JobAction {
-    EPISODE_COLLECTED(false),
-    SEASON_COLLECTED(false),
-    SHOW_COLLECTED(false),
-    EPISODE_WATCHED(true),
-    EPISODE_WATCHED_PREVIOUS(true),
-    SEASON_WATCHED(true),
-    SHOW_WATCHED(true);
-
-    final boolean isWatchNotCollect;
-
-    JobAction(boolean isWatchNotCollect) {
-        this.isWatchNotCollect = isWatchNotCollect;
-    }
+    EPISODE_COLLECTED,
+    SEASON_COLLECTED,
+    SHOW_COLLECTED,
+    EPISODE_WATCHED,
+    EPISODE_WATCHED_PREVIOUS,
+    SEASON_WATCHED,
+    SHOW_WATCHED
 }
