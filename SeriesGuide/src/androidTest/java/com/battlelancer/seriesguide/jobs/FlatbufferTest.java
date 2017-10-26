51279a357d12e55c2f840dff57f71446e7416b87
Uwe Trottmann <uwe+git@uwetrottmann.com>
2017-10-26 10:12:21 +0200

    SgJobInfo: add movie tmdb id. Add test.

--- SeriesGuide/src/androidTest/java/com/battlelancer/seriesguide/jobs/FlatbufferTest.java
+++ SeriesGuide/src/androidTest/java/com/battlelancer/seriesguide/jobs/FlatbufferTest.java
@@ -6,8 +6,7 @@ import java.nio.ByteBuffer;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import static org.hamcrest.CoreMatchers.equalTo;
-import static org.junit.Assert.assertThat;
+import static com.google.common.truth.Truth.assertThat;
 
 @RunWith(AndroidJUnit4.class)
 public class FlatbufferTest {
@@ -25,7 +24,7 @@ public class FlatbufferTest {
         }
 
         int episodes = SgJobInfo.createEpisodesVector(builder, episodeInfos);
-        int jobInfo = SgJobInfo.createSgJobInfo(builder, 42, 1, episodes);
+        int jobInfo = SgJobInfo.createSgJobInfo(builder, 42, 1, episodes, 0);
 
         builder.finish(jobInfo);
 
@@ -35,19 +34,39 @@ public class FlatbufferTest {
 
         SgJobInfo jobInfoReloaded = SgJobInfo.getRootAsSgJobInfo(bufferReloaded);
 
-        assertThat(jobInfoReloaded.showTvdbId(), equalTo(42));
-        assertThat(jobInfoReloaded.flagValue(), equalTo(1));
-        assertThat(jobInfoReloaded.episodesLength(), equalTo(42));
+        assertThat(jobInfoReloaded.showTvdbId()).isEqualTo(42);
+        assertThat(jobInfoReloaded.flagValue()).isEqualTo(1);
+        assertThat(jobInfoReloaded.episodesLength()).isEqualTo(42);
         for (int i = 0; i < 21; i++) {
             EpisodeInfo episodeInfo = jobInfoReloaded.episodes(i);
-            assertThat(episodeInfo.season(), equalTo(1));
-            assertThat(episodeInfo.number(), equalTo(i + 1));
+            assertThat(episodeInfo.season()).isEqualTo(1);
+            assertThat(episodeInfo.number()).isEqualTo(i + 1);
         }
         for (int i = 21; i < 42; i++) {
             EpisodeInfo episodeInfo = jobInfoReloaded.episodes(i);
-            assertThat(episodeInfo.season(), equalTo(2));
-            assertThat(episodeInfo.number(), equalTo(i + 1));
+            assertThat(episodeInfo.season()).isEqualTo(2);
+            assertThat(episodeInfo.number()).isEqualTo(i + 1);
         }
+    }
+
+    @Test
+    public void movieId() {
+        FlatBufferBuilder builder = new FlatBufferBuilder(0);
+
+        int jobInfo = SgJobInfo.createSgJobInfo(builder, 0, 0, 0, 42);
+
+        builder.finish(jobInfo);
+
+        byte[] bytes = builder.sizedByteArray();
+
+        ByteBuffer bufferReloaded = ByteBuffer.wrap(bytes);
+
+        SgJobInfo jobInfoReloaded = SgJobInfo.getRootAsSgJobInfo(bufferReloaded);
+
+        assertThat(jobInfoReloaded.showTvdbId()).isEqualTo(0);
+        assertThat(jobInfoReloaded.flagValue()).isEqualTo(0);
+        assertThat(jobInfoReloaded.episodesLength()).isEqualTo(0);
+        assertThat(jobInfoReloaded.movieTmdbId()).isEqualTo(42);
     }
 
 }
