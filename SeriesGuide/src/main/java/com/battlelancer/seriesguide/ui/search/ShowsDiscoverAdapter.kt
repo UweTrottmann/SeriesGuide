85b4179ed24736ee318c691127e51d791403f1db
Uwe Trottmann <uwe+git@uwetrottmann.com>
2018-02-02 15:16:08 +0100

    Shows Discover: opt for links with larger horizontal margin.
    - Most users (== not connected to trakt) will only see the Popular link.
      It looks weird with a non-aligned left margin. Down-side is links will
      be narrower and there is a larger gap if all are displayed.

--- SeriesGuide/src/main/java/com/battlelancer/seriesguide/ui/search/ShowsDiscoverAdapter.kt
+++ SeriesGuide/src/main/java/com/battlelancer/seriesguide/ui/search/ShowsDiscoverAdapter.kt
@@ -204,7 +204,7 @@ class ShowsDiscoverAdapter(
     }
 
     companion object {
-        const val VIEW_TYPE_LINK = R.layout.item_discover_link
+        const val VIEW_TYPE_LINK = R.layout.item_grid_link
         const val VIEW_TYPE_HEADER = R.layout.item_grid_header
         const val VIEW_TYPE_SHOW = R.layout.item_addshow
     }
