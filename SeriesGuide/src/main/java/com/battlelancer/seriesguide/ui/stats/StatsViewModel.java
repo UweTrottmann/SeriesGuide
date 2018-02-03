e2b85c9fcbded2398d3b764b0ad41523fd9ea4f1
Uwe Trottmann <uwe+git@uwetrottmann.com>
2018-02-03 09:59:39 +0100

    StatsViewModel needs to be accessible for ViewModelProviders.
    - Make class and constructor public.
    - https://fabric.io/seriesguide/android/apps/com.battlelancer.seriesguide/issues/5a748d7e8cb3c2fa6325da64

--- SeriesGuide/src/main/java/com/battlelancer/seriesguide/ui/stats/StatsViewModel.java
+++ SeriesGuide/src/main/java/com/battlelancer/seriesguide/ui/stats/StatsViewModel.java
@@ -3,11 +3,12 @@ package com.battlelancer.seriesguide.ui.stats;
 import android.app.Application;
 import android.arch.lifecycle.AndroidViewModel;
 
-class StatsViewModel extends AndroidViewModel {
+@SuppressWarnings("WeakerAccess") // ViewModelProviders needs access
+public class StatsViewModel extends AndroidViewModel {
 
     private final StatsLiveData data;
 
-    StatsViewModel(Application application) {
+    public StatsViewModel(Application application) {
         super(application);
         data = new StatsLiveData(application);
     }
