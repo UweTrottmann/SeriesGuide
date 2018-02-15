859e51c2b2d24be1a82682cb28e605435fff51a5
Uwe Trottmann <uwe+git@uwetrottmann.com>
2018-02-15 11:39:00 +0100

    TestHttpClientModule: add missing param of overridden method.

--- SeriesGuide/src/androidTest/java/com/battlelancer/seriesguide/modules/TestHttpClientModule.java
+++ SeriesGuide/src/androidTest/java/com/battlelancer/seriesguide/modules/TestHttpClientModule.java
@@ -2,6 +2,7 @@ package com.battlelancer.seriesguide.modules;
 
 import android.content.Context;
 import com.battlelancer.seriesguide.thetvdbapi.SgTheTvdbInterceptor;
+import com.battlelancer.seriesguide.tmdbapi.SgTmdbInterceptor;
 import com.battlelancer.seriesguide.traktapi.SgTraktInterceptor;
 import com.battlelancer.seriesguide.util.AllApisAuthenticator;
 import okhttp3.Cache;
@@ -10,8 +11,13 @@ import okhttp3.OkHttpClient;
 public class TestHttpClientModule extends HttpClientModule {
 
     @Override
-    OkHttpClient provideOkHttpClient(Cache cache, SgTheTvdbInterceptor tvdbInterceptor,
-            SgTraktInterceptor traktInterceptor, AllApisAuthenticator authenticator) {
+    OkHttpClient provideOkHttpClient(
+            Cache cache,
+            SgTheTvdbInterceptor tvdbInterceptor,
+            SgTraktInterceptor traktInterceptor,
+            SgTmdbInterceptor tmdbInterceptor,
+            AllApisAuthenticator authenticator
+    ) {
         return null;
     }
 
