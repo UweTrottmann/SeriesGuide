e141bb38f95155dbace42359b88c97383e2e9b41
Uwe Trottmann <uwe+git@uwetrottmann.com>
2018-08-08 08:03:55 +0200

    JustWatchConfigureDialog: make recommended Kotlin changes.

--- app/src/main/java/com/battlelancer/seriesguide/justwatch/JustWatchConfigureDialog.kt
+++ app/src/main/java/com/battlelancer/seriesguide/justwatch/JustWatchConfigureDialog.kt
@@ -15,37 +15,35 @@ class JustWatchConfigureDialog : AppCompatDialogFragment() {
         val countries = JustWatchSearch.countryToUrl.keys.toList()
 
         val countryOrEmptyOrNull = JustWatchSearch.getCountryOrEmptyOrNull(requireContext())
-        val currentSelection = if (countryOrEmptyOrNull == null) {
-            -1 // not configured
-        } else if (countryOrEmptyOrNull.isEmpty()) {
-            0 // turned off
-        } else {
-            countries.indexOf(countryOrEmptyOrNull) + 1
+        val currentSelection = when {
+            countryOrEmptyOrNull == null -> -1 // not configured
+            countryOrEmptyOrNull.isEmpty() -> 0 // turned off
+            else -> countries.indexOf(countryOrEmptyOrNull) + 1
         }
 
-        val items = Array<CharSequence>(1 + countries.size, { i ->
+        val items = Array<CharSequence>(1 + countries.size) { i ->
             if (i == 0) {
                 getString(R.string.action_turn_off)
             } else {
                 JustWatchSearch.getCountryDisplayName(countries[i - 1])
             }
-        })
+        }
 
         return AlertDialog.Builder(requireContext())
                 .setTitle(R.string.action_just_watch)
                 .setSingleChoiceItems(
                         items,
-                        currentSelection,
-                        { _, position ->
-                            val countryOrEmpty = if (position == 0) {
-                                ""
-                            } else {
-                                countries[position - 1]
-                            }
-                            JustWatchSearch.setCountryOrEmpty(requireContext(), countryOrEmpty)
-                            EventBus.getDefault().post(JustWatchConfiguredEvent(position == 0))
-                            dismiss()
-                        }).create()
+                        currentSelection
+                ) { _, position ->
+                    val countryOrEmpty = if (position == 0) {
+                        ""
+                    } else {
+                        countries[position - 1]
+                    }
+                    JustWatchSearch.setCountryOrEmpty(requireContext(), countryOrEmpty)
+                    EventBus.getDefault().post(JustWatchConfiguredEvent(position == 0))
+                    dismiss()
+                }.create()
     }
 
 }
