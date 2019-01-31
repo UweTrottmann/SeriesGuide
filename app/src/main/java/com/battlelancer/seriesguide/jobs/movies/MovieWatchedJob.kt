48c33db3ffef47d299dff08220c6e99ceb66f18d
Uwe Trottmann <uwe+git@uwetrottmann.com>
2019-01-31 15:45:21 +0100

    MovieWatchedJob: convert to Kotlin

---
+++ app/src/main/java/com/battlelancer/seriesguide/jobs/movies/MovieWatchedJob.kt
@@ -0,0 +1,29 @@
+package com.battlelancer.seriesguide.jobs.movies
+
+import android.content.Context
+import com.battlelancer.seriesguide.R
+import com.battlelancer.seriesguide.jobs.episodes.JobAction
+import com.battlelancer.seriesguide.ui.movies.MovieTools
+
+class MovieWatchedJob(
+    movieTmdbId: Int,
+    private val isWatched: Boolean
+) : MovieJob(
+    if (isWatched) JobAction.MOVIE_WATCHED_SET else JobAction.MOVIE_WATCHED_REMOVE,
+    movieTmdbId
+) {
+
+    override fun applyDatabaseUpdate(context: Context, movieTmdbId: Int): Boolean {
+        return MovieTools.setWatchedFlag(context, movieTmdbId, isWatched)
+    }
+
+    override fun supportsHexagon(): Boolean {
+        return false // hexagon does only have watchlist and collection flags
+    }
+
+    override fun getConfirmationText(context: Context): String {
+        return context.getString(
+            if (isWatched) R.string.action_watched else R.string.action_unwatched
+        )
+    }
+}
