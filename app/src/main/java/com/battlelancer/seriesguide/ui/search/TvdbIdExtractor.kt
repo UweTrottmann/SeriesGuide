ec698afc8ed54a82d6701fb2dd171f5290a23c7e
Uwe Trottmann <uwe+git@uwetrottmann.com>
2019-02-21 10:01:59 +0100

    TvdbIdExtractor: support legacy TheTVDB URLs again.
    Closes https://github.com/UweTrottmann/SeriesGuide/issues/545

--- app/src/main/java/com/battlelancer/seriesguide/ui/search/TvdbIdExtractor.kt
+++ app/src/main/java/com/battlelancer/seriesguide/ui/search/TvdbIdExtractor.kt
@@ -13,7 +13,12 @@ class TvdbIdExtractor(val context: Context, val text: String) {
      */
     suspend fun tryToExtractTvdbId(): Int {
         return withContext(Dispatchers.IO) {
-            lookUpShowTvdbId()
+            val showTvdbId = matchLegacyTvdbId()
+            if (showTvdbId > 0) {
+                showTvdbId
+            } else {
+                lookUpShowTvdbId()
+            }
         }
     }
 
@@ -47,6 +52,33 @@ class TvdbIdExtractor(val context: Context, val text: String) {
         } else {
             null
         }
+    }
+
+    private fun matchLegacyTvdbId(): Int {
+        // match TVDB URLs like
+        // http://thetvdb.com/?tab=season&seriesid=153021&seasonid=627501
+        // http://thetvdb.com/?tab=episode&seriesid=281588&seasonid=585321&id=4881155
+        // http://thetvdb.com/?tab=series&id=322399
+
+        // match season and episode pages first
+        val showTvdbId = matchShowTvdbId(Pattern.compile("thetvdb\\.com.*?seriesid=([0-9]*)"), text)
+        return if (showTvdbId > 0) {
+            showTvdbId
+        } else {
+            // match show pages
+            matchShowTvdbId(Pattern.compile("thetvdb\\.com.*?id=([0-9]*)"), text)
+        }
+    }
+
+    private fun matchShowTvdbId(pattern: Pattern, text: String): Int {
+        val matcher = pattern.matcher(text)
+        if (matcher.find()) {
+            try {
+                return Integer.parseInt(matcher.group(1))
+            } catch (ignored: NumberFormatException) {
+            }
+        }
+        return -1
     }
 
 }
