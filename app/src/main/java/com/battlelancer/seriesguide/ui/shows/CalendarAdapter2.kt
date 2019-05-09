a820535e7c7c5b623ba1d797d9e3c03732f20004
Uwe Trottmann <uwe+git@uwetrottmann.com>
2019-05-09 13:23:30 +0200

    From sample: calendar headers using ItemDecoration, partially broken.

--- app/src/main/java/com/battlelancer/seriesguide/ui/shows/CalendarAdapter2.kt
+++ app/src/main/java/com/battlelancer/seriesguide/ui/shows/CalendarAdapter2.kt
@@ -1,11 +1,17 @@
 package com.battlelancer.seriesguide.ui.shows
 
 import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
+import com.battlelancer.seriesguide.R
 import com.battlelancer.seriesguide.ui.shows.CalendarFragment2ViewModel.CalendarItem
+import com.battlelancer.seriesguide.util.TimeTools
+import java.util.Date
 
 class CalendarAdapter2(private val context: Context) :
     ListAdapter<CalendarItem, RecyclerView.ViewHolder>(DIFF_CALLBACK) {
@@ -30,6 +36,18 @@ class CalendarAdapter2(private val context: Context) :
         val episode = getItem(itemPosition)
         val episodeBefore = getItem(itemPosition - 1)
         return episode.headerTime != episodeBefore.headerTime
+    }
+
+    fun getHeaderView(parent: ViewGroup, position: Int): View {
+        val headerTime = getItem(position).headerTime
+        val headerView = LayoutInflater.from(parent.context).inflate(
+            R.layout.item_grid_header,
+            parent,
+            false
+        ) as TextView
+        // display headers like "Mon in 3 days", also "today" when applicable
+        headerView.text = TimeTools.formatToLocalDayAndRelativeWeek(context, Date(headerTime))
+        return headerView
     }
 
     companion object {
--- app/src/main/java/com/battlelancer/seriesguide/ui/shows/HeaderDecoration.kt
+++ app/src/main/java/com/battlelancer/seriesguide/ui/shows/HeaderDecoration.kt
@@ -2,10 +2,15 @@ package com.battlelancer.seriesguide.ui.shows
 
 import android.graphics.Canvas
 import android.graphics.Rect
+import android.util.SparseArray
 import android.view.View
+import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 
 class HeaderDecoration : RecyclerView.ItemDecoration() {
+
+    // TODO recycle header views
+    private val headers = SparseArray<View>()
 
     override fun getItemOffsets(
         outRect: Rect,
@@ -13,19 +18,62 @@ class HeaderDecoration : RecyclerView.ItemDecoration() {
         parent: RecyclerView,
         state: RecyclerView.State
     ) {
-        val itemPosition = parent.getChildAdapterPosition(view)
-        val itemHasHeader = (parent.adapter as CalendarAdapter2).itemHasHeader(itemPosition)
-        // item with header + all items in same row :/
-        val topInset = if (itemHasHeader) {
-            100
-        } else {
-            0
+        val adapter = (parent.adapter as CalendarAdapter2)
+        val position = parent.getChildLayoutPosition(view)
+        if (position == RecyclerView.NO_POSITION
+            || !adapter.itemHasHeader(position)) {
+            headers.remove(position)
+            return
         }
-        outRect.set(0, topInset, 0, 0)
+
+        val headerView = adapter.getHeaderView(parent, position)
+        headers.put(position, headerView)
+
+        measureHeaderView(headerView, parent)
+        outRect.top = headerView.height
     }
 
-    override fun onDraw(c: Canvas, parent: RecyclerView, state: RecyclerView.State) {
-        super.onDraw(c, parent, state)
+    override fun onDraw(canvas: Canvas, parent: RecyclerView, state: RecyclerView.State) {
+        for (i in 0 until parent.childCount) {
+            val child = parent.getChildAt(i)
+            val position = parent.getChildAdapterPosition(child)
+            if (position != RecyclerView.NO_POSITION
+                && (parent.adapter as CalendarAdapter2).itemHasHeader(position)) {
+                canvas.save()
+                val headerView = headers.get(position)
+                canvas.translate(0f, child.y - headerView.height)
+                headerView.draw(canvas)
+                canvas.restore()
+            }
+        }
+    }
+
+    // TODO does not take into account item width
+    private fun measureHeaderView(view: View, parent: ViewGroup) {
+        if (view.layoutParams == null) {
+            view.layoutParams = ViewGroup.LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT
+            )
+        }
+        val displayMetrics = parent.context.resources.displayMetrics
+
+        val widthSpec =
+            View.MeasureSpec.makeMeasureSpec(displayMetrics.widthPixels, View.MeasureSpec.EXACTLY)
+        val heightSpec =
+            View.MeasureSpec.makeMeasureSpec(displayMetrics.heightPixels, View.MeasureSpec.EXACTLY)
+
+        val childWidth = ViewGroup.getChildMeasureSpec(
+            widthSpec,
+            parent.paddingLeft + parent.paddingRight, view.layoutParams.width
+        )
+        val childHeight = ViewGroup.getChildMeasureSpec(
+            heightSpec,
+            parent.paddingTop + parent.paddingBottom, view.layoutParams.height
+        )
+
+        view.measure(childWidth, childHeight)
+
+        view.layout(0, 0, view.measuredWidth, view.measuredHeight)
     }
 
 }
