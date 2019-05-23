90cc1663541a213579b665ad601ea58102aeea1a
Uwe Trottmann <uwe+git@uwetrottmann.com>
2019-05-23 11:03:04 +0200

    Extract CustomTabsHelper to module.
    Move FeedbackBroadcastReceiver to service package.

--- customtabs/src/main/java/com/uwetrottmann/seriesguide/customtabs/CustomTabsHelper.java
+++ customtabs/src/main/java/com/uwetrottmann/seriesguide/customtabs/CustomTabsHelper.java
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package com.battlelancer.seriesguide.customtabs;
+package com.uwetrottmann.seriesguide.customtabs;
 
 import android.content.Context;
 import android.content.Intent;
