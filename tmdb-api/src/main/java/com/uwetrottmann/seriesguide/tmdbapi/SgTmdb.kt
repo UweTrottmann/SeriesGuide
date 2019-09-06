b50241e626d4d36cea5488e1f4fd460472e36f0f
Uwe Trottmann <uwe+git@uwetrottmann.com>
2019-09-06 10:39:13 +0200

    tmdb-api: convert to Kotlin.

---
+++ tmdb-api/src/main/java/com/uwetrottmann/seriesguide/tmdbapi/SgTmdb.kt
@@ -0,0 +1,18 @@
+package com.uwetrottmann.seriesguide.tmdbapi
+
+import com.uwetrottmann.tmdb2.Tmdb
+import okhttp3.OkHttpClient
+
+/**
+ * Creates a custom [Tmdb] using the given API key and HTTP client.
+ */
+class SgTmdb(
+    private val okHttpClient: OkHttpClient,
+    apiKey: String
+) : Tmdb(apiKey) {
+
+    @Synchronized
+    override fun okHttpClient(): OkHttpClient {
+        return okHttpClient
+    }
+}
