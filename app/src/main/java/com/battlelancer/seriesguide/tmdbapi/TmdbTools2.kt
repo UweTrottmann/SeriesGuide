78d6a0d488faa924066bddd0bb2a937fbee7a6f0
Uwe Trottmann <uwe+git@uwetrottmann.com>
2019-09-13 10:10:13 +0200

    ShowsDiscoverLiveData: extract safe language code gen to TmdbTools2.
    - Do not require context for TmdbTools2.

--- app/src/main/java/com/battlelancer/seriesguide/tmdbapi/TmdbTools2.kt
+++ app/src/main/java/com/battlelancer/seriesguide/tmdbapi/TmdbTools2.kt
@@ -2,17 +2,17 @@ package com.battlelancer.seriesguide.tmdbapi
 
 import android.content.Context
 import com.battlelancer.seriesguide.SgApp
+import com.battlelancer.seriesguide.settings.DisplaySettings
 import com.battlelancer.seriesguide.util.Errors
 import com.uwetrottmann.tmdb2.enumerations.ExternalSource
 
-class TmdbTools2(context: Context) {
-
-    private val tmdb = SgApp.getServicesComponent(context.applicationContext).tmdb()
+class TmdbTools2 {
 
     /**
      * Tries to find the TMDB id for the given show's TheTVDB id. Returns null on error or failure.
      */
-    fun findShowTmdbId(showTvdbId: Int): Int? {
+    fun findShowTmdbId(context: Context, showTvdbId: Int): Int? {
+        val tmdb = SgApp.getServicesComponent(context.applicationContext).tmdb()
         try {
             val response = tmdb.findService()
                 .find(showTvdbId, ExternalSource.TVDB_ID, null)
@@ -33,6 +33,15 @@ class TmdbTools2(context: Context) {
         }
 
         return null
+    }
+
+    fun getSafeLanguageCode(languageCodeOrAny: String, anyCode: String): String {
+        return if (languageCodeOrAny == anyCode) {
+            // TMDB falls back to English if sending 'xx', so set to English beforehand.
+            DisplaySettings.LANGUAGE_EN
+        } else {
+            languageCodeOrAny
+        }
     }
 
 }
