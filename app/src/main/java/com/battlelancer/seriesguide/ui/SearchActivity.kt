490e4cface0fe3b539bbd4979ab84110de4f6569
Uwe Trottmann <uwe+git@uwetrottmann.com>
2019-11-06 14:45:40 +0100

    SearchActivity: correct mention of EpisodesActivity.

--- app/src/main/java/com/battlelancer/seriesguide/ui/SearchActivity.kt
+++ app/src/main/java/com/battlelancer/seriesguide/ui/SearchActivity.kt
@@ -20,7 +20,6 @@ import butterknife.ButterKnife
 import com.battlelancer.seriesguide.R
 import com.battlelancer.seriesguide.adapters.TabStripAdapter
 import com.battlelancer.seriesguide.settings.SearchSettings
-import com.battlelancer.seriesguide.ui.episodes.EpisodeDetailsActivity
 import com.battlelancer.seriesguide.ui.episodes.EpisodesActivity
 import com.battlelancer.seriesguide.ui.search.AddShowDialogFragment
 import com.battlelancer.seriesguide.ui.search.EpisodeSearchFragment
@@ -48,7 +47,7 @@ import kotlin.coroutines.CoroutineContext
 
 /**
  * Handles search intents and displays a [EpisodeSearchFragment] when needed or redirects
- * directly to an [EpisodeDetailsActivity].
+ * directly to an [EpisodesActivity].
  */
 class SearchActivity : BaseNavDrawerActivity(), CoroutineScope,
     AddShowDialogFragment.OnAddShowListener, SearchTriggerListener {
