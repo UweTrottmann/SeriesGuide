4161800df6042e020d22cb948b68f4ee5a567bdd
Uwe Trottmann <uwe+git@uwetrottmann.com>
2019-11-07 17:23:24 +0100

    EpisodesActivity: use ViewModel to load season info.

--- app/src/main/java/com/battlelancer/seriesguide/provider/EpisodeHelper.java
+++ app/src/main/java/com/battlelancer/seriesguide/provider/EpisodeHelper.java
@@ -2,6 +2,7 @@ package com.battlelancer.seriesguide.provider;
 
 import static com.battlelancer.seriesguide.provider.SeriesGuideDatabase.Tables.EPISODES;
 
+import androidx.annotation.Nullable;
 import androidx.paging.DataSource;
 import androidx.room.Dao;
 import androidx.room.Query;
@@ -9,6 +10,7 @@ import androidx.room.RawQuery;
 import androidx.sqlite.db.SupportSQLiteQuery;
 import com.battlelancer.seriesguide.model.EpisodeWithShow;
 import com.battlelancer.seriesguide.model.SgEpisode;
+import com.battlelancer.seriesguide.model.SgEpisodeSeasonAndShow;
 import com.battlelancer.seriesguide.model.SgShow;
 
 /**
@@ -22,6 +24,10 @@ public interface EpisodeHelper {
      */
     @Query("SELECT * FROM " + EPISODES + " LIMIT 1")
     SgEpisode getEpisode();
+
+    @Nullable
+    @Query("SELECT season_id, season, series_id  FROM episodes WHERE _id=:episodeTvdbId")
+    SgEpisodeSeasonAndShow getEpisodeMinimal(int episodeTvdbId);
 
     @RawQuery(observedEntities = {SgEpisode.class, SgShow.class})
     DataSource.Factory<Integer, EpisodeWithShow> getEpisodesWithShow(SupportSQLiteQuery query);
