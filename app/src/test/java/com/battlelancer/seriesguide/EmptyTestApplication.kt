a1fd4db14e60d566b3abd641638704d741e655cf
Uwe Trottmann <uwe+git@uwetrottmann.com>
2020-08-20 08:52:44 +0200

    Use empty application for Robolectric tests.

---
+++ app/src/test/java/com/battlelancer/seriesguide/EmptyTestApplication.kt
@@ -0,0 +1,9 @@
+package com.battlelancer.seriesguide
+
+import android.app.Application
+
+/**
+ * Empty [Application] to use with Robolectric @Config to avoid errors
+ * with components that require an actual device to initialize.
+ */
+class EmptyTestApplication : Application()
