f04527f78a97c63a7b4b18648a2e648d9dd6670d
Uwe Trottmann <uwe+git@uwetrottmann.com>
2020-10-16 11:00:11 +0200

    ShowsDiscoverLiveData: use coroutine to fetch results.

--- app/src/main/java/com/battlelancer/seriesguide/ui/search/ShowsDiscoverViewModel.kt
+++ app/src/main/java/com/battlelancer/seriesguide/ui/search/ShowsDiscoverViewModel.kt
@@ -2,9 +2,10 @@ package com.battlelancer.seriesguide.ui.search
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.viewModelScope
 
 class ShowsDiscoverViewModel(application: Application) : AndroidViewModel(application) {
 
-    val data = ShowsDiscoverLiveData(application)
+    val data = ShowsDiscoverLiveData(application, viewModelScope)
 
 }
