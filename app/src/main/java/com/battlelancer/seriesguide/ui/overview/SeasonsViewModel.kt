9ef4bd3cd4cfd92e2a3f4b5b1e6ba275500bb9be
Uwe Trottmann <uwe+git@uwetrottmann.com>
2020-10-16 10:31:07 +0200

    RemainingCountLiveData: use coroutine for calculation.

--- app/src/main/java/com/battlelancer/seriesguide/ui/overview/SeasonsViewModel.kt
+++ app/src/main/java/com/battlelancer/seriesguide/ui/overview/SeasonsViewModel.kt
@@ -2,9 +2,10 @@ package com.battlelancer.seriesguide.ui.overview
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.viewModelScope
 
 class SeasonsViewModel(application: Application): AndroidViewModel(application) {
 
-    val remainingCountData = RemainingCountLiveData(application)
+    val remainingCountData = RemainingCountLiveData(application, viewModelScope)
 
 }
