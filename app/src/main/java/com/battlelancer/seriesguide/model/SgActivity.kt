63f6366ac07bdee58c7f77136a3ff311d67744c0
Uwe Trottmann <uwe+git@uwetrottmann.com>
2021-01-20 10:38:40 +0100

    SgActivity: convert to Kotlin data class.

---
+++ app/src/main/java/com/battlelancer/seriesguide/model/SgActivity.kt
@@ -0,0 +1,25 @@
+package com.battlelancer.seriesguide.model
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.Index
+import androidx.room.PrimaryKey
+import com.battlelancer.seriesguide.provider.SeriesGuideContract.Activity
+import com.battlelancer.seriesguide.provider.SeriesGuideDatabase
+
+/**
+ * Note: ensure to use CONFLICT_REPLACE when inserting to mimic SQLite UNIQUE x ON CONFLICT REPLACE.
+ */
+@Entity(
+    tableName = SeriesGuideDatabase.Tables.ACTIVITY,
+    indices = [Index(value = [Activity.EPISODE_TVDB_OR_TMDB_ID], unique = true)]
+)
+data class SgActivity (
+    @PrimaryKey(autoGenerate = true) @ColumnInfo(name = Activity._ID) val id: Long?,
+    /**
+     * Unique string identifier.
+     */
+    @ColumnInfo(name = Activity.EPISODE_TVDB_ID) val episodeTvdbId: String,
+    @ColumnInfo(name = Activity.SHOW_TVDB_ID) val showTvdbId: String,
+    @ColumnInfo(name = Activity.TIMESTAMP_MS) val timestampMs: Long
+)
