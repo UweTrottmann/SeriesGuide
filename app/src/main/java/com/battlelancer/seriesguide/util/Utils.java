4af2c77712fd3834d549422afc4e7836653fd7d0
Uwe Trottmann <uwe+git@uwetrottmann.com>
2021-03-10 14:42:27 +0100

    Demote X pass log: info -> debug.

--- app/src/main/java/com/battlelancer/seriesguide/util/Utils.java
+++ app/src/main/java/com/battlelancer/seriesguide/util/Utils.java
@@ -131,7 +131,7 @@ public class Utils {
             }
         } catch (NameNotFoundException e) {
             // Expected exception that occurs if the package is not present.
-            Timber.i("X Pass not found.");
+            Timber.d("X Pass not found.");
         }
 
         return false;
