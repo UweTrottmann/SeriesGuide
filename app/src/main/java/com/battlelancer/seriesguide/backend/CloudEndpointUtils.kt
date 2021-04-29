ace7be89220dcf417b1726140b75e4960521707f
Uwe Trottmann <uwe+git@uwetrottmann.com>
2021-04-29 10:37:17 +0200

    CloudEndpointUtils: convert to Kotlin.

--- app/src/main/java/com/battlelancer/seriesguide/backend/CloudEndpointUtils.kt
+++ app/src/main/java/com/battlelancer/seriesguide/backend/CloudEndpointUtils.kt
@@ -1,9 +1,11 @@
-package com.battlelancer.seriesguide.backend;
+package com.battlelancer.seriesguide.backend
 
-import android.content.Context;
-import com.battlelancer.seriesguide.BuildConfig;
-import com.battlelancer.seriesguide.util.Utils;
-import com.google.api.client.googleapis.services.AbstractGoogleClient;
+import android.content.Context
+import com.battlelancer.seriesguide.BuildConfig
+import com.battlelancer.seriesguide.util.Utils
+import com.google.api.client.googleapis.services.AbstractGoogleClient
+import com.google.api.client.googleapis.services.AbstractGoogleClientRequest
+import com.google.api.client.googleapis.services.GoogleClientRequestInitializer
 
 /**
  * Common utilities for working with Cloud Endpoints.
@@ -13,26 +15,26 @@ import com.google.api.client.googleapis.services.AbstractGoogleClient;
  *
  * See https://cloud.google.com/appengine/docs/java/endpoints
  */
-public class CloudEndpointUtils {
+object CloudEndpointUtils {
 
-    private static final String PATH_API = "/_ah/api/";
+    private const val PATH_API = "/_ah/api/"
 
     /**
      * Change this to 'true' if you're running your backend locally using the DevAppServer.
      */
-    @SuppressWarnings("PointlessBooleanExpression")
-    protected static final boolean USE_LOCAL_VERSION = false && BuildConfig.DEBUG;
+    @Suppress("SimplifyBooleanWithConstants")
+    private val USE_LOCAL_VERSION = false && BuildConfig.DEBUG
 
-    @SuppressWarnings("PointlessBooleanExpression")
-    private static final boolean USE_STAGING_VERSION = false && BuildConfig.DEBUG;
+    @Suppress("SimplifyBooleanWithConstants")
+    private val USE_STAGING_VERSION = false && BuildConfig.DEBUG
 
-    private static final String ROOT_URL_STAGING = "";
+    private const val ROOT_URL_STAGING = ""
 
     /**
      * The root URL of where your DevAppServer is running (if you're running the DevAppServer
      * locally).
      */
-    private static final String ROOT_URL_LOCALHOST = "http://localhost:8080/";
+    private const val ROOT_URL_LOCALHOST = "http://localhost:8080/"
 
     /**
      * The root URL of where your DevAppServer is running when it's being accessed via the Android
@@ -40,7 +42,7 @@ public class CloudEndpointUtils {
      * Android's virtual router. See http://developer.android.com/tools/devices/emulator.html#networkaddresses
      * for more information.
      */
-    private static final String ROOT_URL_LOCALHOST_ANDROID = "http://10.0.2.2:8080";
+    private const val ROOT_URL_LOCALHOST_ANDROID = "http://10.0.2.2:8080"
 
     /**
      * Updates the Google client builder to connect the appropriate server based on whether
@@ -49,25 +51,29 @@ public class CloudEndpointUtils {
      * @param builder Google client builder
      * @return same Google client builder
      */
-    public static <B extends AbstractGoogleClient.Builder> B updateBuilder(Context context,
-            B builder) {
+    @JvmStatic
+    fun <B : AbstractGoogleClient.Builder> updateBuilder(
+        context: Context,
+        builder: B
+    ): B {
         if (USE_LOCAL_VERSION) {
-            builder.setRootUrl(ROOT_URL_LOCALHOST_ANDROID + PATH_API);
+            builder.rootUrl = ROOT_URL_LOCALHOST_ANDROID + PATH_API
         } else if (USE_STAGING_VERSION) {
-            builder.setRootUrl(ROOT_URL_STAGING + PATH_API);
+            builder.rootUrl = ROOT_URL_STAGING + PATH_API
         }
         // used for user agent
-        builder.setApplicationName("SeriesGuide " + Utils.getVersion(context));
+        builder.applicationName = "SeriesGuide " + Utils.getVersion(context)
 
         // only enable GZip when connecting to remote server
-        final boolean enableGZip = builder.getRootUrl().startsWith("https:");
+        val enableGZip = builder.rootUrl.startsWith("https:")
 
-        builder.setGoogleClientRequestInitializer(request -> {
-            if (!enableGZip) {
-                request.setDisableGZipContent(true);
+        builder.googleClientRequestInitializer =
+            GoogleClientRequestInitializer { request: AbstractGoogleClientRequest<*> ->
+                if (!enableGZip) {
+                    request.disableGZipContent = true
+                }
             }
-        });
 
-        return builder;
+        return builder
     }
 }
