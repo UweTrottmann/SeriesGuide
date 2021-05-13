6fd1a0d046f27e127f2d9472dc233bedd4aeafdf
Uwe Trottmann <uwe+git@uwetrottmann.com>
2021-05-13 08:49:54 +0200

    Import: do import legacy list items again.

--- app/src/main/java/com/battlelancer/seriesguide/dataliberation/JsonImportTask.java
+++ app/src/main/java/com/battlelancer/seriesguide/dataliberation/JsonImportTask.java
@@ -419,12 +419,17 @@ public class JsonImportTask extends AsyncTask<Void, Integer, Integer> {
         // Insert the lists items
         ArrayList<ContentValues> items = new ArrayList<>();
         for (ListItem item : list.items) {
-            // Note: do not import legacy types (seasons and episodes).
+            // Note: DO import legacy types (seasons and episodes),
+            // as e.g. older backups can still contain legacy show data to allow displaying them.
             int type;
             if (ListItemTypesExport.SHOW.equals(item.type)) {
                 type = ListItemTypes.TVDB_SHOW;
             } else if (ListItemTypesExport.TMDB_SHOW.equals(item.type)) {
                 type = ListItemTypes.TMDB_SHOW;
+            } else if (ListItemTypesExport.SEASON.equals(item.type)) {
+                type = ListItemTypes.SEASON;
+            } else if (ListItemTypesExport.EPISODE.equals(item.type)) {
+                type = ListItemTypes.EPISODE;
             } else {
                 // Unknown item type, skip
                 continue;
