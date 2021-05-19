d1226e0ff2e21c8f0da4a7e05fd41519680900d9
Uwe Trottmann <uwe+git@uwetrottmann.com>
2021-05-19 15:40:06 +0200

    Add toolbar to about screen.

--- app/src/main/java/com/battlelancer/seriesguide/ui/preferences/AboutActivity.kt
+++ app/src/main/java/com/battlelancer/seriesguide/ui/preferences/AboutActivity.kt
@@ -1,7 +1,9 @@
 package com.battlelancer.seriesguide.ui.preferences
 
 import android.os.Bundle
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import com.battlelancer.seriesguide.R
 
 /**
@@ -11,5 +13,23 @@ class AboutActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_about)
+        setupActionBar()
     }
+
+    private fun setupActionBar() {
+        val toolbar = findViewById<Toolbar>(R.id.sgToolbar)
+        setSupportActionBar(toolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            android.R.id.home -> {
+                onBackPressed()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
 }
