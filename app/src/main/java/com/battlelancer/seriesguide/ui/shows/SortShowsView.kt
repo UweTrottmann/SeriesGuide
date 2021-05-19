283b1526f214d3ff35644922ed540d8088440444
Uwe Trottmann <uwe+git@uwetrottmann.com>
2021-05-19 13:40:25 +0200

    View binding: use in SortShowsView.

--- app/src/main/java/com/battlelancer/seriesguide/ui/shows/SortShowsView.kt
+++ app/src/main/java/com/battlelancer/seriesguide/ui/shows/SortShowsView.kt
@@ -3,13 +3,9 @@ package com.battlelancer.seriesguide.ui.shows
 import android.content.Context
 import android.util.AttributeSet
 import android.view.LayoutInflater
-import android.widget.CheckBox
 import android.widget.LinearLayout
-import android.widget.RadioButton
-import android.widget.RadioGroup
-import butterknife.BindView
-import butterknife.ButterKnife
 import com.battlelancer.seriesguide.R
+import com.battlelancer.seriesguide.databinding.ViewSortShowsBinding
 import com.battlelancer.seriesguide.settings.DisplaySettings
 import com.battlelancer.seriesguide.ui.shows.ShowsDistillationSettings.ShowsSortOrder
 
@@ -18,59 +14,43 @@ class SortShowsView @JvmOverloads constructor(
     defStyleAttr: Int = 0
 ) : LinearLayout(context, attrs, defStyleAttr) {
 
+    private val binding: ViewSortShowsBinding
+
     init {
         orientation = VERTICAL
-        LayoutInflater.from(context).inflate(R.layout.view_sort_shows, this)
 
         // can't do in onFinishInflate as that is only called when inflating from XML
-        ButterKnife.bind(this)
+        binding = ViewSortShowsBinding.inflate(LayoutInflater.from(context), this)
 
-        radioSortTitle.setOnClickListener { updateSortOrderListener() }
-        radioSortLatestEpisode.setOnClickListener { updateSortOrderListener() }
-        radioSortOldestEpisode.setOnClickListener { updateSortOrderListener() }
-        radioSortLastWatched.setOnClickListener { updateSortOrderListener() }
-        radioSortRemaining.setOnClickListener { updateSortOrderListener() }
-        radio_shows_sort_status.setOnClickListener { updateSortOrderListener() }
-        checkBoxFavoritesFirst.setOnClickListener { updateSortOrderListener() }
-        checkBoxIgnoreArticles.setOnClickListener { updateSortOrderListener(true) }
+        binding.apply {
+            radioShowsSortTitle.setOnClickListener { updateSortOrderListener() }
+            radioShowsSortLatestEpisode.setOnClickListener { updateSortOrderListener() }
+            radioShowsSortOldestEpisode.setOnClickListener { updateSortOrderListener() }
+            radioShowsSortLastWatched.setOnClickListener { updateSortOrderListener() }
+            radioShowsSortRemaining.setOnClickListener { updateSortOrderListener() }
+            radioShowsSortStatus.setOnClickListener { updateSortOrderListener() }
+            checkboxShowsSortFavorites.setOnClickListener { updateSortOrderListener() }
+            checkboxShowsSortIgnoreArticles.setOnClickListener { updateSortOrderListener(true) }
+        }
     }
-
-    @BindView(R.id.radio_group_shows_sort)
-    internal lateinit var radioGroup: RadioGroup
-    @BindView(R.id.radio_shows_sort_title)
-    internal lateinit var radioSortTitle: RadioButton
-    @BindView(R.id.radio_shows_sort_latest_episode)
-    internal lateinit var radioSortLatestEpisode: RadioButton
-    @BindView(R.id.radio_shows_sort_oldest_episode)
-    internal lateinit var radioSortOldestEpisode: RadioButton
-    @BindView(R.id.radio_shows_sort_last_watched)
-    internal lateinit var radioSortLastWatched: RadioButton
-    @BindView(R.id.radio_shows_sort_remaining)
-    internal lateinit var radioSortRemaining: RadioButton
-    @BindView(R.id.radio_shows_sort_status)
-    internal lateinit var radio_shows_sort_status: RadioButton
-    @BindView(R.id.checkbox_shows_sort_favorites)
-    internal lateinit var checkBoxFavoritesFirst: CheckBox
-    @BindView(R.id.checkbox_shows_sort_ignore_articles)
-    internal lateinit var checkBoxIgnoreArticles: CheckBox
 
     private var sortOrderListener: SortOrderListener? = null
 
     private fun updateSortOrderListener(changedIgnoreArticles: Boolean = false) {
-        val sortOrderId = when (radioGroup.checkedRadioButtonId) {
+        val sortOrderId = when (binding.radioGroupShowsSort.checkedRadioButtonId) {
             R.id.radio_shows_sort_title -> ShowsSortOrder.TITLE_ID
             R.id.radio_shows_sort_latest_episode -> ShowsSortOrder.LATEST_EPISODE_ID
             R.id.radio_shows_sort_oldest_episode -> ShowsSortOrder.OLDEST_EPISODE_ID
             R.id.radio_shows_sort_last_watched -> ShowsSortOrder.LAST_WATCHED_ID
             R.id.radio_shows_sort_remaining -> ShowsSortOrder.LEAST_REMAINING_EPISODES_ID
             R.id.radio_shows_sort_status -> ShowsSortOrder.STATUS
-            else -> throw IllegalArgumentException("Unknown radio button id ${radioGroup.checkedRadioButtonId}")
+            else -> throw IllegalArgumentException("Unknown radio button id ${binding.radioGroupShowsSort.checkedRadioButtonId}")
         }
         sortOrderListener?.onSortOrderUpdate(
             ShowSortOrder(
                 sortOrderId,
-                checkBoxFavoritesFirst.isChecked,
-                checkBoxIgnoreArticles.isChecked,
+                binding.checkboxShowsSortFavorites.isChecked,
+                binding.checkboxShowsSortIgnoreArticles.isChecked,
                 changedIgnoreArticles
             )
         )
@@ -86,9 +66,9 @@ class SortShowsView @JvmOverloads constructor(
             ShowsSortOrder.STATUS -> R.id.radio_shows_sort_status
             else -> R.id.radio_shows_sort_title // fall back to default
         }
-        radioGroup.check(radioButtonId)
-        checkBoxFavoritesFirst.isChecked = showSortOrder.isSortFavoritesFirst
-        checkBoxIgnoreArticles.isChecked = showSortOrder.isSortIgnoreArticles
+        binding.radioGroupShowsSort.check(radioButtonId)
+        binding.checkboxShowsSortFavorites.isChecked = showSortOrder.isSortFavoritesFirst
+        binding.checkboxShowsSortIgnoreArticles.isChecked = showSortOrder.isSortIgnoreArticles
     }
 
     fun setSortOrderListener(sortOrderListener: SortOrderListener) {
