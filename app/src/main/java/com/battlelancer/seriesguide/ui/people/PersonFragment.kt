d13dd11b405b4679d62e60ba53272494b51cbc2c
Uwe Trottmann <uwe+git@uwetrottmann.com>
2021-06-09 15:11:35 +0200

    View binding: use in PersonFragment.

--- app/src/main/java/com/battlelancer/seriesguide/ui/people/PersonFragment.kt
+++ app/src/main/java/com/battlelancer/seriesguide/ui/people/PersonFragment.kt
@@ -10,20 +10,12 @@ import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
 import android.view.animation.AnimationUtils
-import android.widget.Button
-import android.widget.ImageView
-import android.widget.ProgressBar
-import android.widget.TextView
 import android.widget.Toast
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import butterknife.BindView
-import butterknife.ButterKnife
-import butterknife.OnClick
-import butterknife.OnLongClick
-import butterknife.Unbinder
 import com.battlelancer.seriesguide.R
+import com.battlelancer.seriesguide.databinding.FragmentPersonBinding
 import com.battlelancer.seriesguide.settings.DisplaySettings
 import com.battlelancer.seriesguide.ui.dialogs.L10nDialogFragment
 import com.battlelancer.seriesguide.util.ServiceUtils
@@ -42,24 +34,7 @@ import org.greenrobot.eventbus.ThreadMode
  */
 class PersonFragment : Fragment() {
 
-    @BindView(R.id.progressBarPerson)
-    lateinit var progressBar: ProgressBar
-
-    @BindView(R.id.imageViewPersonHeadshot)
-    lateinit var imageViewHeadshot: ImageView
-
-    @BindView(R.id.textViewPersonName)
-    lateinit var textViewName: TextView
-
-    @BindView(R.id.textViewPersonBiography)
-    lateinit var textViewBiography: TextView
-
-    @BindView(R.id.buttonPersonTmdbLink)
-    lateinit var buttonTmdbLink: Button
-
-    @BindView(R.id.buttonPersonWebSearch)
-    lateinit var buttonWebSearch: Button
-    private lateinit var unbinder: Unbinder
+    private var binding: FragmentPersonBinding? = null
 
     private var personTmdbId: Int = 0
     private var person: Person? = null
@@ -78,14 +53,30 @@ class PersonFragment : Fragment() {
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        val rootView = inflater.inflate(R.layout.fragment_person, container, false)
-        unbinder = ButterKnife.bind(this, rootView)
-        return rootView
+    ): View {
+        return FragmentPersonBinding.inflate(inflater, container, false).also {
+            binding = it
+        }.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        binding?.apply {
+            buttonPersonTmdbLink.setOnClickListener {
+                person?.id?.let { TmdbTools.openTmdbPerson(activity, it) }
+            }
+            buttonPersonTmdbLink.setOnLongClickListener {
+                person?.id?.let {
+                    copyTextToClipboard(view.context, TmdbTools.buildPersonUrl(it))
+                    true
+                }
+                false
+            }
+            buttonPersonWebSearch.setOnClickListener {
+                person?.let { ServiceUtils.performWebSearch(activity, it.name) }
+            }
+        }
 
         setProgressVisibility(true)
         model.personLiveData.observe(viewLifecycleOwner, {
@@ -125,8 +116,7 @@ class PersonFragment : Fragment() {
 
     override fun onDestroyView() {
         super.onDestroyView()
-
-        unbinder.unbind()
+        binding = null
     }
 
     @Subscribe(threadMode = ThreadMode.MAIN)
@@ -135,84 +125,72 @@ class PersonFragment : Fragment() {
         model.languageCode.value = event.selectedLanguageCode
     }
 
-    @OnClick(R.id.buttonPersonTmdbLink)
-    fun onClickButtonTmdbLink() {
-        person?.id?.let { TmdbTools.openTmdbPerson(activity, it) }
-    }
-
-    @OnLongClick(R.id.buttonPersonTmdbLink)
-    fun onLongClickButtonTmdbLink(view: View): Boolean {
-        person?.id?.let {
-            copyTextToClipboard(view.context, TmdbTools.buildPersonUrl(it))
-            return true
-        }
-        return false
-    }
-
-    @OnClick(R.id.buttonPersonWebSearch)
-    fun onClickButtonWebSearch() {
-        person?.let { ServiceUtils.performWebSearch(activity, it.name) }
-    }
-
     private fun populatePersonViews(person: Person?) {
-        if (person == null) {
-            if (AndroidUtils.isNetworkConnected(requireContext())) {
-                textViewBiography.text = getString(
-                    R.string.api_error_generic,
-                    getString(R.string.tmdb)
+        binding?.apply {
+            if (person == null) {
+                if (AndroidUtils.isNetworkConnected(requireContext())) {
+                    textViewPersonBiography.text = getString(
+                        R.string.api_error_generic,
+                        getString(R.string.tmdb)
+                    )
+                } else {
+                    Toast.makeText(activity, R.string.offline, Toast.LENGTH_SHORT).show()
+                    textViewPersonBiography.text = getString(R.string.offline)
+                }
+                return
+            }
+
+            this@PersonFragment.person = person
+
+            textViewPersonName.text = person.name
+            val biography = if (TextUtils.isEmpty(person.biography)) {
+                TextToolsK.textNoTranslationMovieLanguage(
+                    requireContext(),
+                    model.languageCode.value
                 )
             } else {
-                Toast.makeText(activity, R.string.offline, Toast.LENGTH_SHORT).show()
-                textViewBiography.text = getString(R.string.offline)
+                person.biography
             }
-            return
+            textViewPersonBiography.text = TextTools.textWithTmdbSource(
+                textViewPersonBiography.context,
+                biography
+            )
+
+            if (!TextUtils.isEmpty(person.profile_path)) {
+                ServiceUtils.loadWithPicasso(
+                    activity,
+                    TmdbTools.buildProfileImageUrl(
+                        activity, person.profile_path, TmdbTools.ProfileImageSize.H632
+                    )
+                ).placeholder(
+                    ColorDrawable(
+                        ContextCompat.getColor(requireContext(), R.color.protection_dark)
+                    )
+                ).into(imageViewPersonHeadshot)
+            }
+
+            // show actions
+            requireActivity().invalidateOptionsMenu()
         }
-
-        this.person = person
-
-        textViewName.text = person.name
-        val biography = if (TextUtils.isEmpty(person.biography)) {
-            TextToolsK.textNoTranslationMovieLanguage(requireContext(), model.languageCode.value)
-        } else {
-            person.biography
-        }
-        textViewBiography.text = TextTools.textWithTmdbSource(
-            textViewBiography.context,
-            biography
-        )
-
-        if (!TextUtils.isEmpty(person.profile_path)) {
-            ServiceUtils.loadWithPicasso(
-                activity,
-                TmdbTools.buildProfileImageUrl(
-                    activity, person.profile_path, TmdbTools.ProfileImageSize.H632
-                )
-            ).placeholder(
-                ColorDrawable(
-                    ContextCompat.getColor(requireContext(), R.color.protection_dark)
-                )
-            ).into(imageViewHeadshot)
-        }
-
-        // show actions
-        requireActivity().invalidateOptionsMenu()
     }
 
     /**
      * Shows or hides a custom indeterminate progress indicator inside this activity layout.
      */
     private fun setProgressVisibility(isVisible: Boolean) {
-        if (progressBar.visibility == (if (isVisible) View.VISIBLE else View.GONE)) {
-            // already in desired state, avoid replaying animation
-            return
-        }
-        progressBar.startAnimation(
-            AnimationUtils.loadAnimation(
-                progressBar.context,
-                if (isVisible) R.anim.fade_in else R.anim.fade_out
+        binding?.apply {
+            if (progressBarPerson.visibility == (if (isVisible) View.VISIBLE else View.GONE)) {
+                // already in desired state, avoid replaying animation
+                return
+            }
+            progressBarPerson.startAnimation(
+                AnimationUtils.loadAnimation(
+                    progressBarPerson.context,
+                    if (isVisible) R.anim.fade_in else R.anim.fade_out
+                )
             )
-        )
-        progressBar.visibility = if (isVisible) View.VISIBLE else View.GONE
+            progressBarPerson.visibility = if (isVisible) View.VISIBLE else View.GONE
+        }
     }
 
     companion object {
