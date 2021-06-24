da5c45e7446934e051d1dcbad026b6e571ce1ca1
Uwe Trottmann <uwe+git@uwetrottmann.com>
2021-06-24 08:42:52 +0200

    Update versions plugin [0.38.0 -> 0.39.0]

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     // https://github.com/ben-manes/gradle-versions-plugin/releases
-    id("com.github.ben-manes.versions") version "0.38.0"
+    id("com.github.ben-manes.versions") version "0.39.0"
     // https://github.com/Codearte/gradle-nexus-staging-plugin/releases
     id("io.codearte.nexus-staging") version "0.22.0" // api
 }
