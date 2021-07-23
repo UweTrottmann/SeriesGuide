48b644ff8863a6e20240e99ac3602b0ece2d18cb
Uwe Trottmann <uwe+git@uwetrottmann.com>
2021-07-23 11:54:06 +0200

    Kotlin: convert DataLiberationTools.

--- app/src/main/java/com/battlelancer/seriesguide/dataliberation/DataLiberationTools.kt
+++ app/src/main/java/com/battlelancer/seriesguide/dataliberation/DataLiberationTools.kt
@@ -1,87 +1,72 @@
-package com.battlelancer.seriesguide.dataliberation;
+package com.battlelancer.seriesguide.dataliberation
 
-import android.annotation.TargetApi;
-import android.content.Intent;
-import android.os.Build;
-import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
-import com.battlelancer.seriesguide.dataliberation.JsonExportTask.ShowStatusExport;
-import com.battlelancer.seriesguide.ui.shows.ShowTools.Status;
-import com.battlelancer.seriesguide.util.Utils;
+import android.annotation.TargetApi
+import android.content.Intent
+import android.os.Build
+import androidx.fragment.app.Fragment
+import com.battlelancer.seriesguide.dataliberation.JsonExportTask.ShowStatusExport
+import com.battlelancer.seriesguide.ui.shows.ShowTools.Status
+import com.battlelancer.seriesguide.util.Utils
 
-public class DataLiberationTools {
+object DataLiberationTools {
 
     /**
-     * Transform a string representation of {@link ShowStatusExport}
-     * to a {@link Status} to be stored in the database.
+     * Transform a string representation of [ShowStatusExport]
+     * to a [Status] to be stored in the database.
      *
-     * <p>Falls back to {@link Status#UNKNOWN}.
+     * Falls back to [Status.UNKNOWN].
      */
-    public static int encodeShowStatus(@Nullable String status) {
-        if (status == null) {
-            return Status.UNKNOWN;
-        }
-        switch (status) {
-            case ShowStatusExport.IN_PRODUCTION:
-                return Status.IN_PRODUCTION;
-            case ShowStatusExport.PILOT:
-                return Status.PILOT;
-            case ShowStatusExport.CANCELED:
-                return Status.CANCELED;
-            case ShowStatusExport.UPCOMING:
-                return Status.PLANNED;
-            case ShowStatusExport.CONTINUING:
-                return Status.RETURNING;
-            case ShowStatusExport.ENDED:
-                return Status.ENDED;
-            default:
-                return Status.UNKNOWN;
+    fun encodeShowStatus(status: String?): Int {
+        return if (status == null) {
+            Status.UNKNOWN
+        } else when (status) {
+            ShowStatusExport.IN_PRODUCTION -> Status.IN_PRODUCTION
+            ShowStatusExport.PILOT -> Status.PILOT
+            ShowStatusExport.CANCELED -> Status.CANCELED
+            ShowStatusExport.UPCOMING -> Status.PLANNED
+            ShowStatusExport.CONTINUING -> Status.RETURNING
+            ShowStatusExport.ENDED -> Status.ENDED
+            else -> Status.UNKNOWN
         }
     }
 
     /**
-     * Transform an int representation of {@link Status}
-     * to a {@link ShowStatusExport} to
+     * Transform an int representation of [Status]
+     * to a [ShowStatusExport] to
      * be used for exporting data.
      *
-     * @param encodedStatus Detection based on {@link Status}.
+     * @param encodedStatus Detection based on [Status].
      */
-    public static String decodeShowStatus(int encodedStatus) {
-        switch (encodedStatus) {
-            case Status.IN_PRODUCTION:
-                return ShowStatusExport.IN_PRODUCTION;
-            case Status.PILOT:
-                return ShowStatusExport.PILOT;
-            case Status.CANCELED:
-                return ShowStatusExport.CANCELED;
-            case Status.PLANNED:
-                return ShowStatusExport.UPCOMING;
-            case Status.RETURNING:
-                return ShowStatusExport.CONTINUING;
-            case Status.ENDED:
-                return ShowStatusExport.ENDED;
-            default:
-                return ShowStatusExport.UNKNOWN;
+    fun decodeShowStatus(encodedStatus: Int): String {
+        return when (encodedStatus) {
+            Status.IN_PRODUCTION -> ShowStatusExport.IN_PRODUCTION
+            Status.PILOT -> ShowStatusExport.PILOT
+            Status.CANCELED -> ShowStatusExport.CANCELED
+            Status.PLANNED -> ShowStatusExport.UPCOMING
+            Status.RETURNING -> ShowStatusExport.CONTINUING
+            Status.ENDED -> ShowStatusExport.ENDED
+            else -> ShowStatusExport.UNKNOWN
         }
     }
 
     @TargetApi(Build.VERSION_CODES.KITKAT)
-    public static void selectExportFile(Fragment fragment, String suggestedFileName,
-            int requestCode) {
-        Intent intent = new Intent(Intent.ACTION_CREATE_DOCUMENT);
+    fun selectExportFile(
+        fragment: Fragment?, suggestedFileName: String?,
+        requestCode: Int
+    ) {
+        val intent = Intent(Intent.ACTION_CREATE_DOCUMENT)
         // Filter to only show results that can be "opened", such as
         // a file (as opposed to a list of contacts or timezones).
-        intent.addCategory(Intent.CATEGORY_OPENABLE);
+        intent.addCategory(Intent.CATEGORY_OPENABLE)
 
         // do NOT use the probably correct application/json as it would prevent selecting existing
         // backup files on Android, which re-classifies them as application/octet-stream.
         // also do NOT use application/octet-stream as it prevents selecting backup files from
         // providers where the correct application/json mime type is used, *sigh*
         // so, use application/* and let the provider decide
-        intent.setType("application/*");
-        intent.putExtra(Intent.EXTRA_TITLE, suggestedFileName);
+        intent.type = "application/*"
+        intent.putExtra(Intent.EXTRA_TITLE, suggestedFileName)
 
-        Utils.tryStartActivityForResult(fragment, intent, requestCode);
+        Utils.tryStartActivityForResult(fragment, intent, requestCode)
     }
-
 }
