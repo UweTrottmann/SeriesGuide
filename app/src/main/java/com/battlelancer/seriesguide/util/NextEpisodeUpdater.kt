48f4b45e20cedd13f6deac40aa2da28255f20f3f
Uwe Trottmann <uwe+git@uwetrottmann.com>
2021-10-20 11:29:11 +0200

    NextEpisodeUpdater: revert back to choosing next episode by release date.
    Considering Anime this was a bad change, as special episodes are very common.
    Also there is a setting to exclude specials if users do not want to see them.

--- app/src/main/java/com/battlelancer/seriesguide/util/NextEpisodeUpdater.kt
+++ app/src/main/java/com/battlelancer/seriesguide/util/NextEpisodeUpdater.kt
@@ -53,31 +53,33 @@ class NextEpisodeUpdater {
             // STEP 1: get last watched episode details
             var season = show.seasonNumber
             var number = show.episodeNumber
+            var releaseTime = show.episodeReleaseDateMs
             val plays = if (show.episodePlays == null || show.episodePlays == 0) {
                 1
             } else show.episodePlays
             // Note: Due to LEFT JOIN query, episode values are null if no matching episode found.
-            if (show.lastWatchedEpisodeId == 0L || season == null || number == null) {
-                // by default: no watched episodes, include all starting with specials season 0
+            if (show.lastWatchedEpisodeId == 0L
+                || season == null || number == null || releaseTime == null) {
+                // by default: no watched episodes, include all starting with special 0
                 season = -1
                 number = -1
+                releaseTime = Long.MIN_VALUE
             }
 
-            // STEP 2: get episode with less plays and closest higher number,
-            // otherwise first of higher season.
-            // Note: previously this selected the next episode based
-            // on it having a higher release date. This was done to handle special episodes
-            // releasing in between regular episodes. However, when e.g. skipping all special
-            // episodes this may select an unexpected regular episode.
+            // STEP 2: get episode released closest afterwards
+            // (next episode or special episode - important for Anime, also often special episodes
+            // are not added in the order they are released, see e.g. Daemon Slayer);
+            // or at the same time, but with a different number (if all episodes are released at once).
+            // Note: there is a setting to exclude special episodes.
             val selectionArgs: Array<Any> = if (isNoReleasedEpisodes) {
                 // restrict to episodes with future release date
                 arrayOf(
-                    plays, season, number, season, currentTime
+                    plays, releaseTime, number, season, releaseTime, currentTime
                 )
             } else {
-                // any episodes
+                // restrict to episodes with any valid air date
                 arrayOf(
-                    plays, season, number, season
+                    plays, releaseTime, number, season, releaseTime
                 )
             }
             val episodeOrNull = episodeHelper
@@ -177,23 +179,24 @@ class NextEpisodeUpdater {
         const val UNKNOWN_NEXT_RELEASE_DATE = Long.MAX_VALUE
 
         /**
-         * Less plays, not skipped, if in the same season has a higher number,
-         * otherwise has a higher season.
+         * Less plays, not skipped, released later
+         * or has a different (to also match specials) number or season if released the same time.
          */
         private const val SELECT_NEXT =
             (SgEpisode2Columns.PLAYS + "<? AND " + SgEpisode2Columns.SELECTION_NOT_SKIPPED + " AND ("
-                    + "(" + SgEpisode2Columns.SEASON + "=? AND " + SgEpisode2Columns.NUMBER + ">?) "
-                    + "OR " + SgEpisode2Columns.SEASON + ">?"
-                    + ")")
+                    + "(" + SgEpisode2Columns.FIRSTAIREDMS + "=? AND "
+                    + "(" + SgEpisode2Columns.NUMBER + "!=? OR " + SgEpisode2Columns.SEASON + "!=?)) "
+                    + "OR " + SgEpisode2Columns.FIRSTAIREDMS + ">?)")
 
         private const val SELECT_WITHAIRDATE = " AND " + SgEpisode2Columns.FIRSTAIREDMS + "!=-1"
 
         private const val SELECT_ONLYFUTURE = " AND " + SgEpisode2Columns.FIRSTAIREDMS + ">=?"
 
         /**
-         * Lowest season first, or if identical lowest episode number.
+         * Oldest release date first, then lowest season, then lowest episode number.
          */
-        private const val SORTORDER = (SgEpisode2Columns.SEASON + " ASC,"
+        private const val SORTORDER = (SgEpisode2Columns.FIRSTAIREDMS + " ASC,"
+                + SgEpisode2Columns.SEASON + " ASC,"
                 + SgEpisode2Columns.NUMBER + " ASC")
     }
 
