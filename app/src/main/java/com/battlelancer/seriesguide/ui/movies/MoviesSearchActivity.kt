293ad8eb6336cb8f181e1ae98a8a66abb6caeab4
Uwe Trottmann <uwe+git@uwetrottmann.com>
2021-11-25 12:13:22 +0100

    Kotlin: convert MoviesSearchActivity.

--- app/src/main/java/com/battlelancer/seriesguide/ui/movies/MoviesSearchActivity.kt
+++ app/src/main/java/com/battlelancer/seriesguide/ui/movies/MoviesSearchActivity.kt
@@ -1,209 +1,203 @@
-package com.battlelancer.seriesguide.ui.movies;
+package com.battlelancer.seriesguide.ui.movies
 
-import android.os.Bundle;
-import android.view.KeyEvent;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
-import android.view.inputmethod.EditorInfo;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.AutoCompleteTextView;
-import android.widget.TextView;
-import androidx.appcompat.app.ActionBar;
-import androidx.fragment.app.Fragment;
-import butterknife.BindView;
-import butterknife.ButterKnife;
-import com.battlelancer.seriesguide.R;
-import com.battlelancer.seriesguide.settings.SearchSettings;
-import com.battlelancer.seriesguide.ui.BaseMessageActivity;
-import com.battlelancer.seriesguide.util.SearchHistory;
-import com.battlelancer.seriesguide.util.ViewTools;
-import com.google.android.material.textfield.TextInputLayout;
-import org.greenrobot.eventbus.Subscribe;
-import org.greenrobot.eventbus.ThreadMode;
+import android.os.Bundle
+import android.view.KeyEvent
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+import android.view.inputmethod.EditorInfo
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.TextView
+import android.widget.TextView.OnEditorActionListener
+import com.battlelancer.seriesguide.R
+import com.battlelancer.seriesguide.databinding.ActivityMoviesSearchBinding
+import com.battlelancer.seriesguide.settings.SearchSettings
+import com.battlelancer.seriesguide.ui.BaseMessageActivity
+import com.battlelancer.seriesguide.ui.movies.MovieLocalizationDialogFragment.Companion.show
+import com.battlelancer.seriesguide.ui.movies.MovieLocalizationDialogFragment.LocalizationChangedEvent
+import com.battlelancer.seriesguide.ui.movies.MoviesSearchFragment.Companion.newInstance
+import com.battlelancer.seriesguide.ui.movies.MoviesSearchFragment.OnSearchClickListener
+import com.battlelancer.seriesguide.util.SearchHistory
+import com.battlelancer.seriesguide.util.ViewTools
+import org.greenrobot.eventbus.Subscribe
+import org.greenrobot.eventbus.ThreadMode
 
-public class MoviesSearchActivity extends BaseMessageActivity implements
-        MoviesSearchFragment.OnSearchClickListener {
+/**
+ * Hosts [MoviesSearchFragment], provides a special toolbar with search bar that expands.
+ */
+class MoviesSearchActivity : BaseMessageActivity(), OnSearchClickListener {
 
-    static final String EXTRA_ID_LINK = "idLink";
-    private static final String STATE_SEARCH_VISIBLE = "searchVisible";
+    private lateinit var binding: ActivityMoviesSearchBinding
 
-    @BindView(R.id.containerSearchBar) View containerSearchBar;
-    @BindView(R.id.text_input_layout_toolbar) TextInputLayout searchInputLayout;
-    @BindView(R.id.auto_complete_view_toolbar) AutoCompleteTextView searchView;
-    @BindView(R.id.containerMoviesSearchFragment) View containerMoviesSearchFragment;
+    private lateinit var searchHistory: SearchHistory
+    private lateinit var searchHistoryAdapter: ArrayAdapter<String>
+    private var showSearchView = false
 
-    private SearchHistory searchHistory;
-    private ArrayAdapter<String> searchHistoryAdapter;
-    private boolean showSearchView;
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMoviesSearchBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_movies_search);
-
-        int linkId = getIntent().getIntExtra(EXTRA_ID_LINK,
-                MoviesDiscoverAdapter.DISCOVER_LINK_DEFAULT.id);
-        MoviesDiscoverLink link = MoviesDiscoverLink.fromId(linkId);
-        showSearchView = link == MoviesDiscoverAdapter.DISCOVER_LINK_DEFAULT;
+        val linkId = intent.getIntExtra(
+            EXTRA_ID_LINK,
+            MoviesDiscoverAdapter.DISCOVER_LINK_DEFAULT.id
+        )
+        val link = MoviesDiscoverLink.fromId(linkId)
+        showSearchView = link == MoviesDiscoverAdapter.DISCOVER_LINK_DEFAULT
         if (savedInstanceState != null) {
-            showSearchView = savedInstanceState.getBoolean(STATE_SEARCH_VISIBLE, showSearchView);
+            showSearchView = savedInstanceState.getBoolean(STATE_SEARCH_VISIBLE, showSearchView)
         }
 
-        ButterKnife.bind(this);
-        setupActionBar(link);
+        setupActionBar(link)
 
         if (savedInstanceState == null) {
             if (showSearchView) {
-                ViewTools.showSoftKeyboardOnSearchView(this, searchView);
+                ViewTools.showSoftKeyboardOnSearchView(
+                    this,
+                    binding.sgToolbar.autoCompleteViewToolbar
+                )
             }
-
-            getSupportFragmentManager().beginTransaction()
-                    .add(R.id.containerMoviesSearchFragment,
-                            MoviesSearchFragment.newInstance(link))
-                    .commit();
+            supportFragmentManager.beginTransaction()
+                .add(R.id.containerMoviesSearchFragment, newInstance(link))
+                .commit()
         } else {
-            postponeEnterTransition();
+            postponeEnterTransition()
             // allow the adapter to repopulate during the next layout pass
             // before starting the transition animation
-            containerMoviesSearchFragment.post(this::startPostponedEnterTransition);
+            binding.containerMoviesSearchFragment.post { startPostponedEnterTransition() }
         }
     }
 
-    private void setupActionBar(MoviesDiscoverLink link) {
-        super.setupActionBar();
+    private fun setupActionBar(link: MoviesDiscoverLink) {
+        super.setupActionBar()
 
-        ActionBar actionBar = getSupportActionBar();
-        if (actionBar != null) {
-            actionBar.setDisplayHomeAsUpEnabled(true);
-        }
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
         // set title for screen readers
         if (showSearchView) {
-            setTitle(R.string.search);
+            setTitle(R.string.search)
         } else {
-            setTitle(link.titleRes);
+            setTitle(link.titleRes)
         }
 
-        setSearchViewVisible(showSearchView);
+        setSearchViewVisible(showSearchView)
 
         // setup search box
-        searchView.setThreshold(1);
-        searchView.setOnClickListener(searchViewClickListener);
-        searchView.setOnItemClickListener(searchViewItemClickListener);
-        searchView.setOnEditorActionListener(searchViewActionListener);
-        searchInputLayout.setHint(getString(R.string.movies_search_hint));
+        val searchView = binding.sgToolbar.autoCompleteViewToolbar
+        searchView.threshold = 1
+        searchView.setOnClickListener(searchViewClickListener)
+        searchView.onItemClickListener = searchViewItemClickListener
+        searchView.setOnEditorActionListener(searchViewActionListener)
+        binding.sgToolbar.textInputLayoutToolbar.hint = getString(R.string.movies_search_hint)
         // set in code as XML is overridden
-        searchView.setImeOptions(EditorInfo.IME_ACTION_SEARCH);
-        searchView.setInputType(EditorInfo.TYPE_CLASS_TEXT);
+        searchView.imeOptions = EditorInfo.IME_ACTION_SEARCH
+        searchView.inputType = EditorInfo.TYPE_CLASS_TEXT
 
         // setup search history
-        searchHistory = new SearchHistory(this, SearchSettings.KEY_SUFFIX_MOVIES);
-        searchHistoryAdapter = new ArrayAdapter<>(this, R.layout.item_dropdown,
-                searchHistory.getSearchHistory());
-        searchView.setAdapter(searchHistoryAdapter);
+        searchHistory = SearchHistory(this, SearchSettings.KEY_SUFFIX_MOVIES)
+        searchHistoryAdapter = ArrayAdapter(
+            this,
+            R.layout.item_dropdown,
+            searchHistory.searchHistory
+        )
+        searchView.setAdapter(searchHistoryAdapter)
         // drop-down is auto-shown on config change, ensure it is hidden when recreating views
-        searchView.dismissDropDown();
+        searchView.dismissDropDown()
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        super.onCreateOptionsMenu(menu);
-        getMenuInflater().inflate(R.menu.movies_search_menu, menu);
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        super.onCreateOptionsMenu(menu)
+        menuInflater.inflate(R.menu.movies_search_menu, menu)
 
-        MenuItem itemSearch = menu.findItem(R.id.menu_action_movies_search_display_search);
-        itemSearch.setVisible(!showSearchView);
-        itemSearch.setEnabled(!showSearchView);
+        val itemSearch = menu.findItem(R.id.menu_action_movies_search_display_search)
+        itemSearch.isVisible = !showSearchView
+        itemSearch.isEnabled = !showSearchView
 
-        return true;
+        return true
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        int itemId = item.getItemId();
-        if (itemId == R.id.menu_action_movies_search_change_language) {
-            MovieLocalizationDialogFragment.show(getSupportFragmentManager());
-            return true;
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.menu_action_movies_search_change_language -> {
+                show(supportFragmentManager)
+                return true
+            }
+            R.id.menu_action_movies_search_display_search -> {
+                setSearchViewVisible(true)
+                ViewTools.showSoftKeyboardOnSearchView(
+                    this,
+                    binding.sgToolbar.autoCompleteViewToolbar
+                )
+                showSearchView = true
+                invalidateOptionsMenu()
+                return true
+            }
+            R.id.menu_action_movies_search_clear_history -> {
+                searchHistory.clearHistory()
+                searchHistoryAdapter.clear()
+                // setting text to null seems to fix the dropdown from not clearing
+                binding.sgToolbar.autoCompleteViewToolbar.setText(null)
+                return true
+            }
+            else -> return super.onOptionsItemSelected(item)
         }
-        if (itemId == R.id.menu_action_movies_search_display_search) {
-            setSearchViewVisible(true);
-            ViewTools.showSoftKeyboardOnSearchView(this, searchView);
-            showSearchView = true;
-            supportInvalidateOptionsMenu();
-            return true;
-        }
-        if (itemId == R.id.menu_action_movies_search_clear_history) {
-            searchHistory.clearHistory();
-            searchHistoryAdapter.clear();
-            // setting text to null seems to fix the dropdown from not clearing
-            searchView.setText(null);
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
     }
 
-    @Override
-    protected void onSaveInstanceState(Bundle outState) {
-        super.onSaveInstanceState(outState);
-        outState.putBoolean(STATE_SEARCH_VISIBLE, showSearchView);
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putBoolean(STATE_SEARCH_VISIBLE, showSearchView)
     }
 
     @Subscribe(threadMode = ThreadMode.MAIN)
-    public void onEventLanguageChanged(
-            MovieLocalizationDialogFragment.LocalizationChangedEvent event) {
+    fun onEventLanguageChanged(event: LocalizationChangedEvent?) {
         // just run the current search again
-        search();
+        search()
     }
 
-    @Override
-    public void onSearchClick() {
-        search();
+    override fun onSearchClick() {
+        search()
     }
 
-    private void search() {
-        Fragment fragment = getSupportFragmentManager().findFragmentById(
-                R.id.containerMoviesSearchFragment);
-        if (fragment == null) {
-            return;
-        }
+    private fun search() {
+        val fragment = supportFragmentManager.findFragmentById(
+            R.id.containerMoviesSearchFragment
+        ) ?: return
 
-        String query = searchView.getText().toString().trim();
+        val query = binding.sgToolbar.autoCompleteViewToolbar.text.toString().trim()
         // perform search
-        MoviesSearchFragment searchFragment = (MoviesSearchFragment) fragment;
-        searchFragment.search(query);
+        val searchFragment = fragment as MoviesSearchFragment
+        searchFragment.search(query)
         // update history
         if (searchHistory.saveRecentSearch(query)) {
-            searchHistoryAdapter.clear();
-            searchHistoryAdapter.addAll(searchHistory.getSearchHistory());
+            searchHistoryAdapter.clear()
+            searchHistoryAdapter.addAll(searchHistory.searchHistory)
         }
     }
 
-    private void setSearchViewVisible(boolean visible) {
-        containerSearchBar.setVisibility(visible ? View.VISIBLE : View.GONE);
-        ActionBar actionBar = getSupportActionBar();
-        if (actionBar != null) {
-            actionBar.setDisplayShowTitleEnabled(!visible);
-        }
+    private fun setSearchViewVisible(visible: Boolean) {
+        binding.sgToolbar.containerSearchBar.visibility = if (visible) View.VISIBLE else View.GONE
+        supportActionBar?.setDisplayShowTitleEnabled(!visible)
     }
 
-    private View.OnClickListener searchViewClickListener = new View.OnClickListener() {
-        @Override
-        public void onClick(View v) {
-            searchView.showDropDown();
-        }
-    };
+    private val searchViewClickListener =
+        View.OnClickListener { binding.sgToolbar.autoCompleteViewToolbar.showDropDown() }
 
-    private AdapterView.OnItemClickListener searchViewItemClickListener
-            = (parent, view, position, id) -> search();
+    private val searchViewItemClickListener =
+        AdapterView.OnItemClickListener { _: AdapterView<*>?, _: View?, _: Int, _: Long -> search() }
 
-    private TextView.OnEditorActionListener searchViewActionListener = (v, actionId, event) -> {
-        if (actionId == EditorInfo.IME_ACTION_SEARCH
-                || (event != null && event.getAction() == KeyEvent.ACTION_DOWN
-                && event.getKeyCode() == KeyEvent.KEYCODE_ENTER)) {
-            search();
-            return true;
+    private val searchViewActionListener =
+        OnEditorActionListener { _: TextView?, actionId: Int, event: KeyEvent? ->
+            if (actionId == EditorInfo.IME_ACTION_SEARCH
+                || event != null && event.action == KeyEvent.ACTION_DOWN && event.keyCode == KeyEvent.KEYCODE_ENTER) {
+                search()
+                return@OnEditorActionListener true
+            }
+            false
         }
-        return false;
-    };
+
+    companion object {
+        const val EXTRA_ID_LINK = "idLink"
+        private const val STATE_SEARCH_VISIBLE = "searchVisible"
+    }
 }
