620ae4a010ef01cb8450df38a2cb73f6ba8f7852
Uwe Trottmann <uwe+git@uwetrottmann.com>
2021-11-25 15:22:39 +0100

    Update Firebase and Crashlytics

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -241,13 +241,13 @@ dependencies {
     implementation("com.github.lenguyenthanh.debugdrawer:debugdrawer-timber:$debugDrawerVersion")
 
     // Import the Firebase BoM
-    implementation(platform("com.google.firebase:firebase-bom:28.4.1"))
+    implementation(platform("com.google.firebase:firebase-bom:29.0.0"))
     // Firebase Sign-In https://github.com/firebase/FirebaseUI-Android/releases
     implementation("com.firebaseui:firebase-ui-auth:8.0.0")
 
     // Crashlytics
     // https://firebase.google.com/support/release-notes/android
-    implementation("com.google.firebase:firebase-crashlytics:18.2.1")
+    implementation("com.google.firebase:firebase-crashlytics:18.2.4")
 
     // App Engine
     // https://github.com/googleapis/google-api-java-client/releases
