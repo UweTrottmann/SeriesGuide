3576f6aa101ca8ef74c86c1b106dc254d6a701c0
Uwe Trottmann <uwe+git@uwetrottmann.com>
2022-01-13 13:08:51 +0100

    Kotlin: convert some settings dialogs.

--- app/src/main/java/com/battlelancer/seriesguide/ui/dialogs/NotificationThresholdDialogFragment.kt
+++ app/src/main/java/com/battlelancer/seriesguide/ui/dialogs/NotificationThresholdDialogFragment.kt
@@ -1,174 +1,183 @@
-package com.battlelancer.seriesguide.ui.dialogs;
+package com.battlelancer.seriesguide.ui.dialogs
 
-import android.content.res.Resources;
-import android.os.Bundle;
-import android.text.Editable;
-import android.text.TextWatcher;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.RadioButton;
-import android.widget.RadioGroup;
-import androidx.annotation.Nullable;
-import androidx.annotation.PluralsRes;
-import androidx.appcompat.app.AppCompatDialogFragment;
-import androidx.preference.PreferenceManager;
-import butterknife.BindView;
-import butterknife.ButterKnife;
-import butterknife.Unbinder;
-import com.battlelancer.seriesguide.R;
-import com.battlelancer.seriesguide.settings.NotificationSettings;
-import java.util.regex.Pattern;
-import timber.log.Timber;
+import android.content.res.Resources
+import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import android.widget.RadioButton
+import android.widget.RadioGroup
+import androidx.annotation.PluralsRes
+import androidx.appcompat.app.AppCompatDialogFragment
+import androidx.preference.PreferenceManager
+import butterknife.BindView
+import butterknife.ButterKnife
+import butterknife.Unbinder
+import com.battlelancer.seriesguide.R
+import com.battlelancer.seriesguide.settings.NotificationSettings
+import timber.log.Timber
+import java.util.regex.Pattern
 
 /**
  * Dialog which allows to select the number of minutes, hours or days when a notification should
  * appear before an episode is released.
  */
-public class NotificationThresholdDialogFragment extends AppCompatDialogFragment {
+class NotificationThresholdDialogFragment : AppCompatDialogFragment() {
 
-    @BindView(R.id.buttonNegative) View buttonNegative;
-    @BindView(R.id.buttonPositive) Button buttonPositive;
-    @BindView(R.id.editTextThresholdValue) EditText editTextValue;
-    @BindView(R.id.radioGroupThreshold) RadioGroup radioGroup;
-    @BindView(R.id.radioButtonThresholdMinutes) RadioButton radioButtonMinutes;
-    @BindView(R.id.radioButtonThresholdHours) RadioButton radioButtonHours;
-    @BindView(R.id.radioButtonThresholdDays) RadioButton radioButtonDays;
-    private Unbinder unbinder;
+    @BindView(R.id.buttonNegative)
+    var buttonNegative: View? = null
 
-    private int value;
+    @BindView(R.id.buttonPositive)
+    var buttonPositive: Button? = null
 
-    @Nullable
-    @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container,
-            @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.dialog_notification_threshold, container, false);
-        unbinder = ButterKnife.bind(this, view);
+    @BindView(R.id.editTextThresholdValue)
+    var editTextValue: EditText? = null
 
-        buttonNegative.setVisibility(View.GONE);
-        buttonPositive.setText(android.R.string.ok);
-        buttonPositive.setOnClickListener(v -> saveAndDismiss());
+    @BindView(R.id.radioGroupThreshold)
+    var radioGroup: RadioGroup? = null
 
-        editTextValue.addTextChangedListener(textWatcher);
+    @BindView(R.id.radioButtonThresholdMinutes)
+    var radioButtonMinutes: RadioButton? = null
 
-        radioGroup.setOnCheckedChangeListener((group, checkedId) -> {
+    @BindView(R.id.radioButtonThresholdHours)
+    var radioButtonHours: RadioButton? = null
+
+    @BindView(R.id.radioButtonThresholdDays)
+    var radioButtonDays: RadioButton? = null
+    private var unbinder: Unbinder? = null
+    private var value = 0
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.dialog_notification_threshold, container, false)
+        unbinder = ButterKnife.bind(this, view)
+
+        buttonNegative!!.visibility = View.GONE
+        buttonPositive!!.setText(android.R.string.ok)
+        buttonPositive!!.setOnClickListener { saveAndDismiss() }
+
+        editTextValue!!.addTextChangedListener(textWatcher)
+
+        radioGroup!!.setOnCheckedChangeListener { _: RadioGroup?, _: Int ->
             // trigger text watcher, takes care of validating the value based on the new unit
-            editTextValue.setText(editTextValue.getText());
-        });
-
-        bindViews();
-
-        return view;
-    }
-
-    @Override
-    public void onDestroyView() {
-        super.onDestroyView();
-        unbinder.unbind();
-    }
-
-    private void bindViews() {
-        int minutes = NotificationSettings.getLatestToIncludeTreshold(getContext());
-
-        int value;
-        if (minutes != 0 && minutes % (24 * 60) == 0) {
-            value = minutes / (24 * 60);
-            radioGroup.check(R.id.radioButtonThresholdDays);
-        } else if (minutes != 0 && minutes % 60 == 0) {
-            value = minutes / 60;
-            radioGroup.check(R.id.radioButtonThresholdHours);
-        } else {
-            value = minutes;
-            radioGroup.check(R.id.radioButtonThresholdMinutes);
+            editTextValue!!.text = editTextValue!!.text
         }
 
-        editTextValue.setText(String.valueOf(value));
+        bindViews()
+
+        return view
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        unbinder!!.unbind()
+    }
+
+    private fun bindViews() {
+        val minutes = NotificationSettings.getLatestToIncludeTreshold(requireContext())
+        val value: Int
+        if (minutes != 0 && minutes % (24 * 60) == 0) {
+            value = minutes / (24 * 60)
+            radioGroup!!.check(R.id.radioButtonThresholdDays)
+        } else if (minutes != 0 && minutes % 60 == 0) {
+            value = minutes / 60
+            radioGroup!!.check(R.id.radioButtonThresholdHours)
+        } else {
+            value = minutes
+            radioGroup!!.check(R.id.radioButtonThresholdMinutes)
+        }
+        editTextValue!!.setText(value.toString())
         // radio buttons are updated by text watcher
     }
 
-    private void parseAndUpdateValue(Editable s) {
-        int value = 0;
+    private fun parseAndUpdateValue(s: Editable) {
+        var value = 0
         try {
-            value = Integer.parseInt(s.toString());
-        } catch (NumberFormatException ignored) {
+            value = s.toString().toInt()
+        } catch (ignored: NumberFormatException) {
         }
 
         // do not allow values bigger than a threshold, based on the selected unit
-        boolean resetValue = false;
-        if (radioGroup.getCheckedRadioButtonId() == radioButtonMinutes.getId()
-                && value > 600) {
-            resetValue = true;
-            value = 600;
-        } else if (radioGroup.getCheckedRadioButtonId() == radioButtonHours.getId()
-                && value > 120) {
-            resetValue = true;
-            value = 120;
-        } else if (radioGroup.getCheckedRadioButtonId() == radioButtonDays.getId()
-                && value > 7) {
-            resetValue = true;
-            value = 7;
+        var resetValue = false
+        if (radioGroup!!.checkedRadioButtonId == radioButtonMinutes!!.id
+            && value > 600) {
+            resetValue = true
+            value = 600
+        } else if (radioGroup!!.checkedRadioButtonId == radioButtonHours!!.id
+            && value > 120) {
+            resetValue = true
+            value = 120
+        } else if (radioGroup!!.checkedRadioButtonId == radioButtonDays!!.id
+            && value > 7) {
+            resetValue = true
+            value = 7
         } else if (value < 0) {
             // should never happen due to text filter, but better safe than sorry
-            resetValue = true;
-            value = 0;
+            resetValue = true
+            value = 0
         }
 
         if (resetValue) {
-            s.replace(0, s.length(), String.valueOf(value));
+            s.replace(0, s.length, value.toString())
         }
 
-        this.value = value;
-        updateRadioButtons(value);
+        this.value = value
+        updateRadioButtons(value)
     }
 
-    private void updateRadioButtons(int value) {
-        Pattern placeholderPattern = Pattern.compile("%d\\s*");
-        Resources res = getResources();
-        radioButtonMinutes.setText(getQuantityStringWithoutPlaceholder(placeholderPattern, res,
-                R.plurals.minutes_before_plural, value));
-        radioButtonHours.setText(getQuantityStringWithoutPlaceholder(placeholderPattern, res,
-                R.plurals.hours_before_plural, value));
-        radioButtonDays.setText(getQuantityStringWithoutPlaceholder(placeholderPattern, res,
-                R.plurals.days_before_plural, value));
+    private fun updateRadioButtons(value: Int) {
+        val placeholderPattern = Pattern.compile("%d\\s*")
+        val res = resources
+        radioButtonMinutes!!.text = getQuantityStringWithoutPlaceholder(
+            placeholderPattern, res, R.plurals.minutes_before_plural, value
+        )
+        radioButtonHours!!.text = getQuantityStringWithoutPlaceholder(
+            placeholderPattern, res, R.plurals.hours_before_plural, value
+        )
+        radioButtonDays!!.text = getQuantityStringWithoutPlaceholder(
+            placeholderPattern, res, R.plurals.days_before_plural, value
+        )
     }
 
-    private String getQuantityStringWithoutPlaceholder(Pattern pattern, Resources res,
-            @PluralsRes int pluralsRes, int value) {
-        return pattern.matcher(res.getQuantityString(pluralsRes, value)).replaceAll("");
+    private fun getQuantityStringWithoutPlaceholder(
+        pattern: Pattern,
+        res: Resources,
+        @PluralsRes pluralsRes: Int,
+        value: Int
+    ): String {
+        return pattern.matcher(res.getQuantityString(pluralsRes, value)).replaceAll("")
     }
 
-    private void saveAndDismiss() {
-        int minutes = this.value;
+    private fun saveAndDismiss() {
+        var minutes = value
 
         // if not already, convert to minutes
-        if (radioGroup.getCheckedRadioButtonId() == radioButtonHours.getId()) {
-            minutes *= 60;
-        } else if (radioGroup.getCheckedRadioButtonId() == radioButtonDays.getId()) {
-            minutes *= 60 * 24;
+        if (radioGroup!!.checkedRadioButtonId == radioButtonHours!!.id) {
+            minutes *= 60
+        } else if (radioGroup!!.checkedRadioButtonId == radioButtonDays!!.id) {
+            minutes *= 60 * 24
         }
 
         PreferenceManager.getDefaultSharedPreferences(requireContext()).edit()
-                .putString(NotificationSettings.KEY_THRESHOLD, String.valueOf(minutes))
-                .apply();
-        Timber.i("Notification threshold set to %d minutes", minutes);
+            .putString(NotificationSettings.KEY_THRESHOLD, minutes.toString())
+            .apply()
+        Timber.i("Notification threshold set to %d minutes", minutes)
 
-        dismiss();
+        dismiss()
     }
 
-    private TextWatcher textWatcher = new TextWatcher() {
-        @Override
-        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+    private val textWatcher: TextWatcher = object : TextWatcher {
+        override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {}
+        override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {}
+        override fun afterTextChanged(s: Editable) {
+            parseAndUpdateValue(s)
         }
-
-        @Override
-        public void onTextChanged(CharSequence s, int start, int before, int count) {
-        }
-
-        @Override
-        public void afterTextChanged(Editable s) {
-            parseAndUpdateValue(s);
-        }
-    };
+    }
 }
--- app/src/main/java/com/battlelancer/seriesguide/ui/dialogs/TimeOffsetDialogFragment.kt
+++ app/src/main/java/com/battlelancer/seriesguide/ui/dialogs/TimeOffsetDialogFragment.kt
@@ -1,145 +1,146 @@
-package com.battlelancer.seriesguide.ui.dialogs;
+package com.battlelancer.seriesguide.ui.dialogs
 
-import android.annotation.SuppressLint;
-import android.os.Bundle;
-import android.text.Editable;
-import android.text.TextWatcher;
-import android.text.format.DateUtils;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatDialogFragment;
-import androidx.preference.PreferenceManager;
-import butterknife.BindView;
-import butterknife.ButterKnife;
-import butterknife.Unbinder;
-import com.battlelancer.seriesguide.R;
-import com.battlelancer.seriesguide.settings.DisplaySettings;
-import org.threeten.bp.LocalDate;
-import org.threeten.bp.LocalDateTime;
-import org.threeten.bp.LocalTime;
-import org.threeten.bp.ZoneId;
-import timber.log.Timber;
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.text.format.DateUtils
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatDialogFragment
+import androidx.preference.PreferenceManager
+import butterknife.BindView
+import butterknife.ButterKnife
+import butterknife.Unbinder
+import com.battlelancer.seriesguide.R
+import com.battlelancer.seriesguide.settings.DisplaySettings
+import com.battlelancer.seriesguide.settings.DisplaySettings.getShowsTimeOffset
+import org.threeten.bp.LocalDate
+import org.threeten.bp.LocalDateTime
+import org.threeten.bp.LocalTime
+import org.threeten.bp.ZoneId
+import timber.log.Timber
 
 /**
- * Dialog which allows to select the number of hours (between +/-24) to offset show release times
- * by.
+ * Dialog which allows to select the number of hours (between +/-24)
+ * to offset show release times by.
  */
-public class TimeOffsetDialogFragment extends AppCompatDialogFragment {
+class TimeOffsetDialogFragment : AppCompatDialogFragment() {
 
-    @BindView(R.id.buttonNegative) View buttonNegative;
-    @BindView(R.id.buttonPositive) Button buttonPositive;
-    @BindView(R.id.editTextOffsetValue) EditText editTextValue;
-    @BindView(R.id.textViewOffsetRange) TextView textViewRange;
-    @BindView(R.id.textViewOffsetSummary) TextView textViewSummary;
-    @BindView(R.id.textViewOffsetExample) TextView textViewExample;
-    private Unbinder unbinder;
+    @BindView(R.id.buttonNegative)
+    var buttonNegative: View? = null
 
-    private int hours;
+    @BindView(R.id.buttonPositive)
+    var buttonPositive: Button? = null
 
-    @Nullable
-    @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
-            @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.dialog_time_offset, container, false);
-        unbinder = ButterKnife.bind(this, view);
+    @BindView(R.id.editTextOffsetValue)
+    var editTextValue: EditText? = null
 
-        buttonNegative.setVisibility(View.GONE);
-        buttonPositive.setText(android.R.string.ok);
-        buttonPositive.setOnClickListener(v -> saveAndDismiss());
+    @BindView(R.id.textViewOffsetRange)
+    var textViewRange: TextView? = null
 
-        textViewRange.setText(getString(R.string.format_time_offset_range, -24, 24));
-        editTextValue.setHint(getString(R.string.format_time_offset_range, -24, 24));
+    @BindView(R.id.textViewOffsetSummary)
+    var textViewSummary: TextView? = null
 
-        editTextValue.addTextChangedListener(textWatcher);
+    @BindView(R.id.textViewOffsetExample)
+    var textViewExample: TextView? = null
+    private var unbinder: Unbinder? = null
 
-        bindViews();
+    private var hours = 0
 
-        return view;
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.dialog_time_offset, container, false)
+        unbinder = ButterKnife.bind(this, view)
+
+        buttonNegative!!.visibility = View.GONE
+        buttonPositive!!.setText(android.R.string.ok)
+        buttonPositive!!.setOnClickListener { v: View? -> saveAndDismiss() }
+
+        textViewRange!!.text = getString(R.string.format_time_offset_range, -24, 24)
+        editTextValue!!.hint = getString(R.string.format_time_offset_range, -24, 24)
+
+        editTextValue!!.addTextChangedListener(textWatcher)
+
+        bindViews()
+
+        return view
     }
 
-    @Override
-    public void onDestroyView() {
-        super.onDestroyView();
-        unbinder.unbind();
+    override fun onDestroyView() {
+        super.onDestroyView()
+        unbinder!!.unbind()
     }
 
-    private void bindViews() {
-        int hours = DisplaySettings.getShowsTimeOffset(getContext());
-
-        editTextValue.setText(String.valueOf(hours));
+    private fun bindViews() {
+        val hours = getShowsTimeOffset(requireContext())
+        editTextValue!!.setText(hours.toString())
         // text views are updated by text watcher
     }
 
-    private void parseAndUpdateValue(Editable s) {
-        int value = 0;
+    private fun parseAndUpdateValue(s: Editable) {
+        var value = 0
         try {
-            value = Integer.parseInt(s.toString());
-        } catch (NumberFormatException ignored) {
+            value = s.toString().toInt()
+        } catch (ignored: NumberFormatException) {
         }
 
         // do only allow values between +/-24
-        boolean resetValue = false;
+        var resetValue = false
         if (value < -24) {
-            resetValue = true;
-            value = -24;
+            resetValue = true
+            value = -24
         } else if (value > 24) {
-            resetValue = true;
-            value = 24;
+            resetValue = true
+            value = 24
         }
 
         if (resetValue) {
-            s.replace(0, s.length(), String.valueOf(value));
+            s.replace(0, s.length, value.toString())
         }
 
-        this.hours = value;
-        updateSummaryAndExample(value);
+        hours = value
+        updateSummaryAndExample(value)
     }
 
     @SuppressLint("SetTextI18n")
-    private void updateSummaryAndExample(int value) {
-        textViewSummary.setText(getString(R.string.pref_offsetsummary, value));
-
-        LocalDateTime original = LocalDateTime.of(LocalDate.now(), LocalTime.of(20, 0));
-        LocalDateTime offset = original.plusHours(value);
-
-        textViewExample.setText(formatToTimeString(original) + " -> " + formatToTimeString(offset));
+    private fun updateSummaryAndExample(value: Int) {
+        textViewSummary!!.text = getString(R.string.pref_offsetsummary, value)
+        val original = LocalDateTime.of(LocalDate.now(), LocalTime.of(20, 0))
+        val offset = original.plusHours(value.toLong())
+        textViewExample!!.text =
+            formatToTimeString(original).toString() + " -> " + formatToTimeString(offset)
     }
 
-    private CharSequence formatToTimeString(LocalDateTime localDateTime) {
-        return DateUtils.getRelativeDateTimeString(getContext(),
-                localDateTime.atZone(ZoneId.systemDefault()).toInstant().toEpochMilli(),
-                DateUtils.DAY_IN_MILLIS, DateUtils.WEEK_IN_MILLIS, 0);
+    private fun formatToTimeString(localDateTime: LocalDateTime): CharSequence {
+        return DateUtils.getRelativeDateTimeString(
+            requireContext(),
+            localDateTime.atZone(ZoneId.systemDefault()).toInstant().toEpochMilli(),
+            DateUtils.DAY_IN_MILLIS, DateUtils.WEEK_IN_MILLIS, 0
+        )
     }
 
-    private void saveAndDismiss() {
-        int hours = this.hours;
-
+    private fun saveAndDismiss() {
+        val hours = hours
         PreferenceManager.getDefaultSharedPreferences(requireContext()).edit()
-                .putString(DisplaySettings.KEY_SHOWS_TIME_OFFSET, String.valueOf(hours))
-                .apply();
-        Timber.i("Time offset set to %d hours", hours);
-
-        dismiss();
+            .putString(DisplaySettings.KEY_SHOWS_TIME_OFFSET, hours.toString())
+            .apply()
+        Timber.i("Time offset set to %d hours", hours)
+        dismiss()
     }
 
-    private TextWatcher textWatcher = new TextWatcher() {
-        @Override
-        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+    private val textWatcher: TextWatcher = object : TextWatcher {
+        override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {}
+        override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {}
+        override fun afterTextChanged(s: Editable) {
+            parseAndUpdateValue(s)
         }
-
-        @Override
-        public void onTextChanged(CharSequence s, int start, int before, int count) {
-        }
-
-        @Override
-        public void afterTextChanged(Editable s) {
-            parseAndUpdateValue(s);
-        }
-    };
+    }
 }
