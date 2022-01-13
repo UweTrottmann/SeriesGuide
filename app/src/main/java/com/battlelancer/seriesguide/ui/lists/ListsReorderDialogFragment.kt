d95260178c9fea14bfcd983b324fe021134bb13f
Uwe Trottmann <uwe+git@uwetrottmann.com>
2022-01-13 10:44:01 +0100

    Kotlin: convert ListsReorderDialogFragment.

--- app/src/main/java/com/battlelancer/seriesguide/ui/lists/ListsReorderDialogFragment.kt
+++ app/src/main/java/com/battlelancer/seriesguide/ui/lists/ListsReorderDialogFragment.kt
@@ -1,126 +1,120 @@
-package com.battlelancer.seriesguide.ui.lists;
+package com.battlelancer.seriesguide.ui.lists
 
-import android.os.Bundle;
-import android.text.TextUtils;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.Button;
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatDialogFragment;
-import androidx.loader.app.LoaderManager;
-import androidx.loader.content.Loader;
-import butterknife.BindView;
-import butterknife.ButterKnife;
-import butterknife.Unbinder;
-import com.battlelancer.seriesguide.R;
-import com.battlelancer.seriesguide.ui.ListsActivity;
-import com.uwetrottmann.seriesguide.widgets.dragsortview.DragSortController;
-import com.uwetrottmann.seriesguide.widgets.dragsortview.DragSortListView;
-import java.util.ArrayList;
-import java.util.List;
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.appcompat.app.AppCompatDialogFragment
+import androidx.loader.app.LoaderManager
+import androidx.loader.content.Loader
+import butterknife.BindView
+import butterknife.ButterKnife
+import butterknife.Unbinder
+import com.battlelancer.seriesguide.R
+import com.battlelancer.seriesguide.ui.ListsActivity
+import com.battlelancer.seriesguide.ui.lists.OrderedListsLoader.OrderedList
+import com.uwetrottmann.seriesguide.widgets.dragsortview.DragSortController
+import com.uwetrottmann.seriesguide.widgets.dragsortview.DragSortListView
+import java.util.ArrayList
 
 /**
  * Dialog to reorder lists using a vertical list with drag handles. Currently not accessibility or
  * keyboard friendly (same as extension configuration screen).
  */
-public class ListsReorderDialogFragment extends AppCompatDialogFragment {
+class ListsReorderDialogFragment : AppCompatDialogFragment() {
 
-    @BindView(R.id.listViewListsReorder) DragSortListView dragSortListView;
-    @BindView(R.id.buttonNegative) Button buttonNegative;
-    @BindView(R.id.buttonPositive) Button buttonPositive;
+    @BindView(R.id.listViewListsReorder)
+    var dragSortListView: DragSortListView? = null
 
-    private Unbinder unbinder;
-    private ListsAdapter adapter;
+    @BindView(R.id.buttonNegative)
+    var buttonNegative: Button? = null
 
-    @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable final ViewGroup container,
-            @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.dialog_lists_reorder, container, false);
-        unbinder = ButterKnife.bind(this, view);
+    @BindView(R.id.buttonPositive)
+    var buttonPositive: Button? = null
 
-        DragSortController controller = new DragSortController(dragSortListView,
-                R.id.dragGripViewItemList, DragSortController.ON_DOWN,
-                DragSortController.CLICK_REMOVE);
-        controller.setRemoveEnabled(false);
-        dragSortListView.setFloatViewManager(controller);
-        dragSortListView.setOnTouchListener(controller);
-        dragSortListView.setDropListener(this::reorderList);
+    private var unbinder: Unbinder? = null
+    private lateinit var adapter: ListsAdapter
 
-        buttonNegative.setText(R.string.discard);
-        buttonNegative.setOnClickListener(v -> dismiss());
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.dialog_lists_reorder, container, false)
+        unbinder = ButterKnife.bind(this, view)
 
-        buttonPositive.setText(android.R.string.ok);
-        buttonPositive.setOnClickListener(v -> {
-            saveListsOrder();
-            dismiss();
-        });
+        val controller = DragSortController(
+            dragSortListView,
+            R.id.dragGripViewItemList, DragSortController.ON_DOWN,
+            DragSortController.CLICK_REMOVE
+        )
+        controller.isRemoveEnabled = false
+        dragSortListView!!.setFloatViewManager(controller)
+        dragSortListView!!.setOnTouchListener(controller)
+        dragSortListView!!.setDropListener { from: Int, to: Int -> reorderList(from, to) }
 
-        return view;
+        buttonNegative!!.setText(R.string.discard)
+        buttonNegative!!.setOnClickListener { v: View? -> dismiss() }
+
+        buttonPositive!!.setText(android.R.string.ok)
+        buttonPositive!!.setOnClickListener { v: View? ->
+            saveListsOrder()
+            dismiss()
+        }
+
+        return view
     }
 
-    @Override
-    public void onActivityCreated(Bundle savedInstanceState) {
-        super.onActivityCreated(savedInstanceState);
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
 
-        adapter = new ListsAdapter(getActivity());
-        dragSortListView.setAdapter(adapter);
+        adapter = ListsAdapter(activity)
+        dragSortListView!!.adapter = adapter
 
         LoaderManager.getInstance(this)
-                .initLoader(ListsActivity.LISTS_REORDER_LOADER_ID, null, listsLoaderCallbacks);
+            .initLoader(ListsActivity.LISTS_REORDER_LOADER_ID, null, listsLoaderCallbacks)
     }
 
-    @Override
-    public void onDestroyView() {
-        super.onDestroyView();
-
-        unbinder.unbind();
+    override fun onDestroyView() {
+        super.onDestroyView()
+        unbinder!!.unbind()
     }
 
-    private void reorderList(int from, int to) {
-        if (adapter == null) {
-            return;
-        }
-        adapter.reorderList(from, to);
+    private fun reorderList(from: Int, to: Int) {
+        adapter.reorderList(from, to)
     }
 
-    private void saveListsOrder() {
-        if (adapter == null) {
-            return;
-        }
-
-        int count = adapter.getCount();
-        List<String> listIdsInOrder = new ArrayList<>(count);
-        for (int position = 0; position < count; position++) {
-            OrderedListsLoader.OrderedList list = adapter.getItem(position);
-            if (list != null && !TextUtils.isEmpty(list.id)) {
-                listIdsInOrder.add(list.id);
+    private fun saveListsOrder() {
+        val count = adapter.count
+        val listIdsInOrder: MutableList<String> = ArrayList(count)
+        for (position in 0 until count) {
+            val list = adapter.getItem(position)
+            if (list != null && !list.id.isNullOrEmpty()) {
+                listIdsInOrder.add(list.id)
             }
         }
-        ListsTools.reorderLists(requireContext(), listIdsInOrder);
+        ListsTools.reorderLists(requireContext(), listIdsInOrder)
     }
 
-    private LoaderManager.LoaderCallbacks<List<OrderedListsLoader.OrderedList>> listsLoaderCallbacks
-            = new LoaderManager.LoaderCallbacks<List<OrderedListsLoader.OrderedList>>() {
-        @Override
-        public Loader<List<OrderedListsLoader.OrderedList>> onCreateLoader(int id, Bundle args) {
-            return new OrderedListsLoader(getActivity());
-        }
-
-        @Override
-        public void onLoadFinished(@NonNull Loader<List<OrderedListsLoader.OrderedList>> loader,
-                List<OrderedListsLoader.OrderedList> data) {
-            if (!isAdded()) {
-                return;
+    private val listsLoaderCallbacks: LoaderManager.LoaderCallbacks<List<OrderedList>> =
+        object : LoaderManager.LoaderCallbacks<List<OrderedList>> {
+            override fun onCreateLoader(id: Int, args: Bundle?): Loader<List<OrderedList>> {
+                return OrderedListsLoader(activity)
             }
 
-            adapter.setData(data);
-        }
+            override fun onLoadFinished(
+                loader: Loader<List<OrderedList>>,
+                data: List<OrderedList>
+            ) {
+                if (!isAdded) {
+                    return
+                }
+                adapter.setData(data)
+            }
 
-        @Override
-        public void onLoaderReset(@NonNull Loader<List<OrderedListsLoader.OrderedList>> loader) {
-            adapter.setData(null);
+            override fun onLoaderReset(loader: Loader<List<OrderedList>>) {
+                adapter.setData(null)
+            }
         }
-    };
 }
