18ae2e13e77972f22c9726ddbbd71539c42bc8ec
Uwe Trottmann <uwe+git@uwetrottmann.com>
2022-01-20 11:00:58 +0100

    Kotlin: convert SgSyncService.

--- app/src/main/java/com/battlelancer/seriesguide/sync/SgSyncService.kt
+++ app/src/main/java/com/battlelancer/seriesguide/sync/SgSyncService.kt
@@ -1,33 +1,32 @@
+package com.battlelancer.seriesguide.sync
 
-package com.battlelancer.seriesguide.sync;
-
-import android.app.Service;
-import android.content.Intent;
-import android.os.IBinder;
-import timber.log.Timber;
+import android.app.Service
+import android.content.Intent
+import android.os.IBinder
+import timber.log.Timber
 
 /**
- * A {@link Service} that returns an IBinder {@link SgSyncAdapter}, allowing the sync adapter
+ * A [Service] that returns an IBinder [SgSyncAdapter], allowing the sync adapter
  * framework to call onPerformSync().
  */
-public class SgSyncService extends Service {
+class SgSyncService : Service() {
 
-    private static final Object syncAdapterLock = new Object();
-    private static SgSyncAdapter syncAdapter = null;
-
-    @Override
-    public void onCreate() {
-        Timber.d("Creating sync service");
-        synchronized (syncAdapterLock) {
+    override fun onCreate() {
+        Timber.d("Creating sync service")
+        synchronized(syncAdapterLock) {
             if (syncAdapter == null) {
-                syncAdapter = new SgSyncAdapter(getApplicationContext());
+                syncAdapter = SgSyncAdapter(applicationContext)
             }
         }
     }
 
-    @Override
-    public IBinder onBind(Intent intent) {
-        Timber.d("Binding sync adapter");
-        return syncAdapter.getSyncAdapterBinder();
+    override fun onBind(intent: Intent): IBinder? {
+        Timber.d("Binding sync adapter")
+        return syncAdapter?.syncAdapterBinder
+    }
+
+    companion object {
+        private val syncAdapterLock = Any()
+        private var syncAdapter: SgSyncAdapter? = null
     }
 }
