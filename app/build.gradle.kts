3d92a49b3e72ee08600c30eeb3bf163e3e3af29e
Uwe Trottmann <uwe+git@uwetrottmann.com>
2022-01-21 10:21:37 +0100

    Update appcompat [1.4.0 -> 1.4.1] and constraintlayout [2.1.2 -> 2.1.3]

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -165,7 +165,7 @@ dependencies {
     implementation("androidx.core:core-ktx:$coreVersion")
     implementation("androidx.annotation:annotation:$annotationVersion")
     // https://developer.android.com/jetpack/androidx/releases/appcompat
-    implementation("androidx.appcompat:appcompat:1.4.0")
+    implementation("androidx.appcompat:appcompat:1.4.1")
     implementation("androidx.cardview:cardview:1.0.0")
     // https://developer.android.com/jetpack/androidx/releases/browser
     implementation("androidx.browser:browser:1.4.0")
@@ -176,7 +176,7 @@ dependencies {
     // https://developer.android.com/jetpack/androidx/releases/recyclerview
     implementation("androidx.recyclerview:recyclerview:1.2.1")
     // https://developer.android.com/jetpack/androidx/releases/constraintlayout
-    implementation("androidx.constraintlayout:constraintlayout:2.1.2")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.3")
     implementation("androidx.preference:preference-ktx:1.1.1")
 
     // ViewModel and LiveData
