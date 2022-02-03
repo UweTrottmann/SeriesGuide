cc53ef4cda8c2f7293668808339dd1fd0db0da9b
Uwe Trottmann <uwe+git@uwetrottmann.com>
2022-02-03 14:59:29 +0100

    Lint: provide state description for show filters.

--- app/src/main/java/com/battlelancer/seriesguide/widgets/FilterBox.kt
+++ app/src/main/java/com/battlelancer/seriesguide/widgets/FilterBox.kt
@@ -3,6 +3,7 @@ package com.battlelancer.seriesguide.widgets
 import android.content.Context
 import android.util.AttributeSet
 import android.view.LayoutInflater
+import android.view.View
 import android.widget.ImageView
 import android.widget.LinearLayout
 import android.widget.TextView
@@ -10,6 +11,7 @@ import androidx.vectordrawable.graphics.drawable.VectorDrawableCompat
 import butterknife.BindView
 import butterknife.ButterKnife
 import com.battlelancer.seriesguide.R
+import com.uwetrottmann.androidutils.AndroidUtils
 
 /**
  * Three state check box for filter being disabled, included or excluded.
@@ -47,16 +49,30 @@ class FilterBox @JvmOverloads constructor(
     var state: Boolean? = null
         set(value) {
             field = value
-            updateImage()
+            updateState()
         }
 
-    private fun updateImage() {
+    private fun updateState() {
         val vectorDrawable = when (state) {
             null -> VectorDrawableCompat.create(context.resources, R.drawable.ic_box_blank_white_24dp, context.theme)
             true -> VectorDrawableCompat.create(context.resources, R.drawable.ic_box_plus_green_24dp, context.theme)
             false -> VectorDrawableCompat.create(context.resources, R.drawable.ic_box_minus_red_24dp, context.theme)
         }
+        val stateDescription = when (state) {
+            null -> context.getString(R.string.state_shows_filter_disabled)
+            true -> context.getString(R.string.state_shows_filter_included)
+            false -> context.getString(R.string.state_shows_filter_excluded)
+        }
         imageView.setImageDrawable(vectorDrawable)
+        if (AndroidUtils.isAtLeastR) {
+            textView.stateDescription = stateDescription
+        } else {
+            if (textView.layoutDirection == View.LAYOUT_DIRECTION_RTL) {
+                textView.contentDescription = "$stateDescription ,$filterDescription"
+            } else {
+                textView.contentDescription = "$filterDescription, $stateDescription"
+            }
+        }
     }
 
     override fun performClick(): Boolean {
