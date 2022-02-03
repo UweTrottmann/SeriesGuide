b19d01ae7140f769d0b484a9881fce14ab186e22
Uwe Trottmann <uwe+git@uwetrottmann.com>
2022-02-03 15:07:48 +0100

    View binding: use in FilterBox.

--- app/src/main/java/com/battlelancer/seriesguide/widgets/FilterBox.kt
+++ app/src/main/java/com/battlelancer/seriesguide/widgets/FilterBox.kt
@@ -4,13 +4,10 @@ import android.content.Context
 import android.util.AttributeSet
 import android.view.LayoutInflater
 import android.view.View
-import android.widget.ImageView
 import android.widget.LinearLayout
-import android.widget.TextView
 import androidx.vectordrawable.graphics.drawable.VectorDrawableCompat
-import butterknife.BindView
-import butterknife.ButterKnife
 import com.battlelancer.seriesguide.R
+import com.battlelancer.seriesguide.databinding.ViewFilterBoxBinding
 import com.uwetrottmann.androidutils.AndroidUtils
 
 /**
@@ -22,14 +19,10 @@ class FilterBox @JvmOverloads constructor(
 
     private val filterDescription: String
 
-    @BindView(R.id.textViewFilterBox)
-    lateinit var textView: TextView
-    @BindView(R.id.imageViewFilterBox)
-    lateinit var imageView: ImageView
+    private val binding = ViewFilterBoxBinding.inflate(LayoutInflater.from(context), this)
 
     init {
         orientation = HORIZONTAL
-        LayoutInflater.from(context).inflate(R.layout.view_filter_box, this)
 
         val a = context.theme
             .obtainStyledAttributes(attrs, R.styleable.FilterBox, 0, 0)
@@ -38,9 +31,12 @@ class FilterBox @JvmOverloads constructor(
         } finally {
             a.recycle()
         }
+    }
 
-        ButterKnife.bind(this)
-        textView.text = filterDescription
+    override fun onFinishInflate() {
+        super.onFinishInflate()
+
+        binding.textViewFilterBox.text = filterDescription
     }
 
     /**
@@ -54,23 +50,37 @@ class FilterBox @JvmOverloads constructor(
 
     private fun updateState() {
         val vectorDrawable = when (state) {
-            null -> VectorDrawableCompat.create(context.resources, R.drawable.ic_box_blank_white_24dp, context.theme)
-            true -> VectorDrawableCompat.create(context.resources, R.drawable.ic_box_plus_green_24dp, context.theme)
-            false -> VectorDrawableCompat.create(context.resources, R.drawable.ic_box_minus_red_24dp, context.theme)
+            null -> VectorDrawableCompat.create(
+                context.resources,
+                R.drawable.ic_box_blank_white_24dp,
+                context.theme
+            )
+            true -> VectorDrawableCompat.create(
+                context.resources,
+                R.drawable.ic_box_plus_green_24dp,
+                context.theme
+            )
+            false -> VectorDrawableCompat.create(
+                context.resources,
+                R.drawable.ic_box_minus_red_24dp,
+                context.theme
+            )
         }
         val stateDescription = when (state) {
             null -> context.getString(R.string.state_shows_filter_disabled)
             true -> context.getString(R.string.state_shows_filter_included)
             false -> context.getString(R.string.state_shows_filter_excluded)
         }
-        imageView.setImageDrawable(vectorDrawable)
+        binding.imageViewFilterBox.setImageDrawable(vectorDrawable)
         if (AndroidUtils.isAtLeastR) {
-            textView.stateDescription = stateDescription
+            binding.textViewFilterBox.stateDescription = stateDescription
         } else {
-            if (textView.layoutDirection == View.LAYOUT_DIRECTION_RTL) {
-                textView.contentDescription = "$stateDescription ,$filterDescription"
+            if (binding.textViewFilterBox.layoutDirection == View.LAYOUT_DIRECTION_RTL) {
+                binding.textViewFilterBox.contentDescription =
+                    "$stateDescription ,$filterDescription"
             } else {
-                textView.contentDescription = "$filterDescription, $stateDescription"
+                binding.textViewFilterBox.contentDescription =
+                    "$filterDescription, $stateDescription"
             }
         }
     }
