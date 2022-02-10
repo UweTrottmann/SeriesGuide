1e73ba558770d1b58a9b07f7f7ab2079e646f8da
Uwe Trottmann <uwe+git@uwetrottmann.com>
2022-02-10 15:45:48 +0100

    Update fragment-ktx [1.4.0 -> 1.4.1]

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,7 +23,7 @@ buildscript {
     val annotationVersion by extra("1.3.0")
     val lifecycleVersion by extra("2.4.0")
     val roomVersion by extra("2.3.0") // https://developer.android.com/jetpack/androidx/releases/room
-    val fragmentVersion by extra("1.4.0") // https://developer.android.com/jetpack/androidx/releases/fragment
+    val fragmentVersion by extra("1.4.1") // https://developer.android.com/jetpack/androidx/releases/fragment
 
     val timberVersion by extra("5.0.1") // https://github.com/JakeWharton/timber/blob/master/CHANGELOG.md
 
