9f0d6df949abcd360bd8898eb20317743ec0eb9c
Uwe Trottmann <uwe+git@uwetrottmann.com>
2022-10-12 17:37:59 +0200

    Kotlin: convert AmazonBillingActivity in pure source set.

--- app/src/pure/java/com/battlelancer/seriesguide/billing/amazon/AmazonBillingActivity.kt
+++ app/src/pure/java/com/battlelancer/seriesguide/billing/amazon/AmazonBillingActivity.kt
@@ -1,9 +1,8 @@
-package com.battlelancer.seriesguide.billing.amazon;
+package com.battlelancer.seriesguide.billing.amazon
 
-import com.battlelancer.seriesguide.ui.BaseActivity;
+import com.battlelancer.seriesguide.ui.BaseActivity
 
 /**
  * No-op dummy. Only available in amazon build.
  */
-public class AmazonBillingActivity extends BaseActivity {
-}
+class AmazonBillingActivity : BaseActivity()
