b3fb4843ad8953f4b2d78634b107c242e5765938
Uwe Trottmann <uwe+git@uwetrottmann.com>
2022-10-19 16:55:54 +0200

    Kotlin: convert ConnectTraktCredentialsFragment.

--- app/src/main/java/com/battlelancer/seriesguide/traktapi/ConnectTraktCredentialsFragment.kt
+++ app/src/main/java/com/battlelancer/seriesguide/traktapi/ConnectTraktCredentialsFragment.kt
@@ -1,117 +1,115 @@
-package com.battlelancer.seriesguide.traktapi;
+package com.battlelancer.seriesguide.traktapi
 
-import android.content.Intent;
-import android.os.Bundle;
-import android.text.TextUtils;
-import android.text.method.LinkMovementMethod;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.Button;
-import androidx.annotation.NonNull;
-import androidx.fragment.app.Fragment;
-import com.battlelancer.seriesguide.R;
-import com.battlelancer.seriesguide.backend.settings.HexagonSettings;
-import com.battlelancer.seriesguide.databinding.FragmentConnectTraktCredentialsBinding;
-import com.battlelancer.seriesguide.sync.SyncProgress;
-import com.battlelancer.seriesguide.ui.SearchActivity;
-import org.greenrobot.eventbus.EventBus;
-import org.greenrobot.eventbus.Subscribe;
-import org.greenrobot.eventbus.ThreadMode;
+import android.content.Intent
+import android.os.Bundle
+import android.text.method.LinkMovementMethod
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.battlelancer.seriesguide.R
+import com.battlelancer.seriesguide.backend.settings.HexagonSettings
+import com.battlelancer.seriesguide.databinding.FragmentConnectTraktCredentialsBinding
+import com.battlelancer.seriesguide.sync.SyncProgress.SyncEvent
+import com.battlelancer.seriesguide.ui.SearchActivity.Companion.newIntent
+import org.greenrobot.eventbus.EventBus
+import org.greenrobot.eventbus.Subscribe
+import org.greenrobot.eventbus.ThreadMode
 
 /**
- * Interface to show trakt account features and connect or disconnect trakt.
+ * Interface connect or disconnect Trakt, also shows features not supported while Cloud is signed in.
  */
-public class ConnectTraktCredentialsFragment extends Fragment {
-
-    private FragmentConnectTraktCredentialsBinding binding;
-
-    @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
-            Bundle savedInstanceState) {
-        binding = FragmentConnectTraktCredentialsBinding.inflate(inflater, container, false);
+class ConnectTraktCredentialsFragment : Fragment() {
+    
+    private var binding: FragmentConnectTraktCredentialsBinding? = null
+    
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        val binding = FragmentConnectTraktCredentialsBinding.inflate(inflater, container, false)
+            .also { binding = it }
 
         // make learn more link clickable
-        binding.textViewTraktAbout.setMovementMethod(LinkMovementMethod.getInstance());
+        binding.textViewTraktAbout.movementMethod = LinkMovementMethod.getInstance()
 
-        boolean hexagonEnabled = HexagonSettings.isEnabled(requireContext());
-        binding.featureStatusTraktCheckIn.setFeatureEnabled(!hexagonEnabled);
-        binding.featureStatusTraktSync.setFeatureEnabled(!hexagonEnabled);
-        binding.featureStatusTraktSyncShows.setFeatureEnabled(!hexagonEnabled);
-        binding.featureStatusTraktSyncMovies.setFeatureEnabled(!hexagonEnabled);
+        val hexagonEnabled = HexagonSettings.isEnabled(requireContext())
+        binding.featureStatusTraktCheckIn.setFeatureEnabled(!hexagonEnabled)
+        binding.featureStatusTraktSync.setFeatureEnabled(!hexagonEnabled)
+        binding.featureStatusTraktSyncShows.setFeatureEnabled(!hexagonEnabled)
+        binding.featureStatusTraktSyncMovies.setFeatureEnabled(!hexagonEnabled)
 
         // library button
-        binding.buttonTraktLibrary.setOnClickListener(v -> {
+        binding.buttonTraktLibrary.setOnClickListener {
             // open search tab, will now have links to trakt lists
-            startActivity(SearchActivity.newIntent(requireContext()));
-        });
+            startActivity(newIntent(requireContext()))
+        }
 
-        binding.syncStatusTrakt.setVisibility(View.GONE);
+        binding.syncStatusTrakt.visibility = View.GONE
 
-        return binding.getRoot();
+        return binding.root
     }
 
-    @Override
-    public void onStart() {
-        super.onStart();
-        updateViews();
-        EventBus.getDefault().register(this);
+    override fun onStart() {
+        super.onStart()
+        updateViews()
+        EventBus.getDefault().register(this)
     }
 
-    @Override
-    public void onStop() {
-        super.onStop();
-        EventBus.getDefault().unregister(this);
+    override fun onStop() {
+        super.onStop()
+        EventBus.getDefault().unregister(this)
     }
 
-    @Override
-    public void onDestroyView() {
-        super.onDestroyView();
-        binding = null;
+    override fun onDestroyView() {
+        super.onDestroyView()
+        binding = null
     }
 
     @Subscribe(threadMode = ThreadMode.MAIN, sticky = true)
-    public void onEvent(SyncProgress.SyncEvent event) {
-        binding.syncStatusTrakt.setProgress(event);
+    fun onEvent(event: SyncEvent?) {
+        binding?.syncStatusTrakt?.setProgress(event)
     }
 
-    private void updateViews() {
-        TraktCredentials traktCredentials = TraktCredentials.get(requireContext());
-        boolean hasCredentials = traktCredentials.hasCredentials();
+    private fun updateViews() {
+        val binding = binding ?: return
+        val traktCredentials = TraktCredentials.get(requireContext())
+        val hasCredentials = traktCredentials.hasCredentials()
         if (hasCredentials) {
-            String username = traktCredentials.getUsername();
-            String displayName = traktCredentials.getDisplayName();
-            if (!TextUtils.isEmpty(displayName)) {
-                username += " (" + displayName + ")";
+            var username = traktCredentials.username
+            val displayName = traktCredentials.displayName
+            if (!displayName.isNullOrEmpty()) {
+                username += " ($displayName)"
             }
-            binding.textViewTraktUser.setText(username);
-            setAccountButtonState(false);
-            binding.buttonTraktLibrary.setVisibility(View.VISIBLE);
+            binding.textViewTraktUser.text = username
+            setAccountButtonState(false)
+            binding.buttonTraktLibrary.visibility = View.VISIBLE
         } else {
-            binding.textViewTraktUser.setText(null);
-            setAccountButtonState(true);
-            binding.buttonTraktLibrary.setVisibility(View.GONE);
+            binding.textViewTraktUser.text = null
+            setAccountButtonState(true)
+            binding.buttonTraktLibrary.visibility = View.GONE
         }
     }
 
-    private void connect() {
-        binding.buttonTraktConnect.setEnabled(false);
-        startActivity(new Intent(getActivity(), TraktAuthActivity.class));
+    private fun connect() {
+        val binding = binding ?: return
+        binding.buttonTraktConnect.isEnabled = false
+        startActivity(Intent(activity, TraktAuthActivity::class.java))
     }
 
-    private void disconnect() {
-        TraktCredentials.get(requireContext()).removeCredentials();
-        updateViews();
+    private fun disconnect() {
+        TraktCredentials.get(requireContext()).removeCredentials()
+        updateViews()
     }
 
-    private void setAccountButtonState(boolean connectEnabled) {
-        Button buttonAccount = binding.buttonTraktConnect;
-        buttonAccount.setEnabled(true);
-        buttonAccount.setText(connectEnabled ? R.string.connect : R.string.disconnect);
+    private fun setAccountButtonState(connectEnabled: Boolean) {
+        val buttonAccount = binding?.buttonTraktConnect ?: return
+        buttonAccount.isEnabled = true
+        buttonAccount.setText(if (connectEnabled) R.string.connect else R.string.disconnect)
         if (connectEnabled) {
-            buttonAccount.setOnClickListener(v -> connect());
+            buttonAccount.setOnClickListener { connect() }
         } else {
-            buttonAccount.setOnClickListener(v -> disconnect());
+            buttonAccount.setOnClickListener { disconnect() }
         }
     }
 }
