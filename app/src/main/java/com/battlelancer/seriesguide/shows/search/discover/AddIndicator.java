3206cc594d75139c43640fd2db49d99f0f9486ce
Uwe Trottmann <uwe+git@uwetrottmann.com>
2022-10-20 11:07:16 +0200

    View binding: use for AddIndicator.

--- app/src/main/java/com/battlelancer/seriesguide/shows/search/discover/AddIndicator.java
+++ app/src/main/java/com/battlelancer/seriesguide/shows/search/discover/AddIndicator.java
@@ -4,54 +4,45 @@ import android.content.Context;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
 import android.widget.FrameLayout;
-import android.widget.ImageView;
-import android.widget.ProgressBar;
-import butterknife.BindView;
-import butterknife.ButterKnife;
-import com.battlelancer.seriesguide.R;
+import com.battlelancer.seriesguide.databinding.LayoutAddIndicatorBinding;
 
 public class AddIndicator extends FrameLayout {
 
-    @BindView(R.id.imageViewAddIndicator) ImageView add;
-    @BindView(R.id.imageViewAddIndicatorAdded) ImageView added;
-    @BindView(R.id.progressBarAddIndicator) ProgressBar progressBar;
+    private final LayoutAddIndicatorBinding binding;
 
     public AddIndicator(Context context, AttributeSet attrs) {
         super(context, attrs);
-
-        LayoutInflater.from(context).inflate(R.layout.layout_add_indicator, this);
+        binding = LayoutAddIndicatorBinding.inflate(LayoutInflater.from(context), this);
     }
 
     @Override
     protected void onFinishInflate() {
         super.onFinishInflate();
-        ButterKnife.bind(this);
-
-        added.setVisibility(GONE);
-        progressBar.setVisibility(GONE);
+        binding.imageViewAddIndicatorAdded.setVisibility(GONE);
+        binding.progressBarAddIndicator.setVisibility(GONE);
     }
 
     public void setContentDescriptionAdded(CharSequence contentDescription) {
-        added.setContentDescription(contentDescription);
+        binding.imageViewAddIndicatorAdded.setContentDescription(contentDescription);
     }
 
     public void setOnAddClickListener(OnClickListener onClickListener) {
-        add.setOnClickListener(onClickListener);
+        binding.imageViewAddIndicator.setOnClickListener(onClickListener);
     }
 
     public void setState(int state) {
         if (state == SearchResult.STATE_ADD) {
-            add.setVisibility(VISIBLE);
-            progressBar.setVisibility(GONE);
-            added.setVisibility(GONE);
+            binding.imageViewAddIndicator.setVisibility(VISIBLE);
+            binding.progressBarAddIndicator.setVisibility(GONE);
+            binding.imageViewAddIndicatorAdded.setVisibility(GONE);
         } else if (state == SearchResult.STATE_ADDING) {
-            add.setVisibility(GONE);
-            progressBar.setVisibility(VISIBLE);
-            added.setVisibility(GONE);
+            binding.imageViewAddIndicator.setVisibility(GONE);
+            binding.progressBarAddIndicator.setVisibility(VISIBLE);
+            binding.imageViewAddIndicatorAdded.setVisibility(GONE);
         } else if (state == SearchResult.STATE_ADDED) {
-            add.setVisibility(GONE);
-            progressBar.setVisibility(GONE);
-            added.setVisibility(VISIBLE);
+            binding.imageViewAddIndicator.setVisibility(GONE);
+            binding.progressBarAddIndicator.setVisibility(GONE);
+            binding.imageViewAddIndicatorAdded.setVisibility(VISIBLE);
         }
     }
 }
