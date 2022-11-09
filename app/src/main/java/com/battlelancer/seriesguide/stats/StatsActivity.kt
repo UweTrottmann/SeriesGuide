41c5dcc663e449e67c715e89c0313fe0d955b2ca
Uwe Trottmann <uwe+git@uwetrottmann.com>
2022-11-09 16:50:45 +0100

    Kotlin: convert StatsActivity.

---
+++ app/src/main/java/com/battlelancer/seriesguide/stats/StatsActivity.kt
@@ -0,0 +1,40 @@
+package com.battlelancer.seriesguide.stats
+
+import android.os.Bundle
+import android.view.View
+import com.battlelancer.seriesguide.R
+import com.battlelancer.seriesguide.ui.BaseTopActivity
+
+/**
+ * Hosts fragments displaying statistics.
+ */
+class StatsActivity : BaseTopActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_stats)
+        setupActionBar()
+        setupBottomNavigation(R.id.navigation_item_stats)
+
+        if (savedInstanceState == null) {
+            val f = StatsFragment()
+            val ft = supportFragmentManager.beginTransaction()
+            ft.add(R.id.content_frame, f)
+            ft.commit()
+        }
+    }
+
+    override fun setupActionBar() {
+        super.setupActionBar()
+        supportActionBar?.setTitle(R.string.statistics)
+    }
+
+    override val snackbarParentView: View
+        get() = findViewById(R.id.coordinatorLayoutStats)
+
+    override fun onResume() {
+        super.onResume()
+        // prefs might have changed, update menu
+        invalidateOptionsMenu()
+    }
+}
