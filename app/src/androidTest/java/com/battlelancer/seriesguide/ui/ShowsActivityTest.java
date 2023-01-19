aea9b207e325cba0ad41419b8c4de74948dd971f
Uwe Trottmann <uwe+git@uwetrottmann.com>
2023-01-19 17:58:12 +0100

    ShowsActivityTest: simplify, add comments.

--- app/src/androidTest/java/com/battlelancer/seriesguide/ui/ShowsActivityTest.java
+++ app/src/androidTest/java/com/battlelancer/seriesguide/ui/ShowsActivityTest.java
@@ -5,26 +5,17 @@ import static androidx.test.espresso.Espresso.pressBack;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.contrib.RecyclerViewActions.actionOnItemAtPosition;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
-import static androidx.test.espresso.matcher.ViewMatchers.withClassName;
-import static androidx.test.espresso.matcher.ViewMatchers.withContentDescription;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
 import static org.hamcrest.Matchers.allOf;
-import static org.hamcrest.Matchers.is;
 
 import android.content.Context;
-import android.view.View;
-import android.view.ViewGroup;
-import android.view.ViewParent;
 import androidx.test.core.app.ApplicationProvider;
 import androidx.test.espresso.ViewInteraction;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.rule.ActivityTestRule;
 import com.battlelancer.seriesguide.R;
 import com.battlelancer.seriesguide.provider.SgRoomDatabase;
-import org.hamcrest.Description;
-import org.hamcrest.Matcher;
-import org.hamcrest.TypeSafeMatcher;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
@@ -33,7 +24,7 @@ import org.junit.runner.RunWith;
 /**
  * To avoid flakiness, turn off system animations on the virtual or physical devices used for
  * testing. On your device, under Settings > Developer options, disable the following 3 settings:
- *
+ * <p>
  * - Window animation scale
  * - Transition animation scale
  * - Animator duration scale
@@ -41,6 +32,9 @@ import org.junit.runner.RunWith;
 @RunWith(AndroidJUnit4.class)
 public class ShowsActivityTest {
 
+    // The new ActivityScenarioRule crashes when launching the activity with
+    // java.io.IOException: Failed to load asset path /data/app/~~1yCLpUnAoaSjRHXrCd_Q5Q==/com.battlelancer.seriesguide-aHv2_nXKaE_sVSlEuX_yig==/base.apk
+    @SuppressWarnings("deprecation")
     @Rule
     public ActivityTestRule<ShowsActivity> mActivityTestRule = new ActivityTestRule<>(
             ShowsActivity.class);
@@ -63,12 +57,9 @@ public class ShowsActivityTest {
             e.printStackTrace();
         }
 
+        // Open discover screen
         ViewInteraction floatingActionButton = onView(
-                allOf(withId(R.id.buttonShowsAdd), withContentDescription(R.string.action_shows_add),
-                        childAtPosition(
-                                withId(R.id.rootLayoutShows),
-                                2),
-                        isDisplayed()));
+                allOf(withId(R.id.buttonShowsAdd), isDisplayed()));
         floatingActionButton.perform(click());
 
         // Added a sleep statement to match the app's execution delay.
@@ -80,29 +71,23 @@ public class ShowsActivityTest {
             e.printStackTrace();
         }
 
-        ViewInteraction recyclerView = onView(
-                allOf(withId(R.id.recyclerViewShowsDiscover),
-                        childAtPosition(
-                                withId(R.id.constraintLayoutShowsDiscover),
-                                1)));
-        recyclerView.perform(actionOnItemAtPosition(2, click()));
+        // Open add show dialog
+        // Note: first up to 4 items might be links.
+        int showPosition = 5;
+        ViewInteraction recyclerView = onView(withId(R.id.recyclerViewShowsDiscover));
+        recyclerView.perform(actionOnItemAtPosition(showPosition, click()));
 
+        // Click to add the show
         ViewInteraction appCompatButton = onView(
                 allOf(withId(R.id.buttonPositive), withText(R.string.action_shows_add),
-                        childAtPosition(
-                                childAtPosition(
-                                        withClassName(is("android.widget.LinearLayout")),
-                                        2),
-                                2),
                         isDisplayed()));
         appCompatButton.perform(click());
 
+        // Click on the show again to open it (this will wait until adding is finished due to
+        // the progress indicator being shown)
         ViewInteraction recyclerView2 = onView(
-                allOf(withId(R.id.recyclerViewShowsDiscover),
-                        childAtPosition(
-                                withId(R.id.constraintLayoutShowsDiscover),
-                                1)));
-        recyclerView2.perform(actionOnItemAtPosition(2, click()));
+                allOf(withId(R.id.recyclerViewShowsDiscover)));
+        recyclerView2.perform(actionOnItemAtPosition(showPosition, click()));
 
         // Added a sleep statement to match the app's execution delay.
         // The recommended way to handle such scenarios is to use Espresso idling resources:
@@ -139,24 +124,5 @@ public class ShowsActivityTest {
         }
 
         pressBack();
-    }
-
-    private static Matcher<View> childAtPosition(final Matcher<View> parentMatcher,
-            final int position) {
-
-        return new TypeSafeMatcher<View>() {
-            @Override
-            public void describeTo(Description description) {
-                description.appendText("Child at position " + position + " in parent ");
-                parentMatcher.describeTo(description);
-            }
-
-            @Override
-            public boolean matchesSafely(View view) {
-                ViewParent parent = view.getParent();
-                return parent instanceof ViewGroup && parentMatcher.matches(parent)
-                        && view.equals(((ViewGroup) parent).getChildAt(position));
-            }
-        };
     }
 }
