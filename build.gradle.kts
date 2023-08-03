db3834f7e8cd3cecf58e85495731d21bf06be637
Uwe Trottmann <uwe+git@uwetrottmann.com>
2023-08-03 07:52:36 +0200

    AGP: update to 8.1.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -27,7 +27,7 @@ buildscript {
     }
 
     dependencies {
-        classpath("com.android.tools.build:gradle:7.4.2") // libraries, SeriesGuide
+        classpath("com.android.tools.build:gradle:8.1.0") // libraries, SeriesGuide
         classpath(libs.kotlin.gradle)
         classpath("com.google.cloud.tools:endpoints-framework-gradle-plugin:2.1.0") // SeriesGuide
         // Firebase Crashlytics
