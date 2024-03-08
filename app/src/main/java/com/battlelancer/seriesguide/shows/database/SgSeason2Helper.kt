UweTrottmann/SeriesGuide

commit 065f054a40a5173f0fe69896610c73055c291de9

Seasons: calc stats on demand

app/src/main/java/com/battlelancer/seriesguide/shows/database/SgSeason2Helper.kt
@@ -80,9 +80,6 @@ interface SgSeason2Helper {
     @Query("SELECT * FROM sg_season WHERE series_id = :showId ORDER BY season_number ASC")
     fun getSeasonsForExport(showId: Long): List<SgSeason2>
 
-    @Update(entity = SgSeason2::class)
-    fun updateSeasonCounters(seasonCountUpdate: SgSeason2CountUpdate)
-
     @Query("DELETE FROM sg_season WHERE series_id = :showId")
     suspend fun deleteSeasonsOfShow(showId: Long): Int
 }
@@ -97,15 +94,6 @@ data class SgSeason2Numbers(
     val number: Int
         get() = numberOrNull ?: 0 // == Specials, but should ignore seasons without number.
 }
-
-data class SgSeason2CountUpdate(
-    @ColumnInfo(name = SgSeason2Columns._ID) val id: Long,
-    @ColumnInfo(name = SgSeason2Columns.WATCHCOUNT) val notWatchedReleasedCount: Int,
-    @ColumnInfo(name = SgSeason2Columns.UNAIREDCOUNT) val notWatchedToBeReleasedCount: Int,
-    @ColumnInfo(name = SgSeason2Columns.NOAIRDATECOUNT) val notWatchedNoReleaseCount: Int,
-    @ColumnInfo(name = SgSeason2Columns.TOTALCOUNT) val totalCount: Int,
-    @ColumnInfo(name = SgSeason2Columns.TAGS) val tags: String
-)
 
 data class SgSeason2Update(
     @ColumnInfo(name = SgSeason2Columns._ID) val id: Long,

app/src/main/java/com/battlelancer/seriesguide/shows/database/SgEpisode2Helper.kt
@@ -1,5 +1,5 @@
-// Copyright 2023 Uwe Trottmann
 // SPDX-License-Identifier: Apache-2.0
+// Copyright 2021-2024 Uwe Trottmann
 
 package com.battlelancer.seriesguide.shows.database
 
@@ -261,16 +261,16 @@ interface SgEpisode2Helper {
     fun countNotWatchedEpisodesOfShow(showId: Long, currentTimeToolsTime: Long): Int
 
     @Query("SELECT COUNT(_id) FROM sg_episode WHERE season_id = :seasonId")
-    fun countEpisodesOfSeason(seasonId: Long): Int
+    suspend fun countEpisodesOfSeason(seasonId: Long): Int
 
     @Query("SELECT COUNT(_id) FROM sg_episode WHERE season_id = :seasonId AND episode_watched = ${EpisodeFlags.UNWATCHED} AND episode_firstairedms != ${SgEpisode2.EPISODE_UNKNOWN_RELEASE} AND episode_firstairedms <= :currentTimeToolsTime")
-    fun countNotWatchedReleasedEpisodesOfSeason(seasonId: Long, currentTimeToolsTime: Long): Int
+    suspend fun countNotWatchedReleasedEpisodesOfSeason(seasonId: Long, currentTimeToolsTime: Long): Int
 
     @Query("SELECT COUNT(_id) FROM sg_episode WHERE season_id = :seasonId AND episode_watched = ${EpisodeFlags.UNWATCHED} AND episode_firstairedms > :currentTimeToolsTime")
-    fun countNotWatchedToBeReleasedEpisodesOfSeason(seasonId: Long, currentTimeToolsTime: Long): Int
+    suspend fun countNotWatchedToBeReleasedEpisodesOfSeason(seasonId: Long, currentTimeToolsTime: Long): Int
 
     @Query("SELECT COUNT(_id) FROM sg_episode WHERE season_id = :seasonId AND episode_watched = ${EpisodeFlags.UNWATCHED} AND episode_firstairedms = ${SgEpisode2.EPISODE_UNKNOWN_RELEASE}")
-    fun countNotWatchedNoReleaseEpisodesOfSeason(seasonId: Long): Int
+    suspend fun countNotWatchedNoReleaseEpisodesOfSeason(seasonId: Long): Int
 
     @Query("SELECT COUNT(_id) FROM sg_episode WHERE season_id = :seasonId AND episode_watched = ${EpisodeFlags.SKIPPED}")
     fun countSkippedEpisodesOfSeason(seasonId: Long): Int

app/src/main/java/com/battlelancer/seriesguide/shows/database/SgSeason2.kt
@@ -1,5 +1,5 @@
-// Copyright 2023 Uwe Trottmann
 // SPDX-License-Identifier: Apache-2.0
+// Copyright 2021-2024 Uwe Trottmann
 
 package com.battlelancer.seriesguide.shows.database
 
@@ -36,12 +36,4 @@ data class SgSeason2(
 ) {
     val number: Int
         get() = numberOrNull ?: 0 // == Specials, but should ignore seasons without number.
-    val notWatchedReleased: Int
-        get() = notWatchedReleasedOrNull ?: 0
-    val notWatchedToBeReleased: Int
-        get() = notWatchedToBeReleasedOrNull ?: 0
-    val notWatchedNoRelease: Int
-        get() = notWatchedNoReleaseOrNull ?: 0
-    val total: Int
-        get() = totalOrNull ?: 0
 }

app/src/main/java/com/battlelancer/seriesguide/shows/overview/SeasonsAdapter.kt
@@ -1,5 +1,5 @@
-// Copyright 2023 Uwe Trottmann
 // SPDX-License-Identifier: Apache-2.0
+// Copyright 2021-2024 Uwe Trottmann
 
 package com.battlelancer.seriesguide.shows.overview
 
@@ -15,6 +15,7 @@ import androidx.recyclerview.widget.RecyclerView
 import com.battlelancer.seriesguide.R
 import com.battlelancer.seriesguide.databinding.ItemSeasonBinding
 import com.battlelancer.seriesguide.shows.database.SgSeason2
+import com.battlelancer.seriesguide.shows.overview.SeasonsViewModel.SgSeasonWithStats
 import com.battlelancer.seriesguide.util.TextTools
 import com.uwetrottmann.androidutils.AndroidUtils
 
@@ -24,7 +25,7 @@ import com.uwetrottmann.androidutils.AndroidUtils
 class SeasonsAdapter(
     private val context: Context,
     private val itemClickListener: ItemClickListener
-) : ListAdapter<SgSeason2, SeasonsAdapter.ViewHolder>(SgSeason2DiffCallback()) {
+) : ListAdapter<SgSeasonWithStats, SeasonsAdapter.ViewHolder>(SgSeason2DiffCallback()) {
 
     interface ItemClickListener {
         fun onItemClick(v: View, seasonRowId: Long)
@@ -52,19 +53,20 @@ class SeasonsAdapter(
             }
         }
 
-        fun bindTo(season: SgSeason2, context: Context) {
-            this.season = season
+        fun bindTo(seasonWithStats: SgSeasonWithStats, context: Context) {
+            val season = seasonWithStats.season.also { this.season = it }
+            val stats = seasonWithStats.stats
 
             // Title
             binding.textViewSeasonTitle.text = SeasonTools.getSeasonString(context, season.number)
 
             // Not watched episodes by type.
-            val released = season.notWatchedReleased
-            val toBeReleased = season.notWatchedToBeReleased
-            val noRelease = season.notWatchedNoRelease
+            val released = stats.notWatchedReleased
+            val toBeReleased = stats.notWatchedToBeReleased
+            val noRelease = stats.notWatchedNoRelease
 
             // Progress bar
-            val max = season.total
+            val max = stats.total
             val progress = max - released - toBeReleased - noRelease
             binding.progressBarSeason.apply {
                 this.max = max
@@ -157,10 +159,12 @@ class SeasonsAdapter(
 
 }
 
-class SgSeason2DiffCallback : DiffUtil.ItemCallback<SgSeason2>() {
-    override fun areItemsTheSame(oldItem: SgSeason2, newItem: SgSeason2): Boolean =
-        oldItem.id == newItem.id
+class SgSeason2DiffCallback : DiffUtil.ItemCallback<SgSeasonWithStats>() {
+    override fun areItemsTheSame(oldItem: SgSeasonWithStats, newItem: SgSeasonWithStats): Boolean =
+        oldItem.season.id == newItem.season.id
 
-    override fun areContentsTheSame(oldItem: SgSeason2, newItem: SgSeason2): Boolean =
-        oldItem == newItem
+    override fun areContentsTheSame(
+        oldItem: SgSeasonWithStats,
+        newItem: SgSeasonWithStats
+    ): Boolean = oldItem == newItem
 }

app/src/main/java/com/battlelancer/seriesguide/shows/overview/SeasonsFragment.kt
@@ -225,11 +225,7 @@ class SeasonsFragment() : Fragment() {
         model.remainingCountData.load(showId)
         if (event.flagJob is SeasonWatchedJob) {
             // If we can narrow it down to just one season...
-            UnwatchedUpdateWorker.updateUnwatchedCountFor(
-                requireContext(),
-                showId,
-                event.flagJob.seasonId
-            )
+            model.updateSeasonStats(event.flagJob.seasonId)
         } else {
             updateUnwatchedCounts()
         }
@@ -278,7 +274,7 @@ class SeasonsFragment() : Fragment() {
      * notifies provider causing the loader to reload.
      */
     private fun updateUnwatchedCounts() {
-        UnwatchedUpdateWorker.updateUnwatchedCountFor(requireContext(), showId)
+        model.updateSeasonStats(null)
     }
 
     private fun handleRemainingCountUpdate(result: RemainingCountLiveData.Result?) {

app/src/main/java/com/battlelancer/seriesguide/shows/overview/SeasonsViewModel.kt
@@ -9,12 +9,16 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.viewModelScope
 import com.battlelancer.seriesguide.provider.SgRoomDatabase
+import com.battlelancer.seriesguide.shows.database.SgSeason2
+import com.battlelancer.seriesguide.util.TimeTools
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.flow.flatMapLatest
 import kotlinx.coroutines.flow.flowOn
 import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.launch
 
 class SeasonsViewModel(
     application: Application,
@@ -22,6 +26,20 @@ class SeasonsViewModel(
 ) : AndroidViewModel(application) {
 
     private val order = MutableStateFlow(SeasonsSettings.getSeasonSortOrder(getApplication()))
+
+    data class SeasonStats(
+        val total: Int,
+        val notWatchedReleased: Int,
+        val notWatchedToBeReleased: Int,
+        val notWatchedNoRelease: Int
+    )
+
+    data class SgSeasonWithStats(
+        val season: SgSeason2,
+        val stats: SeasonStats
+    )
+
+    private val seasonStats = MutableStateFlow(mapOf<Long, SeasonStats>())
 
     val seasonsWithStats = order
         .flatMapLatest {
@@ -32,10 +50,13 @@ class SeasonsViewModel(
                 helper.getSeasonsOfShowOldestFirst(showId)
             }
         }
+        .combine(seasonStats) { seasons, stats ->
+            seasons.map { SgSeasonWithStats(it, stats[it.id] ?: SeasonStats(0, 0, 0, 0)) }
+        }
         .flowOn(Dispatchers.IO)
         .stateIn(
             scope = viewModelScope,
-            started = SharingStarted.WhileSubscribed(),
+            started = SharingStarted.WhileSubscribed(5000),
             initialValue = listOf()
         )
 
@@ -43,6 +64,42 @@ class SeasonsViewModel(
 
     fun updateOrder() {
         order.value = SeasonsSettings.getSeasonSortOrder(getApplication())
+    }
+
+    /**
+     * Updates episode counts for a specific [seasonIdToUpdate] or all seasons if null.
+     */
+    fun updateSeasonStats(seasonIdToUpdate: Long?) {
+        viewModelScope.launch(Dispatchers.IO) {
+            val database = SgRoomDatabase.getInstance(getApplication())
+
+            val seasonIds = if (seasonIdToUpdate != null) {
+                listOf(seasonIdToUpdate)
+            } else {
+                database.sgSeason2Helper().getSeasonIdsOfShow(showId)
+            }
+
+            val newMap = seasonStats.value.toMutableMap()
+            val helper = database.sgEpisode2Helper()
+            val currentTime = TimeTools.getCurrentTime(getApplication())
+            for (seasonId in seasonIds) {
+                newMap[seasonId] = SeasonStats(
+                    total = helper.countEpisodesOfSeason(seasonId),
+                    notWatchedReleased = helper.countNotWatchedReleasedEpisodesOfSeason(
+                        seasonId,
+                        currentTime
+                    ),
+                    notWatchedToBeReleased = helper.countNotWatchedToBeReleasedEpisodesOfSeason(
+                        seasonId,
+                        currentTime
+                    ),
+                    notWatchedNoRelease = helper.countNotWatchedNoReleaseEpisodesOfSeason(
+                        seasonId
+                    ),
+                )
+            }
+            seasonStats.value = newMap
+        }
     }
 
 }

app/src/main/java/com/battlelancer/seriesguide/shows/overview/UnwatchedUpdateWorker.kt
@@ -1,76 +1,0 @@
-// Copyright 2023 Uwe Trottmann
-// SPDX-License-Identifier: Apache-2.0
-
-package com.battlelancer.seriesguide.shows.overview
-
-import android.content.Context
-import com.battlelancer.seriesguide.SgApp
-import com.battlelancer.seriesguide.provider.SgRoomDatabase
-import com.battlelancer.seriesguide.shows.database.SgSeason2CountUpdate
-import com.battlelancer.seriesguide.util.TimeTools
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
-import timber.log.Timber
-
-object UnwatchedUpdateWorker {
-
-    /**
-     * Updates episode counts for a specific season or all seasons of a show.
-     *
-     * Runs all calls on a single thread to prevent parallel execution.
-     *
-     * May be cancelled when the app process dies.
-     */
-    fun updateUnwatchedCountFor(context: Context, showRowId: Long, seasonRowId: Long = -1) {
-        SgApp.coroutineScope.launch {
-            updateUnwatchedCount(context.applicationContext, showRowId, seasonRowId)
-        }
-    }
-
-    private suspend fun updateUnwatchedCount(context: Context, showRowId: Long, seasonRowId: Long) =
-        withContext(SgApp.SINGLE) {
-            if (showRowId <= 0) {
-                Timber.e("Not running: invalid show row ID.")
-            }
-
-            val helper = SgRoomDatabase.getInstance(context).sgSeason2Helper()
-            if (seasonRowId != -1L) {
-                // update one season
-                updateUnwatchedCountForSeason(context, seasonRowId)
-            } else {
-                // update all seasons of this show, start with the most recent one
-                val seasons = helper.getSeasonIdsOfShow(showRowId)
-                for (seasonId in seasons) {
-                    updateUnwatchedCountForSeason(context, seasonId)
-                }
-            }
-
-            Timber.d("Updated watched count: show %d, season %d", showRowId, seasonRowId)
-        }
-
-    private fun updateUnwatchedCountForSeason(context: Context, seasonRowId: Long) {
-        val database = SgRoomDatabase.getInstance(context)
-        val helper = database.sgEpisode2Helper()
-        val currentTime = TimeTools.getCurrentTime(context)
-
-        val skippedCount = helper.countSkippedEpisodesOfSeason(seasonRowId)
-
-        database.sgSeason2Helper().updateSeasonCounters(
-            SgSeason2CountUpdate(
-                seasonRowId,
-                totalCount = helper.countEpisodesOfSeason(seasonRowId),
-                notWatchedReleasedCount = helper.countNotWatchedReleasedEpisodesOfSeason(
-                    seasonRowId,
-                    currentTime
-                ),
-                notWatchedToBeReleasedCount = helper.countNotWatchedToBeReleasedEpisodesOfSeason(
-                    seasonRowId,
-                    currentTime
-                ),
-                notWatchedNoReleaseCount = helper.countNotWatchedNoReleaseEpisodesOfSeason(seasonRowId),
-                tags = if (skippedCount > 0) SeasonTags.SKIPPED else SeasonTags.NONE
-            )
-        )
-    }
-
-}

app/src/main/java/com/battlelancer/seriesguide/util/TimeTools.kt
@@ -1,5 +1,5 @@
-// Copyright 2023 Uwe Trottmann
 // SPDX-License-Identifier: Apache-2.0
+// Copyright 2014-2019, 2021-2024 Uwe Trottmann
 
 package com.battlelancer.seriesguide.util
 
@@ -72,7 +72,7 @@ object TimeTools {
      *
      * Note: this may seem harsh, but is equal to how "to be released" are calculated for seasons.
      *
-     * @see com.battlelancer.seriesguide.shows.overview.UnwatchedUpdateWorker
+     * @see com.battlelancer.seriesguide.shows.overview.SeasonsViewModel.updateSeasonStats
      */
     fun isReleased(actualRelease: Date): Boolean {
         return actualRelease.before(Date(System.currentTimeMillis()))

commit ca43cf15b805266a2edc61dee8fceaca09498b26

Regression: fix seasons disappearing if total count is set to 0
Regression from
065f054a Seasons: calc stats on demand

app/src/main/java/com/battlelancer/seriesguide/shows/database/SgSeason2Helper.kt
@@ -65,18 +65,15 @@ interface SgSeason2Helper {
     /**
      * Excludes seasons where total episode count is 0.
      */
-    @Query("SELECT * FROM sg_season WHERE series_id = :showId AND season_totalcount != 0 ORDER BY season_number DESC")
+    @Query("SELECT * FROM sg_season WHERE series_id = :showId ORDER BY season_number DESC")
     fun getSeasonsOfShowLatestFirst(showId: Long): Flow<List<SgSeason2>>
 
     /**
      * Excludes seasons where total episode count is 0.
      */
-    @Query("SELECT * FROM sg_season WHERE series_id = :showId AND season_totalcount != 0 ORDER BY season_number ASC")
+    @Query("SELECT * FROM sg_season WHERE series_id = :showId ORDER BY season_number ASC")
     fun getSeasonsOfShowOldestFirst(showId: Long): Flow<List<SgSeason2>>
 
-    /**
-     * Note: does not exclude seasons based on season_totalcount because it might not be up-to-date.
-     */
     @Query("SELECT * FROM sg_season WHERE series_id = :showId ORDER BY season_number ASC")
     fun getSeasonsForExport(showId: Long): List<SgSeason2>
 