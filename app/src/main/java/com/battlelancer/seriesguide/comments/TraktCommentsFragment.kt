d4fc0294c0d5c603efc644851742674f9eeb0074
Uwe Trottmann <uwe+git@uwetrottmann.com>
2024-04-17 17:19:49 +0200

    Comments: do not error when commenting on a show

--- app/src/main/java/com/battlelancer/seriesguide/comments/TraktCommentsFragment.kt
+++ app/src/main/java/com/battlelancer/seriesguide/comments/TraktCommentsFragment.kt
@@ -28,7 +28,6 @@ import com.battlelancer.seriesguide.traktapi.TraktAction
 import com.battlelancer.seriesguide.traktapi.TraktTask
 import com.battlelancer.seriesguide.traktapi.TraktTask.TraktActionCompleteEvent
 import com.battlelancer.seriesguide.ui.widgets.SgFastScroller
-import com.battlelancer.seriesguide.util.Errors
 import com.battlelancer.seriesguide.util.ThemeUtils
 import com.battlelancer.seriesguide.util.ViewTools
 import com.battlelancer.seriesguide.util.WebTools
@@ -92,12 +91,7 @@ class TraktCommentsFragment : Fragment() {
         )
 
         binding.buttonShouts.setOnClickListener {
-            try {
-                comment()
-            } catch (e: IllegalArgumentException) {
-                Toast.makeText(requireContext(), R.string.database_error, Toast.LENGTH_LONG).show()
-                Errors.logAndReport("comment", e)
-            }
+            comment()
         }
 
         // disable comment button by default, enable if comment entered
@@ -177,9 +171,10 @@ class TraktCommentsFragment : Fragment() {
             @Suppress("DEPRECATION") // AsyncTask
             TraktTask(context).commentShow(showId, comment, isSpoiler)
                 .executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR)
+            return
         }
 
-        // if all ids were 0, do nothing
+        // Should never have launched without a valid ID
         throw IllegalArgumentException("comment: failed, all IDs 0 ($args)")
     }
 
