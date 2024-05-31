91faaaee7e1402541a84d8fd493fd6d113122145
Uwe Trottmann <uwe+git@uwetrottmann.com>
2024-05-31 08:15:51 +0200

    Shows discover: extract ShowsTraktActivity

--- app/src/main/java/com/battlelancer/seriesguide/shows/search/discover/ShowsTraktActivity.kt
+++ app/src/main/java/com/battlelancer/seriesguide/shows/search/discover/ShowsTraktActivity.kt
@@ -1,16 +1,13 @@
 // SPDX-License-Identifier: Apache-2.0
-// Copyright 2018-2024 Uwe Trottmann
+// Copyright 2024 Uwe Trottmann
 
 package com.battlelancer.seriesguide.shows.search.discover
 
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
-import androidx.core.view.isVisible
 import com.battlelancer.seriesguide.R
-import com.battlelancer.seriesguide.databinding.ActivityDiscoverShowsBinding
-import com.battlelancer.seriesguide.shows.search.newepisodes.ShowsNewEpisodesFragment
-import com.battlelancer.seriesguide.shows.search.popular.ShowsPopularFragment
+import com.battlelancer.seriesguide.databinding.ActivityTraktShowsBinding
 import com.battlelancer.seriesguide.shows.search.similar.SimilarShowsActivity
 import com.battlelancer.seriesguide.shows.search.similar.SimilarShowsFragment
 import com.battlelancer.seriesguide.ui.BaseMessageActivity
@@ -18,15 +15,15 @@ import com.battlelancer.seriesguide.util.TaskManager
 import com.battlelancer.seriesguide.util.ThemeUtils
 
 /**
- * Hosts [ShowsDiscoverPagingFragment] configured with [DiscoverShowsLink].
+ * Hosts [TraktAddFragment] configured by [DiscoverShowsLink].
  */
-class DiscoverShowsActivity : BaseMessageActivity(), AddShowDialogFragment.OnAddShowListener {
+class ShowsTraktActivity : BaseMessageActivity(), AddShowDialogFragment.OnAddShowListener {
 
-    lateinit var binding: ActivityDiscoverShowsBinding
+    lateinit var binding: ActivityTraktShowsBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityDiscoverShowsBinding.inflate(layoutInflater)
+        binding = ActivityTraktShowsBinding.inflate(layoutInflater)
         setContentView(binding.root)
         ThemeUtils.configureForEdgeToEdge(binding.root)
 
@@ -34,17 +31,13 @@ class DiscoverShowsActivity : BaseMessageActivity(), AddShowDialogFragment.OnAdd
 
         // Change the scrolling view the AppBarLayout should use to determine if it should lift.
         // This is required so the AppBarLayout does not flicker its background when scrolling.
-        binding.sgAppBarLayout.liftOnScrollTargetViewId =
-            ShowsDiscoverPagingFragment.liftOnScrollTargetViewId
+        binding.sgAppBarLayout.sgAppBarLayout.liftOnScrollTargetViewId =
+            TraktAddFragment.liftOnScrollTargetViewId
 
         setupActionBar(link)
 
         if (savedInstanceState == null) {
-            val fragment = when (link) {
-                DiscoverShowsLink.POPULAR -> ShowsPopularFragment()
-                DiscoverShowsLink.NEW_EPISODES -> ShowsNewEpisodesFragment()
-                else -> throw IllegalArgumentException("$link not supported")
-            }
+            val fragment = TraktAddFragment.newInstance(link)
             supportFragmentManager
                 .beginTransaction()
                 .add(R.id.containerTraktShowsFragment, fragment)
@@ -71,7 +64,7 @@ class DiscoverShowsActivity : BaseMessageActivity(), AddShowDialogFragment.OnAdd
 
         @JvmStatic
         fun intent(context: Context, link: DiscoverShowsLink): Intent {
-            return Intent(context, DiscoverShowsActivity::class.java).putExtra(EXTRA_LINK, link.id)
+            return Intent(context, ShowsTraktActivity::class.java).putExtra(EXTRA_LINK, link.id)
         }
     }
 
