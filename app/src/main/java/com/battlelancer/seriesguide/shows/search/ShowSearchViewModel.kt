70789e978825184720414fd4c837224eb35932f6
Uwe Trottmann <uwe+git@uwetrottmann.com>
2024-06-08 18:31:07 +0200

    Show search: if empty display all, sorted by last watched and title

--- app/src/main/java/com/battlelancer/seriesguide/shows/search/ShowSearchViewModel.kt
+++ app/src/main/java/com/battlelancer/seriesguide/shows/search/ShowSearchViewModel.kt
@@ -5,7 +5,6 @@
 package com.battlelancer.seriesguide.shows.search
 
 import android.app.Application
-import android.text.format.DateUtils
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.switchMap
@@ -13,7 +12,6 @@ import androidx.sqlite.db.SimpleSQLiteQuery
 import com.battlelancer.seriesguide.provider.SeriesGuideContract.SgShow2Columns
 import com.battlelancer.seriesguide.provider.SeriesGuideDatabase.Tables
 import com.battlelancer.seriesguide.provider.SgRoomDatabase
-import com.battlelancer.seriesguide.util.TimeTools
 
 class ShowSearchViewModel(application: Application) : AndroidViewModel(application) {
 
@@ -25,18 +23,17 @@ class ShowSearchViewModel(application: Application) : AndroidViewModel(applicati
         val query = if (searchTerm.isNullOrBlank()) {
             SimpleSQLiteQuery(
                 "SELECT * FROM ${Tables.SG_SHOW} " +
-                        "WHERE ${SgShow2Columns.NEXTEPISODE} != '' AND ${SgShow2Columns.HIDDEN} = 0 " +
-                        "AND ${SgShow2Columns.NEXTAIRDATEMS} < ? " +
-                        "ORDER BY ${SgShow2Columns.SORT_LATEST_EPISODE_THEN_STATUS}",
-                arrayOf((TimeTools.getCurrentTime(getApplication()) + DateUtils.HOUR_IN_MILLIS))
+                        "ORDER BY ${SgShow2Columns.SORT_LAST_WATCHED}, ${SgShow2Columns.SORT_TITLE}"
             )
         } else {
             // Query uses % at the beginning and end of the given string
             // so remove trailing whitespace and replace inner whitespaces with %
             // this improves matching if characters are left out, like "Mr Robot" vs "Mr. Robot"
             val filter = searchTerm.trim().replace("\\s".toRegex(), "%")
-            SimpleSQLiteQuery("SELECT * FROM ${Tables.SG_SHOW} " +
-                    "WHERE ${SgShow2Columns.TITLE} LIKE ?", arrayOf("%$filter%"))
+            SimpleSQLiteQuery(
+                "SELECT * FROM ${Tables.SG_SHOW} " +
+                        "WHERE ${SgShow2Columns.TITLE} LIKE ?", arrayOf("%$filter%")
+            )
         }
         database.sgShow2Helper().getShowsLiveData(query)
     }
