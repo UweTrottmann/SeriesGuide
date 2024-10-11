9c513739262fb34ec49adc6b26103310a57f9335
Uwe Trottmann <uwe+git@uwetrottmann.com>
2024-10-11 07:08:42 +0200

    Kotlin: convert EpisodeToolsTest

--- app/src/test/java/com/battlelancer/seriesguide/util/EpisodeToolsTest.kt
+++ app/src/test/java/com/battlelancer/seriesguide/util/EpisodeToolsTest.kt
@@ -1,41 +1,37 @@
-// Copyright 2023 Uwe Trottmann
 // SPDX-License-Identifier: Apache-2.0
+// Copyright 2013-2024 Uwe Trottmann
 
-package com.battlelancer.seriesguide.util;
+package com.battlelancer.seriesguide.util
 
-import static com.google.common.truth.Truth.assertThat;
-import static org.junit.Assert.fail;
+import com.battlelancer.seriesguide.shows.episodes.EpisodeFlags
+import com.battlelancer.seriesguide.shows.episodes.EpisodeTools
+import com.google.common.truth.Truth.assertThat
+import org.junit.Assert.assertThrows
+import org.junit.Test
 
-import com.battlelancer.seriesguide.shows.episodes.EpisodeFlags;
-import com.battlelancer.seriesguide.shows.episodes.EpisodeTools;
-import org.junit.Test;
-
-public class EpisodeToolsTest {
+class EpisodeToolsTest {
 
     @Test
-    public void test_isWatched() {
-        assertThat(EpisodeTools.isWatched(EpisodeFlags.WATCHED)).isTrue();
-        assertThat(EpisodeTools.isWatched(EpisodeFlags.SKIPPED)).isFalse();
-        assertThat(EpisodeTools.isWatched(EpisodeFlags.UNWATCHED)).isFalse();
+    fun test_isWatched() {
+        assertThat(EpisodeTools.isWatched(EpisodeFlags.WATCHED)).isTrue()
+        assertThat(EpisodeTools.isWatched(EpisodeFlags.SKIPPED)).isFalse()
+        assertThat(EpisodeTools.isWatched(EpisodeFlags.UNWATCHED)).isFalse()
     }
 
     @Test
-    public void test_isUnwatched() {
-        assertThat(EpisodeTools.isUnwatched(EpisodeFlags.UNWATCHED)).isTrue();
-        assertThat(EpisodeTools.isUnwatched(EpisodeFlags.WATCHED)).isFalse();
-        assertThat(EpisodeTools.isUnwatched(EpisodeFlags.SKIPPED)).isFalse();
+    fun test_isUnwatched() {
+        assertThat(EpisodeTools.isUnwatched(EpisodeFlags.UNWATCHED)).isTrue()
+        assertThat(EpisodeTools.isUnwatched(EpisodeFlags.WATCHED)).isFalse()
+        assertThat(EpisodeTools.isUnwatched(EpisodeFlags.SKIPPED)).isFalse()
     }
 
     @Test
-    public void test_validateFlags() {
-        EpisodeTools.validateFlags(EpisodeFlags.UNWATCHED);
-        EpisodeTools.validateFlags(EpisodeFlags.WATCHED);
-        EpisodeTools.validateFlags(EpisodeFlags.SKIPPED);
-        try {
-            EpisodeTools.validateFlags(123);
-            fail("IllegalArgumentException not thrown");
-        } catch (Exception e) {
-            assertThat(e).isInstanceOf(IllegalArgumentException.class);
+    fun test_validateFlags() {
+        EpisodeTools.validateFlags(EpisodeFlags.UNWATCHED)
+        EpisodeTools.validateFlags(EpisodeFlags.WATCHED)
+        EpisodeTools.validateFlags(EpisodeFlags.SKIPPED)
+        assertThrows(IllegalArgumentException::class.java) {
+            EpisodeTools.validateFlags(123)
         }
     }
 
