1ebd36e411a2eb256f72bb7042de4f9c0a908d87
Uwe Trottmann <uwe+git@uwetrottmann.com>
2024-10-17 11:37:13 +0200

    SearchResult: do no longer implement Parcelable
    This was used to pass SearchResult via Intent Bundles which has been
    removed a while ago.

--- app/src/main/java/com/battlelancer/seriesguide/shows/search/discover/SearchResult.java
+++ app/src/main/java/com/battlelancer/seriesguide/shows/search/discover/SearchResult.java
@@ -1,15 +1,12 @@
-// Copyright 2023 Uwe Trottmann
 // SPDX-License-Identifier: Apache-2.0
+// Copyright 2018-2024 Uwe Trottmann
 
 package com.battlelancer.seriesguide.shows.search.discover;
-
-import android.os.Parcel;
-import android.os.Parcelable;
 
 /**
  * Holds a search result, used later for adding this show. Supplying a poster URL is optional.
  */
-public class SearchResult implements Parcelable {
+public class SearchResult {
 
     public static final int STATE_ADD = 0;
     public static final int STATE_ADDING = 1;
@@ -23,28 +20,7 @@ public class SearchResult implements Parcelable {
     private String posterPath;
     private int state;
 
-    public static final Parcelable.Creator<SearchResult> CREATOR
-            = new Parcelable.Creator<SearchResult>() {
-        public SearchResult createFromParcel(Parcel in) {
-            return new SearchResult(in);
-        }
-
-        public SearchResult[] newArray(int size) {
-            return new SearchResult[size];
-        }
-    };
-
     public SearchResult() {
-    }
-
-    public SearchResult(Parcel in) {
-        setTvdbid(in.readInt());
-        setTmdbId(in.readInt());
-        setLanguage(in.readString());
-        setTitle(in.readString());
-        setOverview(in.readString());
-        setPosterPath(in.readString());
-        setState(in.readInt());
     }
 
     public SearchResult copy() {
@@ -57,22 +33,6 @@ public class SearchResult implements Parcelable {
         copy.setPosterPath(this.getPosterPath());
         copy.setState(this.getState());
         return copy;
-    }
-
-    @Override
-    public int describeContents() {
-        return hashCode();
-    }
-
-    @Override
-    public void writeToParcel(Parcel dest, int flags) {
-        dest.writeInt(getTvdbid());
-        dest.writeInt(getTmdbId());
-        dest.writeString(getLanguage());
-        dest.writeString(getTitle());
-        dest.writeString(getOverview());
-        dest.writeString(getPosterPath());
-        dest.writeInt(getState());
     }
 
     /**
