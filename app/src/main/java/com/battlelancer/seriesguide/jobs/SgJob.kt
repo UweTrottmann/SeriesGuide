f97b9d3e77c1fffc4437b23a0f9cdbbaa84a0dd9
Uwe Trottmann <mail@heyuwe.de>
2025-03-14 15:12:44 +0100

    Kotlin: convert SgJob

--- app/src/main/java/com/battlelancer/seriesguide/jobs/SgJob.kt
+++ app/src/main/java/com/battlelancer/seriesguide/jobs/SgJob.kt
@@ -1,32 +1,33 @@
-// Copyright 2023 Uwe Trottmann
 // SPDX-License-Identifier: Apache-2.0
+// Copyright 2018-2025 Uwe Trottmann
 
-package com.battlelancer.seriesguide.jobs;
+package com.battlelancer.seriesguide.jobs
 
-import androidx.room.ColumnInfo;
-import androidx.room.Entity;
-import androidx.room.Index;
-import androidx.room.PrimaryKey;
-import com.battlelancer.seriesguide.provider.SeriesGuideContract.Jobs;
-import com.battlelancer.seriesguide.provider.SeriesGuideDatabase.Tables;
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.Index
+import androidx.room.PrimaryKey
+import com.battlelancer.seriesguide.provider.SeriesGuideContract.Jobs
+import com.battlelancer.seriesguide.provider.SeriesGuideDatabase.Tables
 
 /**
  * Note: ensure to use CONFLICT_REPLACE when inserting to mimic SQLite UNIQUE x ON CONFLICT REPLACE.
  */
-@Entity(tableName = Tables.JOBS,
-        indices = {@Index(value = Jobs.CREATED_MS, unique = true)})
-public class SgJob {
-
+@Entity(
+    tableName = Tables.JOBS,
+    indices = [Index(value = [Jobs.CREATED_MS], unique = true)]
+)
+data class SgJob(
     @PrimaryKey(autoGenerate = true)
     @ColumnInfo(name = Jobs._ID)
-    public Integer id;
+    val id: Int? = null,
 
     @ColumnInfo(name = Jobs.CREATED_MS)
-    public Long createdMs;
+    val createdMs: Long? = null,
 
     @ColumnInfo(name = Jobs.TYPE)
-    public Integer type;
+    val type: Int? = null,
 
     @ColumnInfo(name = Jobs.EXTRAS)
-    public byte[] extras;
-}
+    val extras: ByteArray? = null
+)
