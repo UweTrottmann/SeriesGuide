76068a90f64eb9016d6c9960c8a0bad0d2ed2389
Uwe Trottmann <mail@heyuwe.de>
2025-03-21 10:03:13 +0100

    Kotlin: convert HexagonSync

--- app/src/main/java/com/battlelancer/seriesguide/sync/HexagonSync.kt
+++ app/src/main/java/com/battlelancer/seriesguide/sync/HexagonSync.kt
@@ -1,241 +1,220 @@
 // SPDX-License-Identifier: Apache-2.0
-// Copyright 2017-2024 Uwe Trottmann
+// Copyright 2017-2025 Uwe Trottmann
 
-package com.battlelancer.seriesguide.sync;
+package com.battlelancer.seriesguide.sync
 
-import android.content.Context;
-import androidx.annotation.NonNull;
-import androidx.preference.PreferenceManager;
-import com.battlelancer.seriesguide.SgApp;
-import com.battlelancer.seriesguide.backend.HexagonTools;
-import com.battlelancer.seriesguide.backend.settings.HexagonSettings;
-import com.battlelancer.seriesguide.movies.tools.MovieTools;
-import com.battlelancer.seriesguide.provider.SgRoomDatabase;
-import com.battlelancer.seriesguide.shows.database.SgShow2Helper;
-import com.battlelancer.seriesguide.shows.database.SgShow2Ids;
-import com.battlelancer.seriesguide.shows.tools.AddShowTask;
-import com.battlelancer.seriesguide.util.TaskManager;
-import com.uwetrottmann.androidutils.AndroidUtils;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
+import android.content.Context
+import androidx.preference.PreferenceManager
+import com.battlelancer.seriesguide.SgApp
+import com.battlelancer.seriesguide.backend.HexagonTools
+import com.battlelancer.seriesguide.backend.settings.HexagonSettings
+import com.battlelancer.seriesguide.movies.tools.MovieTools
+import com.battlelancer.seriesguide.provider.SgRoomDatabase
+import com.battlelancer.seriesguide.shows.tools.AddShowTask
+import com.battlelancer.seriesguide.util.TaskManager
+import com.uwetrottmann.androidutils.AndroidUtils
+import java.util.LinkedList
 
-public class HexagonSync {
+class HexagonSync(
+    private val context: Context,
+    private val hexagonTools: HexagonTools,
+    private val movieTools: MovieTools,
+    private val progress: SyncProgress
+) {
 
-    private final Context context;
-    private final HexagonTools hexagonTools;
-    private final MovieTools movieTools;
-    private final SyncProgress progress;
-
-    HexagonSync(Context context, HexagonTools hexagonTools,
-            MovieTools movieTools, SyncProgress progress) {
-        this.context = context;
-        this.hexagonTools = hexagonTools;
-        this.movieTools = movieTools;
-        this.progress = progress;
-    }
-
-    public static class HexagonResult {
-        public final boolean hasAddedShows;
-        public final boolean success;
-        public HexagonResult(boolean hasAddedShows, boolean success) {
-            this.hasAddedShows = hasAddedShows;
-            this.success = success;
-        }
-    }
+    data class HexagonResult(
+        val hasAddedShows: Boolean,
+        val success: Boolean
+    )
 
     /**
      * Syncs episodes, shows and movies with Hexagon.
      *
-     * <p> Merges shows, episodes and movies after a sign-in. Consecutive syncs will only download
+     * Merges shows, episodes and movies after a sign-in. Consecutive syncs will only download
      * changes to shows, episodes and movies.
      */
-    public HexagonResult sync() {
-        Map<Integer, Long> tmdbIdsToShowIds = SgApp.getServicesComponent(context).showTools()
-                .getTmdbIdsToShowIds();
+    fun sync(): HexagonResult {
+        val tmdbIdsToShowIds = SgApp.getServicesComponent(context).showTools()
+            .getTmdbIdsToShowIds()
 
         //// EPISODES
-        progress.publish(SyncProgress.Step.HEXAGON_EPISODES);
-        boolean syncEpisodesSuccessful = syncEpisodes(tmdbIdsToShowIds);
+        progress.publish(SyncProgress.Step.HEXAGON_EPISODES)
+        val syncEpisodesSuccessful = syncEpisodes(tmdbIdsToShowIds)
         if (!syncEpisodesSuccessful) {
-            progress.recordError();
+            progress.recordError()
         }
 
         //// SHOWS
-        progress.publish(SyncProgress.Step.HEXAGON_SHOWS);
-        HexagonResult syncShowsResult = syncShows(tmdbIdsToShowIds);
+        progress.publish(SyncProgress.Step.HEXAGON_SHOWS)
+        val syncShowsResult = syncShows(tmdbIdsToShowIds)
         if (!syncShowsResult.success) {
-            progress.recordError();
+            progress.recordError()
         }
 
         //// MOVIES
-        progress.publish(SyncProgress.Step.HEXAGON_MOVIES);
-        boolean syncMoviesSuccessful = syncMovies();
+        progress.publish(SyncProgress.Step.HEXAGON_MOVIES)
+        val syncMoviesSuccessful = syncMovies()
         if (!syncMoviesSuccessful) {
-            progress.recordError();
+            progress.recordError()
         }
 
         //// LISTS
-        progress.publish(SyncProgress.Step.HEXAGON_LISTS);
-        boolean syncListsSuccessful = syncLists();
+        progress.publish(SyncProgress.Step.HEXAGON_LISTS)
+        val syncListsSuccessful = syncLists()
         if (!syncListsSuccessful) {
-            progress.recordError();
+            progress.recordError()
         }
 
-        boolean success = syncEpisodesSuccessful
+        val success = syncEpisodesSuccessful
                 && syncShowsResult.success
                 && syncMoviesSuccessful
-                && syncListsSuccessful;
+                && syncListsSuccessful
 
-        return new HexagonResult(syncShowsResult.hasAddedShows, success);
+        return HexagonResult(syncShowsResult.hasAddedShows, success)
     }
 
-    private boolean syncEpisodes(@NonNull Map<Integer, Long> tmdbIdsToShowIds) {
-        // get shows that need episode merging
-        SgShow2Helper helper = SgRoomDatabase.getInstance(context).sgShow2Helper();
-        List<SgShow2Ids> showsToMerge = helper.getHexagonMergeNotCompleted();
+    private fun syncEpisodes(tmdbIdsToShowIds: Map<Int, Long>): Boolean {
+        val helper = SgRoomDatabase.getInstance(context).sgShow2Helper()
+        val showsToMerge = helper.getHexagonMergeNotCompleted()
 
         // try merging episodes for them
-        boolean mergeSuccessful = true;
-        HexagonEpisodeSync episodeSync = new HexagonEpisodeSync(context, hexagonTools);
-        for (SgShow2Ids show : showsToMerge) {
+        var mergeSuccessful = true
+        val episodeSync = HexagonEpisodeSync(context, hexagonTools)
+        for (show in showsToMerge) {
             // abort if connection is lost
             if (!AndroidUtils.isNetworkConnected(context)) {
-                return false;
+                return false
             }
 
             // TMDB ID is required, legacy shows with TVDB only data will no longer be synced.
-            Integer showTmdbId = show.getTmdbId();
-            if (showTmdbId == null || showTmdbId == 0) continue;
+            val showTmdbId = show.tmdbId ?: continue
+            if (showTmdbId == 0) continue;
 
-            boolean success = episodeSync.downloadFlags(show.getId(), showTmdbId, show.getTvdbId());
+            var success = episodeSync.downloadFlags(show.id, showTmdbId, show.tvdbId)
             if (!success) {
                 // try again next time
-                mergeSuccessful = false;
-                continue;
+                mergeSuccessful = false
+                continue
             }
 
-            success = episodeSync.uploadFlags(show.getId(), showTmdbId);
+            success = episodeSync.uploadFlags(show.id, showTmdbId)
             if (success) {
                 // set merge as completed
-                helper.setHexagonMergeCompleted(show.getId());
+                helper.setHexagonMergeCompleted(show.id)
             } else {
-                mergeSuccessful = false;
+                mergeSuccessful = false
             }
         }
 
         // download changed episodes and update properties on existing episodes
-        boolean changedDownloadSuccessful = episodeSync.downloadChangedFlags(tmdbIdsToShowIds);
+        val changedDownloadSuccessful = episodeSync.downloadChangedFlags(tmdbIdsToShowIds)
 
-        return mergeSuccessful && changedDownloadSuccessful;
+        return mergeSuccessful && changedDownloadSuccessful
     }
 
-    private HexagonResult syncShows(Map<Integer, Long> tmdbIdsToShowIds) {
-        boolean hasMergedShows = HexagonSettings.hasMergedShows(context);
+    private fun syncShows(tmdbIdsToShowIds: Map<Int, Long>): HexagonResult {
+        val hasMergedShows = HexagonSettings.hasMergedShows(context)
 
         // download shows and apply property changes (if merging only overwrite some properties)
-        HexagonShowSync showSync = new HexagonShowSync(context, hexagonTools);
-        HashMap<Integer, AddShowTask.Show> newShows = new HashMap<>();
-        boolean downloadSuccessful = showSync.download(tmdbIdsToShowIds, newShows, hasMergedShows);
+        val showSync = HexagonShowSync(context, hexagonTools)
+        val newShows = HashMap<Int, AddShowTask.Show>()
+        val downloadSuccessful = showSync.download(tmdbIdsToShowIds, newShows, hasMergedShows)
         if (!downloadSuccessful) {
-            return new HexagonResult(false, false);
+            return HexagonResult(false, false)
         }
 
         // if merge required, upload all shows to Hexagon
         if (!hasMergedShows) {
-            boolean uploadSuccessful = showSync.uploadAll();
+            val uploadSuccessful = showSync.uploadAll()
             if (!uploadSuccessful) {
-                return new HexagonResult(false, false);
+                return HexagonResult(false, false)
             }
         }
 
         // add new shows
-        boolean addNewShows = !newShows.isEmpty();
+        val addNewShows = newShows.isNotEmpty()
         if (addNewShows) {
-            List<AddShowTask.Show> newShowsList = new LinkedList<>(newShows.values());
-            TaskManager.performAddTask(context, newShowsList, true, !hasMergedShows);
+            val newShowsList = LinkedList(newShows.values)
+            TaskManager.performAddTask(context, newShowsList, true, !hasMergedShows)
         } else if (!hasMergedShows) {
             // set shows as merged
-            HexagonSettings.setHasMergedShows(context, true);
+            HexagonSettings.setHasMergedShows(context, true)
         }
 
-        return new HexagonResult(addNewShows, true);
+        return HexagonResult(addNewShows, true)
     }
 
-    private boolean syncMovies() {
-        boolean hasMergedMovies = HexagonSettings.hasMergedMovies(context);
+    private fun syncMovies(): Boolean {
+        val hasMergedMovies = HexagonSettings.hasMergedMovies(context)
 
         // download movies and apply property changes, build list of new movies
-        Set<Integer> newCollectionMovies = new HashSet<>();
-        Set<Integer> newWatchlistMovies = new HashSet<>();
-        Map<Integer, Integer> newWatchedMoviesToPlays = new HashMap<>();
-        HexagonMovieSync movieSync = new HexagonMovieSync(context, hexagonTools);
-        boolean downloadSuccessful = movieSync.download(
-                newCollectionMovies,
-                newWatchlistMovies,
-                newWatchedMoviesToPlays,
-                hasMergedMovies
-        );
+        val newCollectionMovies = HashSet<Int>()
+        val newWatchlistMovies = HashSet<Int>()
+        val newWatchedMoviesToPlays = HashMap<Int, Int>()
+        val movieSync = HexagonMovieSync(context, hexagonTools)
+        val downloadSuccessful = movieSync.download(
+            newCollectionMovies,
+            newWatchlistMovies,
+            newWatchedMoviesToPlays,
+            hasMergedMovies
+        )
         if (!downloadSuccessful) {
-            return false;
+            return false
         }
 
         if (!hasMergedMovies) {
-            boolean uploadSuccessful = movieSync.uploadAll();
+            val uploadSuccessful = movieSync.uploadAll()
             if (!uploadSuccessful) {
-                return false;
+                return false
             }
         }
 
         // add new movies with the just downloaded properties
-        boolean addingSuccessful = movieTools
-                .addMovies(newCollectionMovies, newWatchlistMovies, newWatchedMoviesToPlays);
+        val addingSuccessful = movieTools
+            .addMovies(newCollectionMovies, newWatchlistMovies, newWatchedMoviesToPlays)
         if (!hasMergedMovies) {
             // ensure all missing movies from Hexagon are added before merge is complete
             if (!addingSuccessful) {
-                return false;
+                return false
             }
             // set movies as merged
             PreferenceManager.getDefaultSharedPreferences(context)
-                    .edit()
-                    .putBoolean(HexagonSettings.KEY_MERGED_MOVIES, true)
-                    .apply();
+                .edit()
+                .putBoolean(HexagonSettings.KEY_MERGED_MOVIES, true)
+                .apply()
         }
 
-        return addingSuccessful;
+        return addingSuccessful
     }
 
-    private boolean syncLists() {
-        boolean hasMergedLists = HexagonSettings.hasMergedLists(context);
+    private fun syncLists(): Boolean {
+        val hasMergedLists = HexagonSettings.hasMergedLists(context)
 
-        HexagonListsSync listsSync = new HexagonListsSync(context, hexagonTools);
+        val listsSync = HexagonListsSync(context, hexagonTools)
         if (!listsSync.download(hasMergedLists)) {
-            return false;
+            return false
         }
 
         if (hasMergedLists) {
             // on regular syncs, remove lists gone from hexagon
             if (!listsSync.pruneRemovedLists()) {
-                return false;
+                return false
             }
         } else {
             // upload all lists on initial data merge
             if (!listsSync.uploadAll()) {
-                return false;
+                return false
             }
         }
 
         if (!hasMergedLists) {
             // set lists as merged
             PreferenceManager.getDefaultSharedPreferences(context)
-                    .edit()
-                    .putBoolean(HexagonSettings.KEY_MERGED_LISTS, true)
-                    .apply();
+                .edit()
+                .putBoolean(HexagonSettings.KEY_MERGED_LISTS, true)
+                .apply()
         }
 
-        return true;
+        return true
     }
 }
