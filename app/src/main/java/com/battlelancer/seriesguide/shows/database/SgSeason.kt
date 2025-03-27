858df5305d4a9bf426d26a17ba51cbba2c128267
Uwe <mail@heyuwe.de>
2025-03-27 13:44:01 +0100

    Kotlin: convert SgSeason

--- app/src/main/java/com/battlelancer/seriesguide/shows/database/SgSeason.kt
+++ app/src/main/java/com/battlelancer/seriesguide/shows/database/SgSeason.kt
@@ -1,51 +1,56 @@
-// Copyright 2023 Uwe Trottmann
 // SPDX-License-Identifier: Apache-2.0
+// Copyright 2018-2025 Uwe Trottmann
 
-package com.battlelancer.seriesguide.shows.database;
+package com.battlelancer.seriesguide.shows.database
 
-import androidx.room.ColumnInfo;
-import androidx.room.Entity;
-import androidx.room.ForeignKey;
-import androidx.room.Index;
-import androidx.room.PrimaryKey;
-import com.battlelancer.seriesguide.provider.SeriesGuideContract.Seasons;
-import com.battlelancer.seriesguide.provider.SeriesGuideContract.Shows;
-import com.battlelancer.seriesguide.provider.SeriesGuideContract.ShowsColumns;
-import com.battlelancer.seriesguide.provider.SeriesGuideDatabase.Tables;
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.ForeignKey
+import androidx.room.Index
+import androidx.room.PrimaryKey
+import com.battlelancer.seriesguide.provider.SeriesGuideContract.Seasons
+import com.battlelancer.seriesguide.provider.SeriesGuideContract.Shows
+import com.battlelancer.seriesguide.provider.SeriesGuideContract.ShowsColumns
+import com.battlelancer.seriesguide.provider.SeriesGuideDatabase.Tables
 
 /**
- * Legacy season entity kept for migration of legacy data. See {@link SgSeason2}.
+ * Legacy season entity kept for migration of legacy data. See [SgSeason2].
  */
 @Entity(
-        tableName = Tables.SEASONS,
-        foreignKeys = @ForeignKey(entity = SgShow.class,
-                parentColumns = Shows._ID, childColumns = ShowsColumns.REF_SHOW_ID),
-        indices = {@Index(ShowsColumns.REF_SHOW_ID)}
+    tableName = Tables.SEASONS,
+    foreignKeys = [
+        ForeignKey(
+            entity = SgShow::class,
+            parentColumns = [Shows._ID], childColumns = [ShowsColumns.REF_SHOW_ID]
+        )
+    ],
+    indices = [
+        Index(ShowsColumns.REF_SHOW_ID)
+    ]
 )
-public class SgSeason {
-
+data class SgSeason(
     @PrimaryKey
     @ColumnInfo(name = Seasons._ID)
-    public Integer tvdbId;
+    var tvdbId: Int? = null,
 
     @ColumnInfo(name = Seasons.COMBINED)
-    public Integer number;
+    var number: Int? = null,
 
     @ColumnInfo(name = ShowsColumns.REF_SHOW_ID)
-    public String showTvdbId;
+    var showTvdbId: String? = null,
 
     @ColumnInfo(name = Seasons.WATCHCOUNT)
-    public Integer watchCount = 0;
+    var watchCount: Int? = 0,
 
     @ColumnInfo(name = Seasons.UNAIREDCOUNT)
-    public Integer notReleasedCount = 0;
+    var notReleasedCount: Int? = 0,
 
     @ColumnInfo(name = Seasons.NOAIRDATECOUNT)
-    public Integer noReleaseDateCount = 0;
+    var noReleaseDateCount: Int? = 0,
 
     @ColumnInfo(name = Seasons.TAGS)
-    public String tags = "";
+    var tags: String? = "",
 
     @ColumnInfo(name = Seasons.TOTALCOUNT)
-    public Integer totalCount = 0;
-}
+    var totalCount: Int? = 0
+)
