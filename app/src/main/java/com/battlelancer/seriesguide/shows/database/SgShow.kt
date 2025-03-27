4768e15660fd04d9f848ff4b1851e3ac615fa764
Uwe <mail@heyuwe.de>
2025-03-27 13:39:04 +0100

    Kotlin: convert SgShow

--- app/src/main/java/com/battlelancer/seriesguide/shows/database/SgShow.kt
+++ app/src/main/java/com/battlelancer/seriesguide/shows/database/SgShow.kt
@@ -1,135 +1,133 @@
-// Copyright 2023 Uwe Trottmann
 // SPDX-License-Identifier: Apache-2.0
+// Copyright 2018-2025 Uwe Trottmann
 
-package com.battlelancer.seriesguide.shows.database;
+package com.battlelancer.seriesguide.shows.database
 
-import androidx.annotation.NonNull;
-import androidx.room.ColumnInfo;
-import androidx.room.Entity;
-import androidx.room.PrimaryKey;
-import com.battlelancer.seriesguide.provider.SeriesGuideContract.Shows;
-import com.battlelancer.seriesguide.provider.SeriesGuideDatabase.Tables;
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.battlelancer.seriesguide.provider.SeriesGuideContract.Shows
+import com.battlelancer.seriesguide.provider.SeriesGuideDatabase.Tables
 
 /**
- * Legacy show entity kept for migration of legacy data. See {@link SgShow2}.
+ * Legacy show entity kept for migration of legacy data. See [SgShow2].
  */
 @Entity(tableName = Tables.SHOWS)
-public class SgShow {
-
+data class SgShow(
     @PrimaryKey
     @ColumnInfo(name = Shows._ID)
-    public int tvdbId;
+    var tvdbId: Int,
 
     @ColumnInfo(name = Shows.SLUG)
-    public String slug = "";
+    var slug: String? = "",
 
     /**
      * Ensure this is NOT null (enforced through database constraint).
      */
     @ColumnInfo(name = Shows.TITLE)
-    @NonNull
-    public String title = "";
+    var title: String = "",
 
     /**
      * The title without any articles (e.g. 'the' or 'an'). Added with db version 33.
      */
     @ColumnInfo(name = Shows.TITLE_NOARTICLE)
-    public String titleNoArticle;
+    var titleNoArticle: String? = null,
 
     @ColumnInfo(name = Shows.OVERVIEW)
-    public String overview = "";
+    var overview: String? = "",
 
     /**
      * Local release time. Encoded as integer (hhmm).
      *
-     * <pre>
+     * ```
      * Example: 2035
      * Default: -1
-     * </pre>
+     * ```
      */
     @ColumnInfo(name = Shows.RELEASE_TIME)
-    public Integer releaseTime;
+    var releaseTime: Int? = null,
     /**
      * Local release week day. Encoded as integer.
-     * <pre>
+     *
+     * ```
      * Range:   1-7
      * Daily:   0
      * Default: -1
-     * </pre>
+     * ```
      */
     @ColumnInfo(name = Shows.RELEASE_WEEKDAY)
-    public Integer releaseWeekDay;
+    var releaseWeekDay: Int? = null,
     @ColumnInfo(name = Shows.RELEASE_COUNTRY)
-    public String releaseCountry;
+    var releaseCountry: String? = null,
     @ColumnInfo(name = Shows.RELEASE_TIMEZONE)
-    public String releaseTimeZone;
+    var releaseTimeZone: String? = null,
 
     @ColumnInfo(name = Shows.FIRST_RELEASE)
-    public String firstRelease;
+    var firstRelease: String? = null,
 
     @ColumnInfo(name = Shows.GENRES)
-    public String genres = "";
+    var genres: String? = "",
     @ColumnInfo(name = Shows.NETWORK)
-    public String network = "";
+    var network: String? = "",
 
     @ColumnInfo(name = Shows.RATING_GLOBAL)
-    public Double ratingGlobal;
+    var ratingGlobal: Double? = null,
     @ColumnInfo(name = Shows.RATING_VOTES)
-    public Integer ratingVotes;
+    var ratingVotes: Int? = null,
     @ColumnInfo(name = Shows.RATING_USER)
-    public Integer ratingUser;
+    var ratingUser: Int? = null,
 
     @ColumnInfo(name = Shows.RUNTIME)
-    public String runtime = "";
+    var runtime: String? = "",
     @ColumnInfo(name = Shows.STATUS)
-    public String status = "";
+    var status: String? = "",
     @ColumnInfo(name = Shows.CONTENTRATING)
-    public String contentRating = "";
+    var contentRating: String? = "",
 
     @ColumnInfo(name = Shows.NEXTEPISODE)
-    public String nextEpisode = "";
+    var nextEpisode: String? = "",
 
     @ColumnInfo(name = Shows.POSTER)
-    public String poster = "";
+    var poster: String? = "",
 
     @ColumnInfo(name = Shows.POSTER_SMALL)
-    public String posterSmall = "";
+    var posterSmall: String? = "",
 
     @ColumnInfo(name = Shows.NEXTAIRDATEMS)
-    public Long nextAirdateMs;
+    var nextAirdateMs: Long? = null,
     @ColumnInfo(name = Shows.NEXTTEXT)
-    public String nextText = "";
+    var nextText: String? = "",
 
     @ColumnInfo(name = Shows.IMDBID)
-    public String imdbId = "";
+    var imdbId: String? = "",
     @ColumnInfo(name = Shows.TRAKT_ID)
-    public Integer traktId = 0;
+    var traktId: Int? = 0,
 
     @ColumnInfo(name = Shows.FAVORITE)
-    public boolean favorite = false;
+    var favorite: Boolean = false,
 
     @ColumnInfo(name = Shows.HEXAGON_MERGE_COMPLETE)
-    public boolean hexagonMergeComplete = true;
+    var hexagonMergeComplete: Boolean = true,
 
     @ColumnInfo(name = Shows.HIDDEN)
-    public boolean hidden = false;
+    var hidden: Boolean = false,
 
     @ColumnInfo(name = Shows.LASTUPDATED)
-    public long lastUpdatedMs = 0L;
+    var lastUpdatedMs: Long = 0L,
     @ColumnInfo(name = Shows.LASTEDIT)
-    public long lastEditedSec = 0L;
+    var lastEditedSec: Long = 0L,
 
     @ColumnInfo(name = Shows.LASTWATCHEDID)
-    public int lastWatchedEpisodeId = 0;
+    var lastWatchedEpisodeId: Int = 0,
     @ColumnInfo(name = Shows.LASTWATCHED_MS)
-    public long lastWatchedMs = 0L;
+    var lastWatchedMs: Long = 0L,
 
     @ColumnInfo(name = Shows.LANGUAGE)
-    public String language = "";
+    var language: String? = "",
 
     @ColumnInfo(name = Shows.UNWATCHED_COUNT)
-    public int unwatchedCount = SgShow2.UNKNOWN_UNWATCHED_COUNT;
+    var unwatchedCount: Int = SgShow2.UNKNOWN_UNWATCHED_COUNT,
 
     @ColumnInfo(name = Shows.NOTIFY)
-    public boolean notify = true;
-}
+    var notify: Boolean = true
+)
