b99fe8d57907e6b8aaaf87bd4d8c682d91cd7647
Uwe <mail@heyuwe.de>
2025-03-27 14:08:57 +0100

    Kotlin: convert SqliteTestDbOpenHelper

--- app/src/androidTest/java/com/battlelancer/seriesguide/provider/SqliteTestDbOpenHelper.kt
+++ app/src/androidTest/java/com/battlelancer/seriesguide/provider/SqliteTestDbOpenHelper.kt
@@ -1,32 +1,29 @@
-// Copyright 2023 Uwe Trottmann
 // SPDX-License-Identifier: Apache-2.0
+// Copyright 2018-2025 Uwe Trottmann
 
-package com.battlelancer.seriesguide.provider;
+package com.battlelancer.seriesguide.provider
 
-import android.content.Context;
-import android.database.sqlite.SQLiteDatabase;
-import android.database.sqlite.SQLiteOpenHelper;
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
 
-public class SqliteTestDbOpenHelper extends SQLiteOpenHelper {
+class SqliteTestDbOpenHelper(
+    context: Context, databaseName: String
+) : SQLiteOpenHelper(context, databaseName, null, SeriesGuideDatabase.DBVER_42_JOBS) {
 
-    public SqliteTestDbOpenHelper(Context context, String databaseName) {
-        super(context, databaseName, null, SeriesGuideDatabase.DBVER_42_JOBS);
+    override fun onCreate(db: SQLiteDatabase) {
+        db.execSQL(SeriesGuideDatabase.CREATE_SHOWS_TABLE)
+        db.execSQL(SeriesGuideDatabase.CREATE_SEASONS_TABLE)
+        db.execSQL(SeriesGuideDatabase.CREATE_EPISODES_TABLE)
+        db.execSQL(SeriesGuideDatabase.CREATE_SEARCH_TABLE) /* Invisible to Room */
+        db.execSQL(SeriesGuideDatabase.CREATE_LISTS_TABLE)
+        db.execSQL(SeriesGuideDatabase.CREATE_LIST_ITEMS_TABLE)
+        db.execSQL(SeriesGuideDatabase.CREATE_MOVIES_TABLE)
+        db.execSQL(SeriesGuideDatabase.CREATE_ACTIVITY_TABLE)
+        db.execSQL(SeriesGuideDatabase.CREATE_JOBS_TABLE)
     }
 
-    public void onCreate(SQLiteDatabase db) {
-        db.execSQL(SeriesGuideDatabase.CREATE_SHOWS_TABLE);
-        db.execSQL(SeriesGuideDatabase.CREATE_SEASONS_TABLE);
-        db.execSQL(SeriesGuideDatabase.CREATE_EPISODES_TABLE);
-        db.execSQL(SeriesGuideDatabase.CREATE_SEARCH_TABLE); /* Invisible to Room */
-        db.execSQL(SeriesGuideDatabase.CREATE_LISTS_TABLE);
-        db.execSQL(SeriesGuideDatabase.CREATE_LIST_ITEMS_TABLE);
-        db.execSQL(SeriesGuideDatabase.CREATE_MOVIES_TABLE);
-        db.execSQL(SeriesGuideDatabase.CREATE_ACTIVITY_TABLE);
-        db.execSQL(SeriesGuideDatabase.CREATE_JOBS_TABLE);
-    }
-
-    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+    override fun onUpgrade(db: SQLiteDatabase, oldVersion: Int, newVersion: Int) {
         // Not testing migration from older versions created using SQLiteOpenHelper API
     }
-
 }
