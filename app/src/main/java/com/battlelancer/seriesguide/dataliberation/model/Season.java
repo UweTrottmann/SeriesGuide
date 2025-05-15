6bdd314cdd5f2c35a0139ebcb67d1aa71a50f6b2
Uwe <mail@heyuwe.de>
2025-05-15 15:33:38 +0200

    Export: expand GSON model field documentation

--- app/src/main/java/com/battlelancer/seriesguide/dataliberation/model/Season.java
+++ app/src/main/java/com/battlelancer/seriesguide/dataliberation/model/Season.java
@@ -10,16 +10,25 @@ import java.util.List;
 public class Season {
 
     /**
-     * May be null for legacy backups.
+     * Required when importing.
+     * <p>
+     * It is currently still supported that a {@link #tvdb_id} is set instead if a
+     * {@link Show#tvdb_id} is set, like in exports created before the TMDB migration.
      */
     @Nullable public String tmdb_id;
     /**
-     * Is null on new backups.
+     * May be set instead of a {@link #tmdb_id}, but only if the show has a {@link Show#tvdb_id}
+     * set.
+     * <p>
+     * May be exported for shows that were added before the TMDB migration.
      */
     @SerializedName("tvdb_id")
     @Nullable
     public Integer tvdbId;
 
+    /**
+     * The number of a season. Starting from 0 for Special Episodes.
+     */
     public int season;
 
     public List<Episode> episodes;
