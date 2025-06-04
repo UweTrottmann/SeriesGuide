d8111b59c6bbd3fdf13e53cc51e52259064d21ce
Uwe <mail@heyuwe.de>
2025-06-04 17:27:40 +0200

    SgShow2Helper: Room only returns non-nullable values for List<Int>

--- app/src/main/java/com/battlelancer/seriesguide/shows/database/SgShow2Helper.kt
+++ app/src/main/java/com/battlelancer/seriesguide/shows/database/SgShow2Helper.kt
@@ -126,7 +126,7 @@ interface SgShow2Helper {
     fun countHiddenShows(): Int
 
     @Query("SELECT series_tmdb_id FROM sg_show WHERE series_hidden = 1")
-    fun getHiddenShowsTmdbIds(): List<Int?>
+    fun getHiddenShowsTmdbIds(): List<Int>
 
     @Query("UPDATE sg_show SET series_hidden = 0 WHERE series_hidden = 1")
     fun makeHiddenVisible(): Int
