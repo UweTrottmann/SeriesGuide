6b6f4f447145e7ebd9cda13f1f9f0ef7b9d8395c
Uwe <mail@heyuwe.de>
2025-07-31 15:40:09 +0200

    Prepare version 2025.2.7 (21250209)

--- RELEASING.md
+++ RELEASING.md
@@ -20,7 +20,7 @@
 - Commit and push
 
   ```shell
-  git commit --all --message "Prepare version 2025.2.7 (21250208)"
+  git commit --all --message "Prepare version 2025.2.7 (21250209)"
   git push --set-upstream origin release-2025.2
   ```
 
--- build.gradle.kts
+++ build.gradle.kts
@@ -24,7 +24,7 @@ buildscript {
     // - allows multiple releases per month (though currently unlikely)
     val sgVersionName by extra("2025.2.7")
     // version 21yyrrbb -> min SDK 21, year yy, release rr, build bb
-    val sgVersionCode by extra(21250208)
+    val sgVersionCode by extra(21250209)
 
     val isCiBuild by extra { System.getenv("CI") == "true" }
 
