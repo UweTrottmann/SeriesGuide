1afe079d6aa00bad8a2c6085c8bfb9cab1668313
Uwe <uwe@uwetrottmann.com>
2025-08-22 10:26:52 +0200

    Android: require at least Android 6 (SDK 23)

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,7 +16,7 @@ plugins {
 
 buildscript {
     val sgCompileSdk by extra(35) // Android 15 (VANILLA_ICE_CREAM)
-    val sgMinSdk by extra(21) // Android 5 (L)
+    val sgMinSdk by extra(23) // Android 6 (M)
     // WARNING Can not increase to 36 (Android 16) until Theme.SeriesGuide.DayNight.OptOutEdgeToEdge
     // is no longer used as windowOptOutEdgeToEdgeEnforcement will be ignored (see notes on theme).
     val sgTargetSdk by extra(35) // Android 15 (VANILLA_ICE_CREAM)
