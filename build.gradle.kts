87618d53bec4784c181fb7dc95016c9a41e5f8a4
Uwe <uwe@uwetrottmann.com>
2025-11-20 11:14:22 +0100

    Cloud: drop edge-to-edge opt-out for Firebase Auth UI

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,8 +17,6 @@ plugins {
 buildscript {
     val sgCompileSdk by extra(36) // Android 16 (BAKLAVA)
     val sgMinSdk by extra(23) // Android 6 (M)
-    // WARNING Can not increase to 36 (Android 16) until Theme.SeriesGuide.DayNight.OptOutEdgeToEdge
-    // is no longer used as windowOptOutEdgeToEdgeEnforcement will be ignored (see notes on theme).
     val sgTargetSdk by extra(35) // Android 15 (VANILLA_ICE_CREAM)
 
     // YYYY.<release-of-year>.<build> - like 2024.1.0
