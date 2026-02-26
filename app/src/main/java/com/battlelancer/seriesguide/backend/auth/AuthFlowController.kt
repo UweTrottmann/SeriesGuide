d922b37e7c8bc01e3b83daeb8f5d33103f27a6f7
Uwe <uwe@uwetrottmann.com>
2026-02-26 15:08:25 +0100

    Auth: update copyright header

--- app/src/main/java/com/battlelancer/seriesguide/backend/auth/AuthFlowController.kt
+++ app/src/main/java/com/battlelancer/seriesguide/backend/auth/AuthFlowController.kt
@@ -1,5 +1,6 @@
-// SPDX-License-Identifier: Apache-2.0
+// SPDX-License-Identifier: Apache-2.0 AND AGPL-3.0-or-later
 // SPDX-FileCopyrightText: Copyright © 2025 Google Inc. All Rights Reserved.
+// SPDX-FileCopyrightText: Copyright © 2026 Uwe Trottmann <uwe@uwetrottmann.com>
 
 // Original file by Google Inc. licensed under Apache-2.0 copied from FirebaseUI-Android
 // https://github.com/firebase/FirebaseUI-Android
