8bd26bddae42ffa23998e2a016ab336bfb17bc40
Uwe <uwe@uwetrottmann.com>
2026-05-01 09:41:29 +0200

    Auth: add missing license and copyright info

--- app/src/main/java/com/battlelancer/seriesguide/backend/auth/ui/components/VerificationCodeInputField.kt
+++ app/src/main/java/com/battlelancer/seriesguide/backend/auth/ui/components/VerificationCodeInputField.kt
@@ -1,3 +1,9 @@
+// SPDX-License-Identifier: Apache-2.0
+// SPDX-FileCopyrightText: Copyright © 2025 Google Inc. All Rights Reserved.
+
+// Original file by Google Inc. licensed under Apache-2.0 copied from FirebaseUI-Android
+// https://github.com/firebase/FirebaseUI-Android
+
 package com.firebase.ui.auth.ui.components
 
 import androidx.compose.animation.core.animateDpAsState
