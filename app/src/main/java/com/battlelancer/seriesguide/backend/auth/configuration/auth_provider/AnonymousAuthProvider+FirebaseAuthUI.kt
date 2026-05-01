60173a97ccec41eeab047d0ae54e9fd0e8ff9873
Uwe <uwe@uwetrottmann.com>
2026-05-01 09:41:29 +0200

    Auth: add missing license and copyright info

--- app/src/main/java/com/battlelancer/seriesguide/backend/auth/configuration/auth_provider/AnonymousAuthProvider+FirebaseAuthUI.kt
+++ app/src/main/java/com/battlelancer/seriesguide/backend/auth/configuration/auth_provider/AnonymousAuthProvider+FirebaseAuthUI.kt
@@ -1,3 +1,9 @@
+// SPDX-License-Identifier: Apache-2.0
+// SPDX-FileCopyrightText: Copyright © 2025 Google Inc. All Rights Reserved.
+
+// Original file by Google Inc. licensed under Apache-2.0 copied from FirebaseUI-Android
+// https://github.com/firebase/FirebaseUI-Android
+
 package com.firebase.ui.auth.configuration.auth_provider
 
 import androidx.compose.runtime.Composable
