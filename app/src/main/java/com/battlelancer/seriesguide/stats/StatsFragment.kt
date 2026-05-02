5c06e5ff226917a563c9e708b8ec3d8d9f0c6ad7
Uwe <uwe@uwetrottmann.com>
2026-05-02 08:13:25 +0200

    License contributions of ippschi under GPL-3.0-or-later
    As agreed upon in
    https://github.com/UweTrottmann/SeriesGuide/pull/1163#issuecomment-4363088075

--- app/src/main/java/com/battlelancer/seriesguide/stats/StatsFragment.kt
+++ app/src/main/java/com/battlelancer/seriesguide/stats/StatsFragment.kt
@@ -1,8 +1,6 @@
-// SPDX-License-Identifier: Apache-2.0 AND GPL-3.0-or-later
+// SPDX-License-Identifier: GPL-3.0-or-later
 // SPDX-FileCopyrightText: Copyright © 2019 Uwe Trottmann <uwe@uwetrottmann.com>
 // SPDX-FileCopyrightText: Copyright © 2021 Andre Ippisch
-
-// Contributions by Andre Ippisch licensed under Apache-2.0
 
 package com.battlelancer.seriesguide.stats
 
