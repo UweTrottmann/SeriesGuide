8fc5404f85845a6f813324d6d170cf4fe3ff9795
Uwe <uwe@uwetrottmann.com>
2026-08-13 16:40:38 +0200

    Gradle 9.6: remove remaining deprecated property delegate usage
    Remove isCiBuild, it appears unused since a while ago.

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,11 +18,9 @@ buildscript {
     // YYYY.<release-of-year>.<build> - like 2024.1.0
     // - allows to more easily judge how old a release is
     // - allows multiple releases per month (though currently unlikely)
-    val sgVersionName by extra("2026.3.0")
+    extra.set("sgVersionName", "2026.3.0")
     // version 23yyrrbb -> min SDK 23, year yy, release rr, build bb
-    val sgVersionCode by extra(23260300)
-
-    val isCiBuild by extra { System.getenv("CI") == "true" }
+    extra.set("sgVersionCode", 23260300)
 
     // load some properties that should not be part of version control
     if (file("secret.properties").exists()) {
