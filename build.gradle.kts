716ab5c30b8e37694b5a40eac499fb9cc1c1f875
Uwe <uwe@uwetrottmann.com>
2026-08-13 16:29:46 +0200

    Gradle: print deprecated usages when compiling

--- build.gradle.kts
+++ build.gradle.kts
@@ -37,6 +37,13 @@ buildscript {
     }
 }
 
+allprojects {
+    // Print deprecated usage like Kotlin compiler
+    tasks.withType<JavaCompile> {
+        options.isDeprecation = true
+    }
+}
+
 fun isNonStable(version: String): Boolean {
     val stableKeyword = listOf("RELEASE", "FINAL", "GA").any { version.uppercase().contains(it) }
     val regex = "^[0-9,.v-]+(-r)?$".toRegex()
