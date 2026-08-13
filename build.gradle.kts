c0fa9513285efaffce32f8848218a6f3452af310
Uwe <uwe@uwetrottmann.com>
2026-08-13 09:24:07 +0200

    Gradle: migrate to versions settings plugin, update to 0.61.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,5 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    alias(libs.plugins.versions)
     // app, libraries
     alias(libs.plugins.android) apply false
     alias(libs.plugins.kotlin) apply false
