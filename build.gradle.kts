551bb7f0109ef4801da99bb9ca7a298841c1d647
Uwe <uwe@uwetrottmann.com>
2026-09-17 12:48:34 +0200

    Kotlin 2.4: update Kotlin to 2.4.20

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     // app, libraries
+    alias(libs.plugins.kotlin) apply false
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.android.library) apply false
     alias(libs.plugins.compose.compiler) apply false
